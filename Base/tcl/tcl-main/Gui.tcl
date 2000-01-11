91ac6ea26458906f838718b555ea8287257c6fcb
odonnell <odonnell>
2000-01-11 21:57:12 +0000

    Initial Checkin

--- Base/tcl/tcl-main/Gui.tcl
+++ Base/tcl/tcl-main/Gui.tcl
@@ -5,7 +5,7 @@
 # The following terms apply to all files associated with the software unless
 # explicitly disclaimed in individual files.   
 # 
-# The authors hereby grant permission to use and copy (but not distribute) this
+# The authors hereby grant permission to use, copy, and distribute this
 # software and its documentation for any NON-COMMERCIAL purpose, provided
 # that existing copyright notices are retained verbatim in all copies.
 # The authors grant permission to modify this software and its documentation 
@@ -26,7 +26,7 @@
 # MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #===============================================================================
 # FILE:        Gui.tcl
-# DATE:        12/10/1999 08:40
+# DATE:        12/09/1999 14:09
 # LAST EDITOR: gering
 # PROCEDURES:  
 #   GuiInit
@@ -43,8 +43,6 @@
 #   MakeColorNormalized
 #   GetRelativeFileName
 #   ExpandPath
-#   GetPrefix
-#   GetSuffix
 #   FindUniqueFileName
 #   DecodeFileName
 #   CheckVolumeExists
@@ -63,7 +61,7 @@
 # .END
 #-------------------------------------------------------------------------------
 proc GuiInit {} {
-	global Gui InitProc tcl_platform
+	global Gui Path InitProc tcl_platform
 
 	set InitProc() ""
 
@@ -84,18 +82,18 @@ proc GuiInit {} {
 	set Gui(title) "3D Slicer"
 	
 	# prefix
-	set Gui(prefixSaveImage3D) img3D
-	set Gui(prefixColors) Colors
-	set Gui(prefixSaveCurrentDag) data
-	set Gui(prefixSaveRegister) reg
-	set Gui(prefixSaveDag) data
-	set Gui(prefixOpenDag) data
-	set Gui(prefixOpenVolume) I
-	set Gui(prefixOpenModel) skin
-	set Gui(prefixEditWorking) work
-	set Gui(prefixEditComposite) comp
-	set Gui(prefixSmgModel) skin
-	set Gui(prefixEditSession) edit
+	set Path(prefixSaveImage3D) img3D
+	set Path(prefixColors) Colors
+	set Path(prefixSaveCurrentDag) data
+	set Path(prefixSaveRegister) reg
+	set Path(prefixSaveFile) data
+	set Path(prefixOpenFile) data
+	set Path(prefixOpenVolume) I
+	set Path(prefixOpenModel) skin
+	set Path(prefixEditWorking) work
+	set Path(prefixEditComposite) comp
+	set Path(prefixSmgModel) skin
+	set Path(prefixEditSession) edit
 
 	# INIT
 	set Gui(waitSemaphore) 0
@@ -176,7 +174,7 @@ proc GuiInit {} {
 
 	# Workspace Title Attributes (WTA)
 	lappend attr WTA 
-	set Gui(WTA) {-font {helvetica 8 bold italic} \
+	set Gui(WTA) {-font {helvetica 8 bold} \
 		-bg $Gui(activeWorkspace) -fg $Gui(textDark) \
 		-bd 0 -padx 1 -pady 1 -relief flat }
 
@@ -210,7 +208,13 @@ proc GuiInit {} {
 		-highlightthickness 0 -orient horizontal -showvalue 0 -sliderlength 24 \
 		-bd $Gui(borderWidth) -relief flat}
 
-	# Navigation Scale Attributes (BSA)
+	# Backdrop Label Attributes (BLA)
+	lappend attr BLA 
+	set Gui(BLA) { -font {helvetica 8}\
+		-bg $Gui(backdrop) -fg $Gui(textLight) \
+		-bd 0 -padx 1 -pady 1 -relief flat }
+
+	# Backdrop Scale Attributes (BSA)
 	lappend attr BSA 
 	set Gui(BSA) { -font {helvetica 8}\
 		-bg $Gui(backdrop) -fg $Gui(textDark) \
@@ -218,29 +222,9 @@ proc GuiInit {} {
 		-highlightthickness 0 -orient vertical -showvalue 0 -sliderlength 24 \
 		-bd $Gui(borderWidth) -relief flat}
 
-	# Help Attributes (HA)
-	lappend attr HA 
-	set Gui(HA) {  -font {helvetica 8}\
-		-bg $Gui(inactiveWorkspace) -fg $Gui(textDark) \
-		-bd $Gui(borderWidth) -padx 1 -pady 1 -relief flat }
-
-	# Navigation Label Attributes (NLA)
-	lappend attr NLA 
-	set Gui(NLA) { -font {helvetica 8} \
-		-bg $Gui(inactiveWorkspace) -fg $Gui(textDark) \
-		-bd $Gui(borderWidth) -padx 1 -pady 1 -relief flat }
-
-	# Navigation Annotation Attributes (NAA)
-	lappend attr NAA 
-	set Gui(NAA) {-font {courier 8 bold}  \
-		-bg $Gui(inactiveWorkspace) -fg $Gui(textDark) \
-		-bd 0 -padx 0 -pady 0 -relief flat}
-
-	# Backdrop Label Attributes (BLA)
-	lappend attr BLA 
-	set Gui(BLA) { -font {helvetica 8}\
-		-bg $Gui(backdrop) -fg $Gui(textLight) \
-		-bd 0 -padx 1 -pady 1 -relief flat }
+	# System Menu Attributes (SMA)
+	lappend attr SMA 
+	set Gui(SMA) { -tearoff 0}
 
 	# Change font to large
 	if {$Gui(smallFont) == 0} {
@@ -564,25 +548,11 @@ proc MakeColorNormalized {str} {
 }
 
 #-------------------------------------------------------------------------------
-# .PROC GetRelativeFileName
-# .END
-#-------------------------------------------------------------------------------
-proc GetRelativeFileName {root full} {
-
-	set first [string first $root $full]
-	if {$first == 0} {
-		set rel [string range $full [expr [string length $root] + 1] end]
-		return $rel
-	}
-	return $full
-}
-
-#-------------------------------------------------------------------------------
 # .PROC ExpandPath
 # .END
 #-------------------------------------------------------------------------------
 proc ExpandPath {filename} {
-	global env Path
+	global Path
 
 	set home $Path(program)
  
@@ -591,257 +561,6 @@ proc ExpandPath {filename} {
 	} else {
 		return [file join $home $filename]
 	}
-}
-
-#-------------------------------------------------------------------------------
-# .PROC GetPrefix
-# .END
-#-------------------------------------------------------------------------------
-proc GetPrefix {filename} {
-	set root [file rootname $filename]
-	set f [expr [string last / $root] + 1]
-	if {$f == -1} {
-		return $root
-	}
-	set l [expr [string length $root] - 1]
-	set prefix [string range $root $f $l]
-	return $prefix
-}
-
-#-------------------------------------------------------------------------------
-# .PROC GetSuffix
-# .END
-#-------------------------------------------------------------------------------
-proc GetSuffix {filename} {
-	set f [expr [string last . $filename] + 1]
-	if {$f == -1} {
-		return ""
-	}
-	set l [expr [string length $filename] - 1]
-	set suffix [string range $filename $f $l]
-	return $suffix
-}
-
-		
-#-------------------------------------------------------------------------------
-# GetSaveFile
-#
-# root     = root path
-# rel      = initial path relative to root
-# typelist = see documentation for the tk_getSaveFile proc (ie:
-#	set typelist {
-#		{"MRML File" {".mrml"}}
-#		{"All Files" {*}}
-#	}
-# ext      = default file extension (ie: mrml)
-# title    = popup window title (ie: "Save File")
-#-------------------------------------------------------------------------------
-proc GetSaveFile {root rel typelist ext title {dialog 1}} {
-
-	set path   [file join $root $rel]
-
-	# Allow the prefix to only be a number
-	set code   [DecodeFileName $path] 
-	set dir    [lindex $code 0]
-	set prefix [lindex $code 1]
-
-	# See if the path is just a valid directory
-	if {[file isdirectory $path] == 1} {
-		# Yes, so use no prefix
-		set dir    $path
-		set prefix ""
-	} else {
-		# See if the path is a valid directory and prefix
-		if {[file isdirectory $dir] == 1} {
-		} else {
-			# No, so just use root
-			set dir    $root
-			set prefix ""
-		}
-	}
-
-	set num [FindUniqueFileName $dir $prefix $ext]
-
-	if {$dialog == 1} {
-		set filename [tk_getSaveFile -title "$title" -defaultextension ".$ext" \
-			-filetypes $typelist -initialdir  "$dir" \
-			-initialfile [file join $dir "$prefix$num.$ext"]]
-	} else {
-		set filename [file join $dir "$prefix$num.$ext"]
-	}
-
-	return [GetRelativeFileName $root $filename]
-}
-
-#-------------------------------------------------------------------------------
-# GetOpenFile
-#
-# root     = root path
-# rel      = initial path relative to root
-# typelist = see documentation for the tk_getSaveFile proc (ie:
-#	set typelist {
-#		{"MRML File" {".mrml"}}
-#		{"All Files" {*}}
-#	}
-# ext      = default file extension (ie: mrml)
-# title    = popup window title (ie: "Save File")
-# forceExt = if 1, forces the user to select a file with extension 'ext'
-#-------------------------------------------------------------------------------
-proc GetOpenFile {root rel typelist ext title {forceExt 1}} {
-	global Gui
-
-	set path   [file join $root $rel]
-	set dir    [file dirname $path]
-	set prefix [GetPrefix $path]
-
-	# See if the path is just a valid directory
-	if {[file isdirectory $path] == 1} {
-		# Yes, so use no prefix
-		set dir    $path
-		set prefix ""
-	} else {
-		# See if the path is a valid directory and prefix
-		if {[file isdirectory $dir] == 1} {
-		} else {
-			# No, so just use root
-			set dir    $root
-			set prefix ""
-		}
-	}
-
-	set filename [tk_getOpenFile -title "$title" -defaultextension ".$ext" \
-		-filetypes $typelist -initialdir  "$dir" \
-		-initialfile [file join $dir "$prefix.$ext"]]
-	
-	# User cancelled
-	if {$filename == ""} {return}
-
-	if {$forceExt == 1} {
-		# Allow *.ext files only
-		if {[GetSuffix $filename] != "$ext"} {
-			tk_messageBox -icon error -title $Gui(title) \
-				-message "Please select a file that ends with '.$ext'"
-			return ""
-		}
-	}
-
-	return [GetRelativeFileName $root $filename]
-}
-
-#-------------------------------------------------------------------------------
-# .PROC FindUniqueFileName
-# .END
-#-------------------------------------------------------------------------------
-proc FindUniqueFileName {dir prefix ext} {
-	
-	# Create directory if it does not exist.
-	# If this fails, then use the current directory
-	if {[file isdirectory $dir] == 0} {
-		if {$dir != ""} {
-			file mkdir $dir
-		}
-		if {[file isdirectory $dir] == 0} {
-			set dir ""
-		}
-	}
-
-    # Find a unique filename
-    set name [file join $dir $prefix]
-    set num 1
-    set filename $name$num.$ext
-    while {[file exists $filename] == 1} {
-        incr num
-        set filename $name$num.$ext
-    }
-	return $num
-}
-
-#-------------------------------------------------------------------------------
-# .PROC DecodeFileName
-# .END
-#-------------------------------------------------------------------------------
-proc DecodeFileName {filename} {
-    set dir  [file dirname  $filename]
-    set root [file rootname $filename]
-    set i [expr [string length $root] - 1]
-    set c [string index $root $i]
-    while {$i >= 0 && [lsearch "0 1 2 3 4 5 6 7 8 9" $c] != "-1"} {
-        set i [expr $i - 1]
-        set c [string index $root $i]
-    }  
-    if {$i < 0} {
-        set i [string length $root]
-    }
-    set f [expr [string last / $filename] + 1]
-    set l $i
-    set prefix [string range $root 0 $l]
-	return "$dir $prefix" 
-}
-
-#-------------------------------------------------------------------------------
-# .PROC CheckVolumeExists
-# .END
-#-------------------------------------------------------------------------------
-proc CheckVolumeExists {filePrefix filePattern firstNum lastNum {verbose 1}} {
-	global Gui
-
-	# Check that it's a prefix, not a directory
-	if {[file isdirectory $filePrefix] == 1} {
-		tk_messageBox -icon error -title $Gui(title) \ 
-			-message "'$filePrefix' is a directory instead of a prefix."
-		return
-	}
-
-	# Check directory is there
-	set dir [file dirname $filePrefix]
-	if {[file isdirectory $dir] == 0} {
-		tk_messageBox -icon info -type ok -title $Gui(title) \
-		-message "Directory '$dir' does not exist."
-		return
-	}
-
-	set num $firstNum
-	while {$num <= $lastNum} {
-		set filename [format $filePattern $filePrefix $num]
-		if {[CheckFileExists $filename $verbose] == 0} {
-			return $filename
-		}
-		incr num
-	}
-	return "" 
-}
-
-#-------------------------------------------------------------------------------
-# CheckFileExists
-#
-# Checks if a file exists, is not a directory, and is readable.
-# Returns 1 on success, else 0.
-#-------------------------------------------------------------------------------
-proc CheckFileExists {filename {verbose 1}} {
-	global Gui
-
-	if {[file exists $filename] == 0} {
-		if {$verbose == 1} {
-			tk_messageBox -icon info -type ok -title $Gui(title) \
-				-message "File '$filename' does not exist."
-		}
-		return 0
-	}
-	if {[file isdirectory $filename] == 1} {
-		if {$verbose == 1} {
-			tk_messageBox -icon info -type ok -title $Gui(title) \
-				-message "'$filename' is a directory, not a file."
-		}
-		return 0
-	}
-	if {[file readable $filename] == 0} {
-		if {$verbose == 1} {
-			tk_messageBox -icon info -type ok -title $Gui(title) \
-				-message "'$filename' exists, but is unreadable."
-		}
-		return 0
-	}
-	return 1
 }
 
 #-------------------------------------------------------------------------------
