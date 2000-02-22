353ebe9d14590cdcdd365c69751b256573a3f7c8
samson <samson>
2000-02-22 19:49:17 +0000

    Added a first version of PrintHeaderTcl. This will replace the executable version.  However, it does not handle all types of images yet.  Also, it is only 95% done.

--- Base/tcl/tcl-main/MainHeader.tcl
+++ Base/tcl/tcl-main/MainHeader.tcl
@@ -26,7 +26,8 @@
 # MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #===============================================================================
 # FILE:        MainHeader.tcl
-# DATE:        02/22/2000 11:11
+# DATE:        01/20/2000 09:40
+# LAST EDITOR: gering
 # PROCEDURES:  
 #   ReadHeader
 #   ParsePrintHeader
@@ -35,9 +36,331 @@
 #   GetHeaderInfo
 #==========================================================================auto=
 
+#-------------------------------------------------------------------------------
+# .PROC ReadHeaderTcl 
+#
+#  Opens an Image with a header and sets
+#  Header(xDim yDim xSpacing ySpacing sliceThick sliceSpace  
+#        littleEndian scalarSize rC aC sC rTL aTL sTL rTR aTR sTR rBR aBR sBR)
+#
+#  This corresponds to 
+#     x_resolution y_resolution pixel_xsize pixel_ysize thick
+#     space byte_order bytes_per_pixel coord_center_r coord_center_a
+#     coord_center_s coord_r_top_left coord_a_top_left coord_s_top_left
+#     coord_r_top_right coord_a_top_right coord_s_top_right
+#     coord_r_bottom_right coord_a_bottom_right coord_s_bottom_right"
+#
+# xDim yDim have problems
+# littleEndian has problems
+# bytes_per_Pixel or scalarSize has problems
+# need to have error message for no header
+# only handles signa and genesis
+#
+#
+# .END
+#-------------------------------------------------------------------------------
+proc ReadHeaderTcl { filename } {
 
+    upvar $aHeader Header
+        
+    set f [open $filename r]
+    
+    proc getvar_from_file {filehandle offset length {ScanString ""}} {
+        seek $filehandle $offset start
+        set var [read $filehandle $length]
+        if {[string length $ScanString] != 0} {
+            binary scan $var $ScanString var2
+            return $var2
+        } else {
+            return $var
+        }
+    }
+    
+    # define some header offsets
+    set ihsize 156
+    set p_suite 124
+    set p_suite2 [expr $p_suite + 2]
+    
+    set SU_HDR_LEN 114
+    set EX_HDR_LEN 1024
+    set SE_HDR_LEN 1020
+    set IM_HDR_LEN 1022
+    
+    set SU_HDR_START 0
+    set EX_HDR_START $SU_HDR_LEN
+    set SE_HDR_START [expr $EX_HDR_START+$EX_HDR_LEN]
+    set IM_HDR_START [expr $SE_HDR_START+$SE_HDR_LEN]
+    set IMG_HDR_START 0
+    
+    # define some variable-specific offsets
+    
+    set EX_ex_no 8
+    set EX_patname 97
+    set EX_patid 84
+    set EX_ex_typ 305
+    set EX_patage 122
+    set EX_patsex 126
+    set EX_hospname  10
+    set EX_ex_desc 282
+    
+    set SE_se_actual_dt 16
+    
+    set SE_se_desc 20
+    
+    set MR_slthick 26
+    set MR_dim_X 42
+    set MR_dim_Y 46
+    set MR_pixsize_X 50
+    set MR_pixsize_Y 54
+    set MR_dfov 34
+    set MR_scanspacing 116
+    set MR_loc 126
+    
+    # coordinate info
+    
+    set MR_ctr_R 130
+    set MR_ctr_A 134
+    set MR_ctr_S 138
+    set MR_norm_R 142
+    set MR_norm_A 146
+    set MR_norm_S 150
+    set MR_tlhc_R 154
+    set MR_tlhc_A 158
+    set MR_tlhc_S 162
+    set MR_trhc_R 166
+    set MR_trhc_A 170
+    set MR_trhc_S 174
+    set MR_brhc_R 178
+    set MR_brhc_A 182
+    set MR_brhc_S 186
+    
+    
+    puts "print_header V1.0"
+    set fs [file size  $filename]
+    if {$fs > 500000} {
+        set hs [expr $fs - 524288]
+        puts -nonewline "header_size "
+        puts $hs
+        puts "x_resolution = 512"
+        puts "y_resolution = 512"
+        puts "bytes_per_slice = 524288"
+        set aHeader(xdim) 512
+        set aHeader(ydim) 512
+        
+    }
+    if {$fs > 131072} {
+        set hs [expr $fs - 131072]
+        puts -nonewline "header_size = "
+        puts $hs
+        puts "x_resolution = 256"
+        puts "y_resolution = 256"
+        puts "bytes_per_slice = 131072"
+        set aHeader(xdim) 256
+        set aHeader(ydim) 256
+    }
+    set t [read $f 4]
+    if {[string compare $t "IMGF"] == 0} {
+        puts "image_type_text = genesis"
+    } else {
+        puts "image_type_text = signa"
+    }
+    
+    set variable_header_count [getvar_from_file $f $p_suite2 2 S*]
+
+    set exam_offset  [expr $variable_header_count + $EX_HDR_START]
+    set series_offset  [expr $variable_header_count + $SE_HDR_START]
+    set image_offset [expr $variable_header_count + $IM_HDR_START]
+    
+    #patient name
+    
+    set var [getvar_from_file $f [expr $exam_offset+$EX_patname] 25]
+    puts  -nonewline  "patient_name = "
+    puts $var
+    
+    # patient id
+    
+    set var [getvar_from_file $f [expr $exam_offset+$EX_patid]  13]
+    puts  -nonewline  "patient_id = "
+    puts $var
+    
+    # exam number
+    
+    set var [getvar_from_file $f [expr $exam_offset+$EX_ex_no] 2 S ]
+    puts  -nonewline  "exam_number = "
+    puts $var
+    
+    #hospital
+    
+    set hn2 [getvar_from_file $f [expr $exam_offset+$EX_hospname] 33 a33 ]
+    puts  -nonewline  "hospital_name = "
+    puts $hn2
+    
+    #exam description
+    
+    set var [getvar_from_file $f [expr $exam_offset+$EX_ex_desc]  23 a23 ]
+    puts  -nonewline  "exam_description = "
+    puts $var
+    
+    #series description
+    
+    set var [getvar_from_file $f [expr $series_offset+$SE_se_desc]  30 a30 ]
+    puts  -nonewline  "series_description = "
+    puts $var
+    
+    # patient age
+    
+    set var [getvar_from_file $f [expr $exam_offset+$EX_patage]  2 S]
+    puts  -nonewline  "patient_age = "
+    puts $var
+    
+    # patient sex
+    
+    set var [getvar_from_file $f [expr $exam_offset+$EX_patsex]  2 s ]
+    if {$t == 2} {
+        puts  "patient_sex = female"
+    }
+    if {$t == 1} {
+        puts  "patient_sex = male"
+    }
+    
+    #date 
+    set var [getvar_from_file $f [expr $series_offset+$SE_se_actual_dt]  4 I ]
+    set tm [clock format $var]
+    puts $tm
+    
+    #modality
+    
+    set var [getvar_from_file $f [expr $exam_offset+$EX_ex_typ]  3]
+    puts  -nonewline  "modality = "
+    puts $var
+    
+    # slice thickness
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_slthick]  4 f ]
+    puts  -nonewline  "thickness = "
+    puts $var
+    set aHeader(sliceThick) $var    
+    
+    # spacing
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_scanspacing]  4 f]
+    puts  -nonewline  "spacing = "
+    puts $var
+    set aHeader(sliceSpace) $var    
+    
+    
+    # pxel_X_size
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_pixsize_X]  4 f]
+    puts  -nonewline  "pixel_x_size = "
+    puts $var
+    set aHeader(xSpacing) $var
+    
+    
+    # pixel_Y_size
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_pixsize_Y]  4 f]
+    puts  -nonewline  "pixel_y_size = "
+    puts $var
+    set aHeader(ySpacing) $var
+    
+    # fov
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_dfov]  4 f ]
+    puts  -nonewline  "field_of_view = "
+    puts $var
+    
+    # location
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_loc]  4 f]
+    puts  -nonewline  "image_location = "
+    puts $var
+    
+    # coordinate info
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_ctr_R]  4 f ]
+    puts  -nonewline  "coord_center_r = "
+    puts $var
+    set aHeader(rC) $var
+
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_ctr_A]  4 f]
+    puts  -nonewline  "coord_center_a = "
+    puts $var
+    set aHeader(aC) $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_ctr_S]  4 f]
+    puts  -nonewline  "coord_center_s = "
+    puts $var
+    set aHeader(sC) $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_norm_R]  4 f]
+    puts  -nonewline  "coord_normal_r = "
+    puts $var
+
+    set var [getvar_from_file $f [expr $image_offset+$MR_norm_A]  4 f]
+    puts  -nonewline  "coord_normal_a  = "
+    puts $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_norm_S]  4 f]
+    puts  -nonewline  "coord_normal_s  = "
+    puts $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_tlhc_R]  4 f ]
+    puts  -nonewline  "coord_r_top_left  = "
+    puts $var
+    set aHeader(rTL) $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_tlhc_A]  4 f]
+    puts  -nonewline  "coord_a_top_left  = "
+    puts $var
+    set aHeader(aTL) $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_tlhc_S]  4 f]
+    binary scan  $t f var
+    puts  -nonewline  "coord_s_top_left  = "
+    puts $var
+    set aHeader(sTL) $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_trhc_R]  4 f]
+    puts  -nonewline  "coord_r_top_right  = "
+    puts $var
+    set aHeader(rTR) $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_trhc_A]  4 f]
+    puts  -nonewline  "coord_a_top_right  = "
+    puts $var
+    set aHeader(aTR) $var
+
+    set var [getvar_from_file $f [expr $image_offset+$MR_trhc_S] 4 f]
+    puts  -nonewline  "coord_s_top_right  = "
+    puts $var
+    set aHeader(sTR) $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_brhc_R] 4 f ]
+    puts  -nonewline  "coord_r_bottom_right  = "
+    puts $var
+    set aHeader(rBR) $var
+
+    set var [getvar_from_file $f [expr $image_offset+$MR_brhc_A]  4 f]
+    puts  -nonewline  "coord_a_bottom_right  = "
+    puts $var
+    set aHeader(aBR) $var
+    
+    set var [getvar_from_file $f [expr $image_offset+$MR_brhc_S]  4 f]
+    binary scan  $t f var
+    puts  -nonewline  "coord_s_bottom_right  = "
+    puts $var
+    set aHeader(sBR) $var
+}
 #-------------------------------------------------------------------------------
 # .PROC ReadHeader
+#
+# Calls the print_header program.
+# This routine is obsolete. It exists only
+# for the possible eventuality that ReadHeaderTCl does
+# not handle some type of file.
+#
 # .END
 #-------------------------------------------------------------------------------
 proc ReadHeader {image run utility tk} {
@@ -54,6 +377,7 @@ proc ReadHeader {image run utility tk} {
 			return ""
 		    } else {
 			set hdr $errmsg
+                        puts $errmsg
 		    }
 		}
 	} else {
@@ -66,6 +390,10 @@ proc ReadHeader {image run utility tk} {
 
 #-------------------------------------------------------------------------------
 # .PROC ParsePrintHeader
+# Parses result from the print_header program
+# This routine is obsolete. It exists only
+# for the possible eventuality that ReadHeaderTCl does
+# not handle some type of file.
 # .END
 #-------------------------------------------------------------------------------
 proc ParsePrintHeader {text aHeader} {
@@ -73,7 +401,7 @@ proc ParsePrintHeader {text aHeader} {
 	upvar $aHeader Header
 	set text "$text\n"
 
-	# These are commentys
+	# These are comments
 	# 'text' was read from the file
 	# This routine parses 'text' to set variables in the Header array.
 	# For each key, find it in the 'text', and set the corresponding
@@ -242,6 +570,7 @@ proc DumpHeader {aHeader} {
 
 #-------------------------------------------------------------------------------
 # .PROC GetHeaderInfo
+# Return an error message if files don't exist, else "".
 # .END
 #-------------------------------------------------------------------------------
 proc GetHeaderInfo {img1 num2 node tk} {
