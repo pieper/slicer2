b31327c297e564faf76f24f43d91f853b1ccb820
odonnell <odonnell>
2000-07-28 18:51:24 +0000

    auto comments and copyrights -- sorry if you have to recompile...

--- Base/tests/TestResize.tcl
+++ Base/tests/TestResize.tcl
@@ -7,7 +7,9 @@ vtkImageReader reader
 reader ReleaseDataFlagOff
 reader SetDataByteOrderToLittleEndian
 reader SetDataExtent 0 255 0 255 22 22
-reader SetFilePrefix "../../../vtkdata/fullHead/headsq"
+reader SetFilePrefix "../../../vtk/vtk3.0/vtkdata/fullHead/"
+#"../../../vtkdata/fullHead/headsq"
+
 reader SetDataMask 0x7fff
 
 set resizeExt "0 149 0 149 0 0"
