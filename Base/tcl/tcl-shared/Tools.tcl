9ca76365d28ef313b592bb381c18b3b87ee4cd8a
odonnell <odonnell>
2000-10-31 00:24:17 +0000

    comments

--- Base/tcl/tcl-shared/Tools.tcl
+++ Base/tcl/tcl-shared/Tools.tcl
@@ -1,3 +1,35 @@
+#=auto==========================================================================
+# Copyright (c) 2000 Surgical Planning Lab, Brigham and Women's Hospital
+#  
+# Direct all questions regarding this copyright to slicer@ai.mit.edu.
+# The following terms apply to all files associated with the software unless
+# explicitly disclaimed in individual files.   
+# 
+# The authors hereby grant permission to use, copy, (but NOT distribute) this
+# software and its documentation for any NON-COMMERCIAL purpose, provided
+# that existing copyright notices are retained verbatim in all copies.
+# The authors grant permission to modify this software and its documentation 
+# for any NON-COMMERCIAL purpose, provided that such modifications are not 
+# distributed without the explicit consent of the authors and that existing
+# copyright notices are retained in all copies. Some of the algorithms
+# implemented by this software are patented, observe all applicable patent law.
+# 
+# IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+# DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+# OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+# EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+# 'AS IS' BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+# MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+#===============================================================================
+# FILE:        Tools.tcl
+# PROCEDURES:  
+#   ToolUpdate
+#   ToolStub
+#==========================================================================auto=
 # Tools.tcl
 # 11/30/98 Peter C. Everett peverett@bwh.harvard.edu: Created
 
@@ -73,6 +105,12 @@ proc ToolBar { frame barname args } {
 	return $f
 }
 
+#-------------------------------------------------------------------------------
+# .PROC ToolUpdate
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc ToolUpdate { barname toolenter toolexit } {
 	global $barname
 
@@ -94,5 +132,11 @@ proc ToolUpdate { barname toolenter toolexit } {
 		}
 	}
 
+#-------------------------------------------------------------------------------
+# .PROC ToolStub
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc ToolStub { args } {
 	}
