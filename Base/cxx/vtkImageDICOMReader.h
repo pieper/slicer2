54cf24e99eb173c5bc398204fbfe6b62ec1838b0
odonnell <odonnell>
2000-11-06 21:05:25 +0000

    auto comment

--- Base/cxx/vtkImageDICOMReader.h
+++ Base/cxx/vtkImageDICOMReader.h
@@ -1,10 +1,37 @@
+/*=auto=========================================================================
+Copyright (c) 2000 Surgical Planning Lab, Brigham and Women's Hospital
+ 
+Direct all questions on this copyright to slicer@ai.mit.edu.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files.   
+
+The authors hereby grant permission to use, copy (but NOT distribute) this
+software and its documentation for any NON-COMMERCIAL purpose, provided
+that existing copyright notices are retained verbatim in all copies.
+The authors grant permission to modify this software and its documentation 
+for any NON-COMMERCIAL purpose, provided that such modifications are not 
+distributed without the explicit consent of the authors and that existing
+copyright notices are retained in all copies. Some of the algorithms
+implemented by this software are patented, observe all applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+'AS IS' BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+=========================================================================auto=*/
 /*=========================================================================
 
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkImageDICOMReader.h,v $
   Language:  C++
-  Date:      $Date: 2000/10/24 17:45:56 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2000/11/06 21:05:26 $
+  Version:   $Revision: 1.3 $
   Thanks:    Thanks to C. Charles Law who developed this class.
 
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
