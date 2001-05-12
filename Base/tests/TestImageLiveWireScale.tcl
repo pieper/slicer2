53ab9ee2aa6755cf08d9e25b2cd33584c3974ef2
odonnell <odonnell>
2001-05-12 12:49:07 +0000

    scaling is used in PhaseWire

---
+++ Base/tests/TestImageLiveWireScale.tcl
@@ -0,0 +1,60 @@
+catch {load vtktcl}
+source vtkImageInclude.tcl
+
+# Image pipeline
+
+vtkImageReader reader
+reader ReleaseDataFlagOff
+reader SetDataByteOrderToLittleEndian
+reader SetDataExtent 0 255 0 255 1 93
+reader SetFilePrefix "../../../vtkdata/fullHead/headsq"
+reader SetDataMask 0x7fff
+
+vtkImageLiveWireScale norm2
+norm2 SetInput [reader GetOutput]
+norm2 SetScaleFactor 255
+#norm2 SetTransformationFunctionToInverseLinearRamp
+norm2 SetTransformationFunctionToOneOverX
+
+vtkImageLiveWireScale norm
+#norm SetInput [reader GetOutput]
+norm SetInput [norm2 GetOutput]
+
+# test various normalization methods.
+
+# max val filter will output
+norm SetScaleFactor 255
+# method:
+#norm SetTransformationFunctionToInverseLinearRamp
+#norm SetTransformationFunctionToOneOverX
+
+vtkPoints points
+set numPoints 30
+points SetNumberOfPoints $numPoints
+
+for {set i 0} {$i < $numPoints} {incr i} {
+    #points SetPoint $i $i $i 0
+    points SetPoint $i 100 $i 0
+}
+
+norm SetLookupPoints points
+#norm SetUseLookupTable 1
+norm SetUseGaussianLookup 1
+
+vtkImageViewer viewer
+viewer SetInput [norm GetOutput]
+viewer SetZSlice 22
+viewer SetColorWindow 300
+viewer SetColorLevel 100
+
+#make interface
+source WindowLevelInterface.tcl
+
+
+
+
+
+
+
+
+
