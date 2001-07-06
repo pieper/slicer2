2440a57c77da30aa4c3acf231894ba19af79f9b7
tanacs <tanacs>
2001-07-06 19:42:10 +0000

    Keep/delete subvolumes in labelmaps.

---
+++ Base/cxx/vtkImageLabelVOI.cxx
@@ -0,0 +1,215 @@
+/*=auto=========================================================================
+
+(c) Copyright 2001 Massachusetts Institute of Technology
+
+Permission is hereby granted, without payment, to copy, modify, display 
+and distribute this software and its documentation, if any, for any purpose, 
+provided that the above copyright notice and the following three paragraphs 
+appear on all copies of this software.  Use of this software constitutes 
+acceptance of these terms and conditions.
+
+IN NO EVENT SHALL MIT BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
+INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE 
+AND ITS DOCUMENTATION, EVEN IF MIT HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+MIT SPECIFICALLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTIES INCLUDING, 
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR 
+A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+
+THE SOFTWARE IS PROVIDED "AS IS."  MIT HAS NO OBLIGATION TO PROVIDE 
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================auto=*/
+#include "vtkImageLabelVOI.h"
+#include "vtkObjectFactory.h"
+
+//------------------------------------------------------------------------------
+vtkImageLabelVOI* vtkImageLabelVOI::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkImageLabelVOI");
+  if(ret)
+    {
+    return (vtkImageLabelVOI*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkImageLabelVOI;
+}
+
+
+//----------------------------------------------------------------------------
+// Description:
+// Constructor sets default values
+vtkImageLabelVOI::vtkImageLabelVOI()
+{
+  this->c1x = this->c1y = this->c1z = 0;
+  this->c2x = this->c2y = this->c2z = 0;
+}
+
+//----------------------------------------------------------------------------
+// Description:
+// This templated function executes the filter for any type of data.
+template <class T>
+static void vtkImageLabelVOIExecute(vtkImageLabelVOI *self,
+				     vtkImageData *inData, T *inPtr,
+				     vtkImageData *outData, 
+				     int outExt[6], int id)
+{
+  T *outPtr = (T *)outData->GetScalarPointerForExtent(outExt);
+  // looping
+  int outIncX, outIncY, outIncZ;
+  int idxX, idxY, idxZ, minX, minY, minZ, maxX, maxY, maxZ;
+  
+  // Get increments to march through data 
+  outData->GetContinuousIncrements(outExt, outIncX, outIncY, outIncZ);
+  
+  int c1[3], c2[3];
+  self->GetCorner1(c1);
+  self->GetCorner2(c2);
+  
+  if(c1[0] < c2[0])
+    {
+      minX = c1[0];
+      maxX = c2[0];
+    }
+  else
+    {
+      minX = c2[0];
+      maxX = c1[0];
+    }
+  
+  if(c1[1] < c2[1])
+    {
+      minY = c1[1];
+      maxY = c2[1];
+    }
+  else
+    {
+      minY = c2[1];
+      maxY = c1[1];
+    }
+  
+  if(c1[2] < c2[2])
+    {
+      minZ = c1[2];
+      maxZ = c2[2];
+    }
+  else
+    {
+      minZ = c2[2];
+      maxZ = c1[2];
+    }
+  
+  int method = self->GetMethod();
+  // Loop through output pixels
+  for (idxZ = outExt[4]; idxZ <= outExt[5]; idxZ++)
+    {
+      for (idxY = outExt[2]; !self->AbortExecute && idxY <= outExt[3]; idxY++)
+	{
+	  for (idxX = outExt[0]; idxX <= outExt[1]; idxX++)
+	    {
+	      if((idxX >= minX) && (idxX <= maxX)
+		 && (idxY >= minY) && (idxY <= maxY)
+		 && (idxZ >= minZ) && (idxZ <= maxZ))
+		{
+		  if(method == 0)
+		    *outPtr = *inPtr;
+		  else
+		    *outPtr = (T)0;
+		}
+	      else
+		{
+		  if(method == 0)
+		    *outPtr = (T)0;
+		  else
+		    *outPtr = *inPtr;
+		}
+	      
+	      outPtr++;
+	      inPtr++;
+	    }
+	  outPtr += outIncY;
+	  inPtr += outIncY;
+	}
+      outPtr += outIncZ;
+      inPtr += outIncZ;
+    }
+}
+
+//----------------------------------------------------------------------------
+// Description:
+// This method is passed a input and output data, and executes the filter
+// algorithm to fill the output from the input.
+// It just executes a switch statement to call the correct function for
+// the datas data types.
+void vtkImageLabelVOI::ThreadedExecute(vtkImageData *inData, 
+				       vtkImageData *outData,
+				       int outExt[6], int id)
+{
+  // Check Single component
+  int x1;
+  x1 = inData->GetNumberOfScalarComponents();
+  if (x1 != 1) {
+    vtkErrorMacro(<<"Input has "<<x1<<" instead of 1 scalar component.");
+    return;
+  }
+  
+  void *inPtr = inData->GetScalarPointerForExtent(outExt);
+  
+  switch (inData->GetScalarType())
+    {
+    case VTK_DOUBLE:
+      vtkImageLabelVOIExecute(this, inData, (double *)(inPtr), 
+			      outData, outExt, id);
+      break;
+    case VTK_FLOAT:
+      vtkImageLabelVOIExecute(this, inData, (float *)(inPtr), 
+			      outData, outExt, id);
+      break;		
+    case VTK_LONG:
+      vtkImageLabelVOIExecute(this, inData, (long *)(inPtr), 
+			      outData, outExt, id);
+      break;
+    case VTK_UNSIGNED_LONG:
+      vtkImageLabelVOIExecute(this, inData, (unsigned long *)(inPtr), 
+			      outData, outExt, id);
+      break;
+    case VTK_INT:
+      vtkImageLabelVOIExecute(this, inData, (int *)(inPtr), 
+			      outData, outExt, id);
+      break;
+    case VTK_UNSIGNED_INT:
+      vtkImageLabelVOIExecute(this, inData, (unsigned int *)(inPtr), 
+			      outData, outExt, id);
+      break;
+    case VTK_SHORT:
+      vtkImageLabelVOIExecute(this, inData, (short *)(inPtr), 
+			      outData, outExt, id);
+      break;
+    case VTK_UNSIGNED_SHORT:
+      vtkImageLabelVOIExecute(this, inData, (unsigned short *)(inPtr), 
+			      outData, outExt, id);
+      break;
+    case VTK_CHAR:
+      vtkImageLabelVOIExecute(this, inData, (char *)(inPtr), 
+			      outData, outExt, id);
+      break;
+    case VTK_UNSIGNED_CHAR:
+      vtkImageLabelVOIExecute(this, inData, (unsigned char *)(inPtr), 
+			      outData, outExt, id);
+      break;
+    default:
+      vtkErrorMacro(<< "Execute: Unknown input ScalarType");
+      return;
+    }
+}
+
+void vtkImageLabelVOI::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkImageToImageFilter::PrintSelf(os,indent);
+  
+  os << indent << "Corner1: " << this->c1x << " " << this->c1y << " " << this->c1z << "\n";
+  os << indent << "Corner2: " << this->c2x << " " << this->c2y << " " << this->c2z << "\n";
+}
+
---
+++ Base/cxx/vtkImageLabelVOI.h
@@ -0,0 +1,91 @@
+/*=auto=========================================================================
+
+(c) Copyright 2001 Massachusetts Institute of Technology
+
+Permission is hereby granted, without payment, to copy, modify, display 
+and distribute this software and its documentation, if any, for any purpose, 
+provided that the above copyright notice and the following three paragraphs 
+appear on all copies of this software.  Use of this software constitutes 
+acceptance of these terms and conditions.
+
+IN NO EVENT SHALL MIT BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
+INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE 
+AND ITS DOCUMENTATION, EVEN IF MIT HAS BEEN ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE.
+
+MIT SPECIFICALLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTIES INCLUDING, 
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR 
+A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+
+THE SOFTWARE IS PROVIDED "AS IS."  MIT HAS NO OBLIGATION TO PROVIDE 
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================auto=*/
+// .NAME vtkImageLabelVOI -  Select label volume of interest
+// 
+// .SECTION Description
+//
+// vtkImageLabelVOI will remove label values outside of a subvolume.
+// This is used for editing of labelmaps.
+//
+
+#ifndef __vtkImageLabelVOI_h
+#define __vtkImageLabelVOI_h
+
+#include "vtkImageToImageFilter.h"
+
+class VTK_EXPORT vtkImageLabelVOI : public vtkImageToImageFilter
+{
+public:
+	static vtkImageLabelVOI *New();
+  vtkTypeMacro(vtkImageLabelVOI,vtkImageToImageFilter);
+	void PrintSelf(ostream& os, vtkIndent indent);
+
+  vtkGetMacro(Method, int);
+  vtkSetMacro(Method, int);
+
+  void SetCorner1(int c1x, int c1y, int c1z)
+  {
+    this->c1x = c1x; this->c1y = c1y; this->c1z = c1z;
+  }
+
+  void SetCorner2(int c2x, int c2y, int c2z)
+  {
+    this->c2x = c2x; this->c2y = c2y; this->c2z = c2z;
+  }
+
+  void GetCorner1(int c1[3])
+  {
+    c1[0] = this->c1x;
+    c1[1] = this->c1y;
+    c1[2] = this->c1z;
+  }
+
+  void GetCorner2(int c2[3])
+  {
+    c2[0] = this->c2x;
+    c2[1] = this->c2y;
+    c2[2] = this->c2z;
+  }
+
+protected:
+	vtkImageLabelVOI();
+	~vtkImageLabelVOI() {};
+	vtkImageLabelVOI(const vtkImageLabelVOI&) {};
+	void operator=(const vtkImageLabelVOI&) {};
+
+  int c1x;
+  int c1y;
+  int c1z;
+  int c2x;
+  int c2y;
+  int c2z;
+
+  int Method;
+
+	void ThreadedExecute(vtkImageData *inData, vtkImageData *outData, 
+		int extent[6], int id);
+};
+
+#endif
+
