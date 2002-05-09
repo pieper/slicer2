825748b564a0e32c41831ef5d7935e85c5d96608
samson <samson>
2002-05-09 19:28:30 +0000

    Looks like a good test.

--- Base/tests/TestImageWeightedSum.tcl
+++ Base/tests/TestImageWeightedSum.tcl
@@ -14,20 +14,34 @@ vtkImageMagnify mag
   mag SetInput [reader GetOutput]
   mag SetMagnificationFactors 4 4 1
 
+vtkImageThreshold th
+th SetInput [mag GetOutput]
+th SetReplaceIn 1
+th SetReplaceOut 1
+th ThresholdBetween -1000 1000
+th SetOutValue 0
+th SetInValue 2000
+
 vtkImageCast cast
 cast SetInput [mag GetOutput]
 cast SetOutputScalarTypeToFloat
 
+vtkImageCast cast2
+cast2 SetInput [th GetOutput]
+cast2 SetOutputScalarTypeToFloat
+
 vtkImageWeightedSum sum
 sum SetInput 0 [cast GetOutput]
-sum SetInput 1 [cast GetOutput]
+sum SetInput 1 [cast2 GetOutput]
+sum SetWeightForInput 0 10
+sum SetWeightForInput 1 4
 puts "set inputs to sum"
 
 vtkImageViewer viewer
 viewer SetInput [sum GetOutput]
 viewer SetZSlice 22
-viewer SetColorWindow 2000
-viewer SetColorLevel 1000
+viewer SetColorWindow 1819
+viewer SetColorLevel 939
 
 puts "grabbed sum's output"
 
