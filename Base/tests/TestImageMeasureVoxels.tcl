d4120f9b338c7c8bca8fe0e41bab70e3e75a49c6
samson <samson>
2002-05-09 15:10:48 +0000

    This seems to work reasonably, it spits out the file TestImageMeasureVoxels.tst.
    There does seem to be some round-off error : that is the total volume
    isn't exactly what it should be.

--- Base/tests/TestImageMeasureVoxels.tcl
+++ Base/tests/TestImageMeasureVoxels.tcl
@@ -1,19 +1,26 @@
-catch {load vtktcl}
-source vtkImageInclude.tcl
+package require vtk
+package require vtkSlicerBase
 
 # Image pipeline
 
 vtkImageReader reader
-reader ReleaseDataFlagOff
-reader SetDataByteOrderToLittleEndian
-reader SetDataExtent 0 255 0 255 1 93
-reader SetFilePrefix "../../../vtkdata/fullHead/headsq"
-reader SetDataMask 0x7fff
+  reader ReleaseDataFlagOff
+  reader SetDataByteOrderToLittleEndian
+  reader SetDataExtent 0 63 0 63 1 93
+  reader SetFilePrefix ${VTK_DATA_ROOT}/Data/headsq/quarter
+  reader SetDataMask 0x7fff
+
+vtkImageThreshold thresh
+  thresh SetInput [reader GetOutput]
+  thresh SetReplaceIn 1
+  thresh SetReplaceOut 1
+  thresh SetInValue 4000
+  thresh SetOutValue 0
+  thresh ThresholdBetween 0 10
 
 vtkImageMeasureVoxels measure
-measure SetInput [reader GetOutput]
+measure SetInput [thresh GetOutput]
 measure SetFileName TestImageMeasureVoxels.txt
-
 measure Update
 
 puts "results are in the file TestImageMeasureVoxels.txt"
