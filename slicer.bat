ad6185c576a2942e82fdd20c4975ec433db6f989
pieper <pieper>
2002-05-10 14:02:25 +0000

    added documentation and a -test option to easily launch tests in windows

--- slicer.bat
+++ slicer.bat
@@ -1,25 +1,54 @@
+@echo off
 
-set TCL_DIR=.
-set ITCL_LIBRARY=c:\pieper\bwh\slicer\Tcl-8.3\lib\itcl3.2
+::
+:: startup batch file for Slicer2 on Windows
+::
 
+:: locations:
+set TCL_BIN_DIR=c:\pieper\bwh\slicer\Tcl-8.3\bin
+set VTK_BIN_DIR=c:\downloads\vtk40\VTK-build\bin\debug
+
+:: Windows system paths - fill these in with values appropriate for system
+:: - these are the locations with the .dll files you want to use
+:: - the Path entries use BACKSLASHES separated by SEMICOLONS
+::
+:: first the vtk
 set Path=Base\builds\Win32VC7\bin\debug;%Path%
-set Path=c:\downloads\vtk40\VTK-build\bin\debug;%Path%
-set Path=c:\pieper\bwh\slicer\Tcl-8.3\bin;%Path%
+:: then vtkSlicerBase
+set Path=%VTK_BIN_DIR%;%Path%
+:: then tcl and tk
+set Path=%TCL_BIN_DIR%;%Path%
 
+:: Tcl library paths for packages.
+:: - these locations contain pkgIndex.tcl files
+:: - TCLLIBPATH entries use FORWARD SLASHES and are separated by SPACES
 set TCLLIBPATH=Base/Wrapping/Tcl/vtkSlicerBase %TCLLIBPATH%
 set TCLLIBPATH=c:/downloads/vtk40/VTK/Wrapping/Tcl %TCLLIBPATH%
 set TCLLIBPATH=Base/tcl %TCLLIBPATH%
 
+set ITCL_LIBRARY=c:\pieper\bwh\slicer\Tcl-8.3\lib\itcl3.2
+
 set VTK_DATA_ROOT=c:/downloads/vtk40/VTKData
 
-start c:\pieper\bwh\slicer\Tcl-8.3\bin/wish83.exe Base/tcl/Go.tcl %1 %2 %3 %4 %5 %6 %7 %8 %9
+IF .%1==.-test GOTO test
 
-rem ####################### for testing ##################
-rem start c:\downloads\vtk40\VTK-build\bin\debug\vtk.exe Base/tcl/Go.tcl %1 %2 %3 %4 %5 %6 %7 %8 %9
+start wish83.exe Base/tcl/Go.tcl %1 %2 %3 %4 %5 %6 %7 %8 %9
 
-rem start c:\downloads\vtk40\VTK-build\bin\debug\vtk.exe c:/pieper/test.tcl %1
-rem
-rem #######################             ##################
+exit
+
+:: ####################### for testing ##################
+
+:: launch slicer from pre-linked vtk rather than loading packages
+:: from dlls
+
+:: start %VTK_BIN_DIR%\vtk.exe Base/tcl/Go.tcl %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:test
+:: run just a test script with tkcon
+::
+start %VTK_BIN_DIR%\vtk.exe Base/tcl/tcl-shared/TestUtil.tcl %2 %3 %4 %5 %6 %7 %8 %9
+::
+:: #######################             ##################
 
 exit
 
