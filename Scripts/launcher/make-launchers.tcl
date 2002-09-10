79ee4579b1e3a4a4e4a396776389534bf4b521a2
pieper <pieper>
2002-09-10 12:39:44 +0000

    fix to handle bug in starpack

--- Scripts/launcher/make-launchers.tcl
+++ Scripts/launcher/make-launchers.tcl
@@ -25,5 +25,8 @@ exec sdx wrap slicer2 -runtime starkit-200209/tclkit-solaris-sparc
 file rename -force slicer2 ../../slicer2-solaris-sparc
 
 puts "making win32..."
-exec sdx wrap slicer2.exe -runtime starkit-200209/tclkit-win32.exe
-file rename -force slicer2.exe ../../slicer2-win32.exe
+#exec sdx wrap slicer2.exe -runtime starkit-200209/tclkit-win32.exe
+#file rename -force slicer2.exe ../../slicer2-win32.exe
+puts "NOTE: the windows starpack method doesn't pass arguments correctly."
+puts "Use \"freewrap.exe slicer2-win32.tcl\" to create the launcher"
+puts "and copy it up to the top level (must be done on windows)."
