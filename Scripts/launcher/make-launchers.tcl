e798fa91cad23999a97a09315da705b8113d0759
nicole <nicole>
2002-10-16 18:44:11 +0000

    Inserted a variable name for the version of the starkit, to make updates a bit more easy, esp for windows bug fix.

--- Scripts/launcher/make-launchers.tcl
+++ Scripts/launcher/make-launchers.tcl
@@ -15,18 +15,19 @@ set __comment {
 
 }
 
+set starkitRelease "-200209"
 
 puts "making linux..."
-exec tclkit starkit-200209/sdx.kit wrap slicer2 -runtime starkit-200209/tclkit-linux-x86
+exec tclkit starkit${starkitRelease}/sdx.kit wrap slicer2 -runtime starkit${starkitRelease}/tclkit-linux-x86
 file rename -force slicer2 ../../slicer2-linux-x86
 
 puts "making solaris..."
-exec tclkit starkit-200209/sdx.kit wrap slicer2 -runtime starkit-200209/tclkit-solaris-sparc
+exec tclkit starkit${starkitRelease}/sdx.kit wrap slicer2 -runtime starkit${starkitRelease}/tclkit-solaris-sparc
 file rename -force slicer2 ../../slicer2-solaris-sparc
 
 puts "making win32..."
-#exec  tclkit starkit-200209/sdx.kit wrap slicer2.exe -runtime starkit-200209/tclkit-win32.exe
-#file rename -force slicer2.exe ../../slicer2-win32.exe
-puts "NOTE: the windows starpack method doesn't pass arguments correctly."
+# exec  tclkit starkit${starkitRelease}/sdx.kit wrap slicer2.exe -runtime starkit${starkitRelease}/tclkit-win32.exe
+# file rename -force slicer2.exe ../../slicer2-win32.exe
+puts "NOTE: the windows starpack method doesn't pass arguments correctly with version 200209, please update, but until then:"
 puts "Use \"freewrap.exe slicer2-win32.tcl\" to create the launcher"
 puts "and copy it up to the top level (must be done on windows)."
