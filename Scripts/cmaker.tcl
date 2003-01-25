ad092e24539ff4394fe208aa3091a8ab00a603e2
pieper <pieper>
2003-01-25 22:28:42 +0000

    new script to do a toplevel make of slicerbase and modules

---
+++ Scripts/cmaker.tcl
@@ -0,0 +1,54 @@
+
+#
+# make slicerbase and all the modules using the same configurations
+# - sp 2003-01-25
+# TODO: add unix building
+#
+
+set SLICER_HOME c:/pieper/bwh/slicer2/latest/slicer2
+set BUILD Win32VC7
+set GENERATOR "Visual Studio 7" 
+set TARGETS {
+        Base
+        Modules/vtkFastMarching
+        Modules/vtkFluxDiffusion
+        Modules/vtkFreeSurferReaders
+        Modules/vtkITK
+        Modules/vtkSlicerTensors
+    }
+
+
+set VTK_ARG1 -DUSE_BUILT_VTK:BOOL=ON
+set VTK_ARG2 -DVTK_BINARY_PATH:PATH=c:/downloads/vtk/latest/Win32VC7
+set SLICER_ARG1 -DVTKSLICERBASE_SOURCE_DIR:PATH=$SLICER_HOME/Base
+set SLICER_ARG2 -DVTKSLICERBASE_BUILD_DIR:PATH=$SLICER_HOME/Base/builds/$BUILD
+set SLICER_ARG3 -DVTKSLICERBASE_BUILD_LIB:PATH=$SLICER_HOME/Base/builds/$BUILD/bin/debug/vtkSlicerBase.lib
+
+
+foreach target $TARGETS {
+
+    puts "processing $target..."
+
+    set build $SLICER_HOME/$target/builds/$BUILD 
+    catch "file mkdir $build"
+    cd $build
+    puts "enter directory $build..."
+
+    puts "running cmake ..."
+    exec cmake $SLICER_HOME/$target -G$GENERATOR \
+        $VTK_ARG1 $VTK_ARG2 \
+        $SLICER_ARG1 $SLICER_ARG2 $SLICER_ARG3
+
+    if { $target == "Base" } {
+        set sln VTKSLICER.sln
+    } else {
+        set sln [string toupper [file tail $target]].sln
+    }
+    puts "running: devenv $sln /build debug"
+    exec devenv $sln /build debug
+
+}
+
+
+exit
+
