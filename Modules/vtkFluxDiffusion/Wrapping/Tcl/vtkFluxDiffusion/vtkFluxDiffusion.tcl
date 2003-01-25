6a3eb2d698dbb671857d3a544cc13a8fa6f379db
pieper <pieper>
2003-01-25 22:52:25 +0000

    added variable setting to tell Go.tcl to load FluxDiffusion as a module

--- Modules/vtkFluxDiffusion/Wrapping/Tcl/vtkFluxDiffusion/vtkFluxDiffusion.tcl
+++ Modules/vtkFluxDiffusion/Wrapping/Tcl/vtkFluxDiffusion/vtkFluxDiffusion.tcl
@@ -10,4 +10,9 @@ if {[info commands vtkAnisoGaussSeidel] != "" ||
     global PACKAGE_DIR_VTKFLUXDIFFUSION
     source  $PACKAGE_DIR_VTKFLUXDIFFUSION/../../../tcl/FluxDiffusion.tcl
     package provide vtkFluxDiffusion 1.0
+    # add this module's name to the list of custom modules in order 
+    # to have it's init procedure get called, @ModuleName@Init will be 
+    # called by the Slicer Base code
+    global Module
+    lappend Module(customModules) FluxDiffusion
 }
