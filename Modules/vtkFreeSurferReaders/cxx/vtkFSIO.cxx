7d6a740518c2381eac722d4d2c225e266d2e0d29
kteich <kteich>
2003-02-19 23:55:08 +0000

    added a few more read funcs

--- Modules/vtkFreeSurferReaders/cxx/vtkFSIO.cxx
+++ Modules/vtkFreeSurferReaders/cxx/vtkFSIO.cxx
@@ -3,13 +3,39 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkFSIO.cxx,v $
   Language:  C++
-  Date:      $Date: 2002/10/07 21:08:44 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 2003/02/19 23:55:08 $
+  Version:   $Revision: 1.2 $
 
 =========================================================================*/
 
 #include "vtkFSIO.h"
 #include "vtkByteSwap.h"
+
+int vtkFSIO::ReadShort (FILE* iFile, short& oShort) {
+
+  short s = 0;
+  int result ;
+
+  // Read an short. Swap if we need to. Return the value.
+  result = fread (&s, sizeof(short), 1, iFile);
+  vtkByteSwap::Swap2BE (&s);
+  oShort = s;
+
+  return result;
+}
+
+int vtkFSIO::ReadInt (FILE* iFile, int& oInt) {
+
+  int i = 0;
+  int result ;
+
+  // Read an int. Swap if we need to. Return the value.
+  result = fread (&i, sizeof(int), 1, iFile);
+  vtkByteSwap::Swap4BE (&i);
+  oInt = i;
+
+  return result;
+}
 
 int vtkFSIO::ReadInt3 (FILE* iFile, int& oInt) {
 
--- Modules/vtkFreeSurferReaders/cxx/vtkFSIO.h
+++ Modules/vtkFreeSurferReaders/cxx/vtkFSIO.h
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkFSIO.h,v $
   Language:  C++
-  Date:      $Date: 2002/10/07 21:08:44 $
-  Version:   $Revision: 1.1 $
+  Date:      $Date: 2003/02/19 23:55:11 $
+  Version:   $Revision: 1.2 $
 
 =========================================================================*/
 // .NAME vtkFSIO - Some IO functions for irregular FreeSurface files.
@@ -25,9 +25,11 @@ class  vtkFSIO {
   // These use FILE types instead of file streams for no good reason,
   // simply because the old code from which this is adapted is
   // C-based.
+  static int ReadShort (FILE* iFile, short& oShort);
+  static int ReadInt (FILE* iFile, int& oInt);
   static int ReadInt3 (FILE* iFile, int& oInt);
   static int ReadInt2 (FILE* iFile, int& oInt);
-  static int ReadFloat (FILE* iFile, float& oInt);
+  static int ReadFloat (FILE* iFile, float& oFloat);
 
 };
 
