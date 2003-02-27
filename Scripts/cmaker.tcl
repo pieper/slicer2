47607029df7159df38d31caeac12177ac199fcb4
nicole <nicole>
2003-02-27 21:21:09 +0000

    Updates for SunOS, automatically adds all Modules/vtk* directories to target list for making

--- Scripts/cmaker.tcl
+++ Scripts/cmaker.tcl
@@ -1,4 +1,4 @@
-
+#!/local/os/bin/tclsh
 #
 # make slicerbase and all the modules using the same configurations
 # - sp 2003-01-25
@@ -6,7 +6,11 @@
 
 switch $tcl_platform(os) {
     "SunOS" {
-        # TODO
+        set SLICER_HOME /projects/birn/nicole/slicer2
+        set VTK_BINARY_PATH /projects/birn/slicer2/Lib/solaris8/vtk/VTK-build
+        set BUILD solaris8
+        set VTKSLICERBASE_BUILD_LIB $SLICER_HOME/Base/builds/$BUILD/bin/vtkSlicerBase.so
+        set GENERATOR "Unix Makefiles"
     }
     "Linux" {
         set SLICER_HOME /home/nicole/slicer2
@@ -29,21 +33,38 @@ switch $tcl_platform(os) {
 }
 
 
+set TARGETS { Base }
 
-set TARGETS {
-        Base
-        Modules/vtkFastMarching
-        Modules/vtkFluxDiffusion
-        Modules/vtkFreeSurferReaders
-        Modules/vtkITK
+# Add any modules here, as found in the Modules/vtk* directories of SLICER_HOME
+regsub -all {\\} $SLICER_HOME / slicer_home
+set baseModulePath ${slicer_home}/Modules
+set modulePaths ""
+catch {set modulePaths [glob $baseModulePath/vtk*]}
+
+foreach dir $modulePaths {
+    # get the module name
+    if {[regexp "$baseModulePath/(\.\*)" $dir match moduleName] == 1} {
+        # if it's not the custom one, append it to the list of targets
+        if {[string first Custom $moduleName] == -1} {
+            if {[file isdirectory ${baseModulePath}/${moduleName}] == 1} {
+                puts "Adding module to target list: ${moduleName}"
+                lappend TARGETS Modules/${moduleName}
+            }
     }
+    }
+}
+# puts "Using TARGETS = ${TARGETS}"
 
-
+# use an already built version of vtk
 set VTK_ARG1 "-DUSE_BUILT_VTK:BOOL=ON"
 set VTK_ARG2 "-DVTK_BINARY_PATH:PATH=$VTK_BINARY_PATH"
+# in order to bypass warnings about Source files
 set VTK_ARG3 "-DCMAKE_BACKWARDS_COMPATIBILITY:STRING=1.2"
+# explicitly specify the compiler used to compile the version of vtk that 
+# we link with
 set VTK_ARG4 "-DCMAKE_CXX_COMPILER:STRING=g++"
 set VTK_ARG5 "-DCMAKE_CXX_COMPILER_FULLPATH:FILEPATH=/usr/bin/g++"
+# make sure to generate shared libraries
 set VTK_ARG6 "-DBUILD_SHARED_LIBS:BOOL=ON"
 
 set SLICER_ARG1 "-DVTKSLICERBASE_SOURCE_DIR:PATH=$SLICER_HOME/Base"
