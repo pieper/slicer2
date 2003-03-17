0a011a45520f929de80f17f370f561d5a4065131
pieper <pieper>
2003-03-17 03:03:37 +0000

    special function to init static class members and maybe other stuff for Mac OSX

---
+++ Base/cxx/vtkOSXSlicerBaseTclInit.c
@@ -0,0 +1,9 @@
+//-----  This hack needed to compile using gcc3 on OSX until new stdc++.dylib
+// grabbed from vtk4.2 Rendering
+#include <stdio.h>
+void oft_initSlicerBaseOSXInit() 
+{
+  extern void _ZNSt8ios_base4InitC4Ev();
+  _ZNSt8ios_base4InitC4Ev();
+}
+
