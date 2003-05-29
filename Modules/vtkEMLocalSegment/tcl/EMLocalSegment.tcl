5d8f835cc972afa9d6d9ce17d2b7b0833b630788
pieper <pieper>
2003-05-29 12:40:11 +0000

    new comment

--- Modules/vtkEMLocalSegment/tcl/EMLocalSegment.tcl
+++ Modules/vtkEMLocalSegment/tcl/EMLocalSegment.tcl
@@ -1,2 +1,7 @@
 
+#
+# This is a placeholder file while EM segment transitions from a 
+# part of the SlicerBase to being it's own Module - the real work is still done in 
+# Base/tcl/tcl-modules/EMSegment.tcl
+#
 
