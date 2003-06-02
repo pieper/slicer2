ac6bbc570ad411dfc0152c14b3cc4df25bf10cf9
pieper <pieper>
2003-06-02 16:35:02 +0000

    grabbing seems not to work on all linux window managers

--- Base/tcl/tcl-modules/Volumes/VolDicom.tcl
+++ Base/tcl/tcl-modules/Volumes/VolDicom.tcl
@@ -552,7 +552,7 @@ proc FindDICOM2 { StartDir AddDir Pattern } {
         pack [label $w.label -textvariable ::DICOMlabel] 
         pack [button $w.cancel -text "Stop Looking" -command {set ::DICOMabort "true"} ]
         update ;# make sure the window exists before grabbing events
-        grab -global $w
+        catch "grab -global $w"
     }
     
     vtkDCMParser parser
