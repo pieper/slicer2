ac966ea9104ea343c15462a9b6346e903323e0fe
pieper <pieper>
2003-08-26 22:12:39 +0000

    making iSlicer into a tcl package that loas automatically

---
+++ Modules/iSlicer/Wrapping/Tcl/iSlicer/iSlicer.tcl
@@ -0,0 +1,22 @@
+package require vtk
+package require Iwidgets
+
+#
+# rely on the fact that a class loaded from the shared
+# library is uniquely available through this module
+#
+
+if { [info commands isvolume] == "" } {
+    global PACKAGE_DIR_ISLICER
+    package provide iSlicer 1.0
+
+    # source the Module's tcl file that contains it's init procedure
+    set files {
+        collapsablewidget.itk imrml.tcl ismodel.tcl isvolume.tcl
+        is3d.tcl isregistration.tcl evaluation-movies.tcl isbutton.tcl 
+        istask.tcl }
+        
+    foreach f $files {
+        source $PACKAGE_DIR_ISLICER/../../../tcl/$f
+    }
+}
