f5982d96c07209d08ef2faf75d76e9e20dc6787e
pieper <pieper>
2003-11-14 19:09:30 +0000

    upgraded to handle smaller cluster environments with better feedback

--- Modules/vtkMIRIADSegment/scripts/cluster.tcl
+++ Modules/vtkMIRIADSegment/scripts/cluster.tcl
@@ -5,30 +5,56 @@ exec tclsh "$0" "$@"
 proc echo {args} {puts "$args"}
 
 
-proc init {} {
+proc init { {job_limit 200} } {
+
+    switch $::env(HOSTNAME) { 
+        "crayon.rocksclusters.org" {
+            set ::ids [exec ls -1 /home/pieper/data/MIRIAD/Project_0002]
+            set racks {0 0}
+            set rows {2 15}
+            set ::archive  "/home/pieper/data/MIRIAD/Project_0002"
+        }
+        "rockstar.rocksclusters.org" {
+            set ::ids [exec ssh gpop.bwh.harvard.edu \
+                ls -1 /nas/nas0/pieper/data/MIRIAD/Project_0002]
+            set racks {0 3}
+            set rows {2 15}
+            set ::archive  "gpop.bwh.harvard.edu:/nas/nas0/pieper/data/MIRIAD/Project_0002"
+
+        }
+    }
+
+    set ::computes(all) ""
+    for {set rack [lindex $racks 0]} {$rack <= [lindex $racks 1]} {incr rack} {
+        for {set row [lindex $rows 0]} {$row <= [lindex $rows 1]} {incr row} {
+            lappend ::computes(all) compute-$rack-$row
+            lappend ::free_computes compute-$rack-$row
+        }
+    }
 
     puts "collecting jobs"
-    set ::ids [exec ssh gpop.bwh.harvard.edu \
-          ls -1 /nas/nas0/pieper/data/MIRIAD/Project_0002]
-
     set ::jobs ""
     foreach birnid $::ids {
         foreach visit {001 002} {
-            lappend ::jobs [list $birnid $visit]
+            set job [list $birnid $visit]
+            lappend ::jobs $job
+            set ::jobs_to_do($job) 1
+
+            if { [llength $::jobs] >= $job_limit } {
+                puts "only doing $job_limit jobs..."
+                return 
+            }
+
         }
     }
 
-    set ::computes ""
-    for {set rack 0} {$rack < 4} {incr rack} {
-        for {set row 0} {$row < 32} {incr row} {
-            lappend ::computes compute-$rack-$row
-        }
-    }
-    set ::free_computes ""
 }
 
 
 proc load {compute} {
+puts -nonewline " fake load "
+    return .1
+
     if { [catch "exec ssh $compute uptime" res] } {
         return 100.00
     } else {
@@ -38,21 +64,39 @@ proc load {compute} {
 }
 
 proc find_compute {} {
+
+    #
+    # pop the first free compute from the list if there is one
+    #
     if { [llength $::free_computes] == 0 } {
-        puts "searching for free computes"
-        foreach compute $::computes {
-            if { [load $compute] < 0.5 } {
-                lappend ::free_computes $compute
+        return ""
+    } else {
+        set compute [lindex $::free_computes 0]
+        set ::free_computes [lrange $::free_computes 1 end]
+        return $compute 
+    }
+
+    if { 0 } { 
+    # older, load-based calculation - now assume it is loaded
+    # if we are running a job on it, or free otherwise
+
+        if { [llength $::free_computes] == 0 } {
+            puts "searching for free computes"
+            foreach compute $::computes(all) {
+                puts -nonewline "." ; flush stdout
+                if { [load $compute] < 0.5 } {
+                    lappend ::free_computes $compute
+                }
+            }
+            if { [llength $::free_computes] == 0 } {
+                puts "sorry, all nodes busy"
+                return ""
             }
         }
-        if { [llength $::free_computes] == 0 } {
-            puts "sorry, all nodes busy"
-            return ""
-        }
+        set compute [lindex $::free_computes 0]
+        set ::free_computes [lrange $::free_computes 1 end]
+        return $compute 
     }
-    set compute [lindex $::free_computes 0]
-    set ::free_computes [lrange $::free_computes 1 end]
-    return $compute 
 }
 
 proc is_running {pid} {
@@ -64,22 +108,19 @@ proc is_running {pid} {
 }
 
 proc run_job {compute job} {
+
     set birnid [lindex $job 0]
     set visit [lindex $job 1]
 
-    if { 0 } {
-        set fp [open "| csh -c \"ssh $compute ~/birn/bin/runvnc \
-                    --wm /usr/X11R6/bin/xterm -- \
-                    ~/birn/slicer2/slicer2-linux-x86 --no-tkcon \
-                        --exec exit \
-                  \" |& cat" "r"]
-    } else {
-        set fp [open "| csh -c \"ssh $compute ~/birn/bin/runvnc :2 \
-                    --wm /usr/X11R6/bin/xterm -- \
-                    ~/birn/slicer2/slicer2-linux-x86 --no-tkcon \
-                        --exec MIRIADSegmentProcessStudy $birnid $visit ., exit \
-                  \" |& cat" "r"]
-    }
+    set slicercmd "puts hoot"
+    set slicercmd "MIRIADSegmentLoadStudy $::archive 000300742113 001 none"
+    set slicercmd "MIRIADSegmentProcessStudy $::archive $birnid $visit"
+
+    set fp [open "| csh -c \"ssh $compute ~/birn/bin/runvnc \
+                --wm /home/pieper/bin/xterm -- \
+                /home/pieper/birn/slicer2/slicer2-linux-x86 --no-tkcon \
+                    --exec $slicercmd ., exit \
+              \" |& cat" "r"]
     return $fp
 }
 
@@ -92,7 +133,7 @@ proc file_event {fp job} {
         set END 1
     } else {
         gets $fp line
-        lappend ::logs($job) $line
+        lappend ::logs($job) "\n\t$line"
         #puts $line
     }
 }
@@ -100,60 +141,63 @@ proc file_event {fp job} {
 proc dump_logs {} {
     set fp [open "cluster-logs" "w"]
     foreach log [array names ::logs] {
-        puts $fp $::logs($log)
+        puts $fp "\{ $::logs($log) \}\n"
     }
     close $fp
 } 
 
 proc run_jobs {} {
 
-    init
-
-    foreach job $::jobs {
-        while { [set compute [find_compute]] == "" } {
-            puts "waiting for a computer..."
-            after 1000
-        }
-        puts "launching $job on $compute"
-        set fps($job) [run_job $compute $job]
-        fileevent $fps($job) readable "file_event $fps($job) \"$job\""
-        set pids($job) [pid $fps($job)]
-        set ::logs($job) "$job on $compute: "
-        
-        if { [llength [array names pids]] > 10 } {
-            break
-        }
-    }
-    puts "done launching"
+    init 
 
 
+    set all_finished 0
     set iter 0
-    while { [array names pids] != "" } {
-        update
-        foreach job [array names pids] {
-            if { ![is_running $pids($job)] } {
-                puts "finished: $job"
-                unset pids($job)
+
+    while { !$all_finished } {
+
+        foreach job [array names ::jobs_to_do] {
+            set compute [find_compute]
+            if { $compute == "" } {
+                break; # no free computers... go down to check status
+            } else {
+                puts "launching $job on $compute"
+                set ::computes($job) $compute
+                set fps($job) [run_job $compute $job]
+                fileevent $fps($job) readable "file_event $fps($job) \"$job\""
+                set pids($job) [pid $fps($job)]
+                set ::logs($job) "launching $job on $compute at [clock format [clock seconds]]"
+                set ::launch_time($job) [clock seconds]
+                unset ::jobs_to_do($job)
             }
         }
-        after 1000
-        incr iter
-        puts "$iter: [llength [array names pids]] still running..."
-        dump_logs
-    }
 
+        after 1000
+
+        if { [array names pids] == "" } {
+            set all_finished 1
+        } else {
+            update
+            foreach job [array names pids] {
+                if { ![is_running $pids($job)] } {
+                    puts "finished: $job on $::computes($job)"
+                    set elapsed [expr ([clock seconds] - $::launch_time($job)) / 60.]
+                    lappend ::logs($job) "finished $job on $compute at [clock format [clock seconds]] - elapsed time $elapsed minutes"
+                    unset pids($job)
+                    lappend ::free_computes $::computes($job)
+                    unset ::computes($job)
+                }
+            }
+        }
+        incr iter
+        puts -nonewline "\r$iter: [llength [array names pids]] running, [llength [array names ::jobs_to_do]] waiting..."
+        flush stdout
+        dump_logs
+
+    }
 
     puts "done"
     return
-
-
-    if { 0 } {
-        set END 0
-        while { ![catch "pid $fp"] && ![eof $fp] } {
-            vwait END
-        }
-    }
-
 }
 
 run_jobs
