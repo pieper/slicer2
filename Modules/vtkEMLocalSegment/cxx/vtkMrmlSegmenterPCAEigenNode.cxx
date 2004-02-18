fc34dfe2c1fc506170bc1196760c9934d01c766d
pohl <pohl>
2004-02-18 22:58:26 +0000

    For PCA Extension

---
+++ Modules/vtkEMLocalSegment/cxx/vtkMrmlSegmenterPCAEigenNode.cxx
@@ -0,0 +1,110 @@
+/*=auto=========================================================================
+
+(c) Copyright 2003 Massachusetts Institute of Technology (MIT) All Rights Reserved.
+
+This software ("3D Slicer") is provided by The Brigham and Women's 
+Hospital, Inc. on behalf of the copyright holders and contributors.
+Permission is hereby granted, without payment, to copy, modify, display 
+and distribute this software and its documentation, if any, for  
+research purposes only, provided that (1) the above copyright notice and 
+the following four paragraphs appear on all copies of this software, and 
+(2) that source code to any modifications to this software be made 
+publicly available under terms no more restrictive than those in this 
+License Agreement. Use of this software constitutes acceptance of these 
+terms and conditions.
+
+3D Slicer Software has not been reviewed or approved by the Food and 
+Drug Administration, and is for non-clinical, IRB-approved Research Use 
+Only.  In no event shall data or images generated through the use of 3D 
+Slicer Software be used in the provision of patient care.
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+
+THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+NON-INFRINGEMENT.
+
+THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================auto=*/
+#include "vtkMrmlSegmenterPCAEigenNode.h"
+#include "vtkObjectFactory.h"
+
+//------------------------------------------------------------------------------
+vtkMrmlSegmenterPCAEigenNode* vtkMrmlSegmenterPCAEigenNode::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMrmlSegmenterPCAEigenNode");
+  if(ret)
+  {
+    return (vtkMrmlSegmenterPCAEigenNode*)ret;
+  }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMrmlSegmenterPCAEigenNode;
+}
+
+//----------------------------------------------------------------------------
+vtkMrmlSegmenterPCAEigenNode::vtkMrmlSegmenterPCAEigenNode()
+{
+  this->Number          = -1;
+  this->EigenVectorName = NULL; 
+  this->EigenValue      = 0.0;
+}
+
+//----------------------------------------------------------------------------
+vtkMrmlSegmenterPCAEigenNode::~vtkMrmlSegmenterPCAEigenNode()
+{
+  if (this->EigenVectorName)
+  {
+    delete [] this->EigenVectorName;
+    this->EigenVectorName = NULL;
+  }
+}
+
+//----------------------------------------------------------------------------
+void vtkMrmlSegmenterPCAEigenNode::Write(ofstream& of, int nIndent)
+{
+  // Write all attributes not equal to their defaults
+  
+  vtkIndent i1(nIndent);
+  of << i1 << "<SegmenterPCAEigen";
+  of << " Number='" << this->Number << "'";
+  if (this->EigenVectorName && strcmp(this->EigenVectorName, "")) 
+  {
+    of << " EigenVectorName='" << this->EigenVectorName << "'";
+  }
+  of << " EigenValue='" << this->EigenValue << "'";
+  of << "></SegmenterPCAEigen>\n";
+}
+
+//----------------------------------------------------------------------------
+// Copy the node's attributes to this object.
+// Does NOT copy: ID, Name
+void vtkMrmlSegmenterPCAEigenNode::Copy(vtkMrmlNode *anode)
+{
+  vtkMrmlNode::MrmlNodeCopy(anode);
+  vtkMrmlSegmenterPCAEigenNode *node = (vtkMrmlSegmenterPCAEigenNode *) anode;
+
+  this->SetNumber(node->Number);
+  this->SetEigenVectorName(node->EigenVectorName);
+  this->SetEigenValue(node->EigenValue);
+}
+
+//----------------------------------------------------------------------------
+void vtkMrmlSegmenterPCAEigenNode::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkMrmlNode::PrintSelf(os,indent);
+   os << indent << "Number: " << this->Number << "\n";
+   os << indent << "EigenVectorName: " << (this->EigenVectorName ? this->EigenVectorName : "(none)") << "\n";
+   os << indent << "EigenValue: " << this->EigenValue << "\n";
+}
+
+
---
+++ Modules/vtkEMLocalSegment/cxx/vtkMrmlSegmenterPCAEigenNode.h
@@ -0,0 +1,100 @@
+/*=auto=========================================================================
+
+(c) Copyright 2003 Massachusetts Institute of Technology (MIT) All Rights Reserved.
+
+This software ("3D Slicer") is provided by The Brigham and Women's 
+Hospital, Inc. on behalf of the copyright holders and contributors.
+Permission is hereby granted, without payment, to copy, modify, display 
+and distribute this software and its documentation, if any, for  
+research purposes only, provided that (1) the above copyright notice and 
+the following four paragraphs appear on all copies of this software, and 
+(2) that source code to any modifications to this software be made 
+publicly available under terms no more restrictive than those in this 
+License Agreement. Use of this software constitutes acceptance of these 
+terms and conditions.
+
+3D Slicer Software has not been reviewed or approved by the Food and 
+Drug Administration, and is for non-clinical, IRB-approved Research Use 
+Only.  In no event shall data or images generated through the use of 3D 
+Slicer Software be used in the provision of patient care.
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+
+THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+NON-INFRINGEMENT.
+
+THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================auto=*/
+// .NAME vtkMrmlSegmenterPCAEigenNode - MRML node to represent transformation matrices.
+// .SECTION Description
+// The output of a rigid-body registration is a rotation and translation 
+// expressed mathematically as a transformation matrix.  These transforms 
+// can be inserted into MRML files as Segmenter nodes.  Each matrix 
+// affects volumes and models that appear below it in the MRML file.  
+// Multiple matrices can be concatenated together. 
+
+#ifndef __vtkMrmlSegmenterPCAEigenNode_h
+#define __vtkMrmlSegmenterPCAEigenNode_h
+
+//#include <iostream.h>
+//#include <fstream.h>
+#include "vtkMrmlNode.h"
+#include "vtkSlicer.h"
+#include <vtkEMLocalSegmentConfigure.h>
+
+class VTK_EMLOCALSEGMENT_EXPORT vtkMrmlSegmenterPCAEigenNode : public vtkMrmlNode
+{
+public:
+  static vtkMrmlSegmenterPCAEigenNode *New();
+  vtkTypeMacro(vtkMrmlSegmenterPCAEigenNode,vtkMrmlNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+  
+  // Description:
+  // Write the node's attributes to a MRML file in XML format
+  void Write(ofstream& of, int indent);
+
+  //--------------------------------------------------------------------------
+  // Utility Functions
+  //--------------------------------------------------------------------------
+
+  // Description:
+  // Copy the node's attributes to this object
+  void Copy(vtkMrmlNode *node);
+  // Description:
+  // Get/Set for SegmenterPCAEigen
+  vtkGetMacro(Number, int);
+  vtkSetMacro(Number, int);
+
+  // Description:
+  // Get/Set for SegmenterPCAEigen
+  vtkGetMacro(EigenValue, double);
+  vtkSetMacro(EigenValue, double);
+
+  // Description:
+  // Get/Set for SegmenterPCAEigen
+  vtkSetStringMacro(EigenVectorName);
+  vtkGetStringMacro(EigenVectorName);
+protected:
+  vtkMrmlSegmenterPCAEigenNode();
+  ~vtkMrmlSegmenterPCAEigenNode();
+  vtkMrmlSegmenterPCAEigenNode(const vtkMrmlSegmenterPCAEigenNode&) {};
+  void operator=(const vtkMrmlSegmenterPCAEigenNode&) {};
+
+  int    Number;
+  double EigenValue;  
+  char   *EigenVectorName;
+ 
+};
+
+#endif
+
