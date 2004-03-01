4b74d4931b2edbca3addde411d23848659105bd1
pieper <pieper>
2004-03-01 15:49:40 +0000

    new isframes widget

--- Modules/iSlicer/Wrapping/Tcl/iSlicer/iSlicer.tcl
+++ Modules/iSlicer/Wrapping/Tcl/iSlicer/iSlicer.tcl
@@ -12,7 +12,7 @@ if { [info commands isvolume] == "" } {
 
     # source the Module's tcl file that contains it's init procedure
     set files {
-        collapsablewidget.itk imrml.tcl ismodel.tcl isvolume.tcl
+        collapsablewidget.itk imrml.tcl ismodel.tcl isvolume.tcl isframes.tcl
         is3d.tcl isregistration.tcl evaluation-movies.tcl isbutton.tcl 
         istask.tcl spinfloat.tcl }
         
