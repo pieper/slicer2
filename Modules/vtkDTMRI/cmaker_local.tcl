7d3cb8f8526fe1a1a6d243922fa796e3275aebd8
pieper <pieper>
2004-03-14 19:31:45 +0000

    new cmaker syntax (one list of command line args)

--- Modules/vtkDTMRI/cmaker_local.tcl
+++ Modules/vtkDTMRI/cmaker_local.tcl
@@ -1,23 +1,22 @@
 
-set SLICER_MODULE_ARG1 "-DVTKTENSORUTIL_SOURCE_DIR:PATH=$SLICER_HOME/Modules/vtkTensorUtil"
-set SLICER_MODULE_ARG2 "-DVTKTENSORUTIL_BUILD_DIR:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$env(BUILD)" 
+set SLICER_MODULE_ARG "-DVTKTENSORUTIL_SOURCE_DIR:PATH=$SLICER_HOME/Modules/vtkTensorUtil"
+lappend SLICER_MODULE_ARG "-DVTKTENSORUTIL_BUILD_DIR:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$::env(BUILD)" 
 
 switch $tcl_platform(os) {
     "SunOS" {
-       set SLICER_MODULE_ARG3 "-DVTKSLICERTENSOR_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$env(BUILD)/bin/libvtkTensorUtil.so"
-       set SLICER_MODULE_ARG4 "-DVTKSLICERTENSOR_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$env(BUILD)/bin/libvtkTensorUtilTCL.so" 
+       lappend SLICER_MODULE_ARG "-DVTKSLICERTENSOR_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$::env(BUILD)/bin/libvtkTensorUtil.so"
+       lappend SLICER_MODULE_ARG "-DVTKSLICERTENSOR_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$::env(BUILD)/bin/libvtkTensorUtilTCL.so" 
      }
     "Linux" {
-       set SLICER_MODULE_ARG3 "-DVTKSLICERTENSOR_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$env(BUILD)/bin/libvtkTensorUtil.so"
-       set SLICER_MODULE_ARG4 "-DVTKSLICERTENSOR_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$env(BUILD)/bin/libvtkTensorUtilTCL.so" 
+       lappend SLICER_MODULE_ARG "-DVTKSLICERTENSOR_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$::env(BUILD)/bin/libvtkTensorUtil.so"
+       lappend SLICER_MODULE_ARG "-DVTKSLICERTENSOR_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$::env(BUILD)/bin/libvtkTensorUtilTCL.so" 
     }
     "Darwin" {
-       set SLICER_MODULE_ARG3 "-DVTKSLICERTENSOR_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$env(BUILD)/bin/libvtkTensorUtil.dylib"
-       set SLICER_MODULE_ARG4 "-DVTKSLICERTENSOR_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$env(BUILD)/bin/libvtkTensorUtilTCL.dylib" 
+       lappend SLICER_MODULE_ARG "-DVTKSLICERTENSOR_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$::env(BUILD)/bin/libvtkTensorUtil.dylib"
+       lappend SLICER_MODULE_ARG "-DVTKSLICERTENSOR_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$::env(BUILD)/bin/libvtkTensorUtilTCL.dylib" 
     }
     default {
-         set SLICER_MODULE_ARG3 "-DVTKTENSORUTIL_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$env(BUILD)/bin/debug/vtkTensorUtil.lib"
-         set SLICER_MODULE_ARG4 "-DVTKTENSORUTIL_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$env(BUILD)/bin/debug/vtkTensorUtilTCL.lib" 
-
+         lappend SLICER_MODULE_ARG "-DVTKTENSORUTIL_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$::env(BUILD)/bin/debug/vtkTensorUtil.lib"
+         lappend SLICER_MODULE_ARG "-DVTKTENSORUTIL_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkTensorUtil/builds/$::env(BUILD)/bin/debug/vtkTensorUtilTCL.lib" 
     } 
 }
