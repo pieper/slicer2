58328e00fa493e5c5b0fd4976dbbe71683db90c7
pohl <pohl>
2004-03-29 16:41:27 +0000

    *** empty log message ***

--- Modules/vtkEMLocalSegment/Wrapping/Tcl/vtkEMLocalSegment/vtkEMLocalSegment.tcl
+++ Modules/vtkEMLocalSegment/Wrapping/Tcl/vtkEMLocalSegment/vtkEMLocalSegment.tcl
@@ -12,6 +12,7 @@ if {[info commands vtkEMLocalSegment] != "" ||
 
     # source the Module's tcl file that contains it's init procedure
     source [file join $PACKAGE_DIR_VTKEMLocalSegment/../../../tcl/EMLocalSegment.tcl]
+    source [file join $PACKAGE_DIR_VTKEMLocalSegment/../../../tcl/ValidationView.tcl]
     # add this module's name to the list of custom modules in order 
     # to have it's init procedure get called, @ModuleName@Init will be 
     # called by the Slicer Base code
