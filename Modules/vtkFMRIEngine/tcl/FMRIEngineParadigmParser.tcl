694530f8d8c1728cc9de6b8aa107053c3da25fa2
hliu <hliu>
2004-04-22 18:58:48 +0000

    tcl parser for paradigm file.

---
+++ Modules/vtkFMRIEngine/tcl/FMRIEngineParadigmParser.tcl
@@ -0,0 +1,126 @@
+#=auto==========================================================================
+# (c) Copyright 2004 Massachusetts Institute of Technology (MIT) All Rights Reserved.
+#
+# This software ("3D Slicer") is provided by The Brigham and Women's 
+# Hospital, Inc. on behalf of the copyright holders and contributors. 
+# Permission is hereby granted, without payment, to copy, modify, display 
+# and distribute this software and its documentation, if any, for 
+# research purposes only, provided that (1) the above copyright notice and 
+# the following four paragraphs appear on all copies of this software, and 
+# (2) that source code to any modifications to this software be made 
+# publicly available under terms no more restrictive than those in this 
+# License Agreement. Use of this software constitutes acceptance of these 
+# terms and conditions.
+# 
+# 3D Slicer Software has not been reviewed or approved by the Food and 
+# Drug Administration, and is for non-clinical, IRB-approved Research Use 
+# Only.  In no event shall data or images generated through the use of 3D 
+# Slicer Software be used in the provision of patient care.
+# 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+# ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+# DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+# EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+# OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+# NON-INFRINGEMENT.
+# 
+# THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+# IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+# 
+#
+#===============================================================================
+# FILE:        FMRIEngineParadigmParser.tcl
+# PROCEDURES:  
+#   FMRIEngineParseParadigm
+#   FMRIEngineCreateStimulusArray
+#==========================================================================auto=
+#-------------------------------------------------------------------------------
+# .PROC FFMRIEngineParseParadigm 
+# Parses the paradigm file
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
+proc FMRIEngineParseParadigm {} {
+    global FMRIEngine
+
+    if {[file exists $FMRIEngine(paradigmFileName)] == 0} { 
+        DevErrorWindow "Paradigm file doesn't exist: $FMRIEngine(paradigmFileName)"
+        return 0
+    }
+ 
+    # if FMRIEngine(paradigm) exists, unset it
+    if {[info exists FMRIEngine(paradigm)]} { 
+        unset FMRIEngine(paradigm)
+    }
+
+    # Reads the data file
+    set fp [open $FMRIEngine(paradigmFileName) r]
+    set data [read $fp]
+    set lines [split $data "\n"]
+
+    # Values of parameters in paradigm.txt are saved in list FMRIEngine(paradigm).
+    # Here is the index order:
+    # 0 --- total volumes (85)  
+    # 1 --- number of stimuli (1)
+    # 2 --- volumes per baseline (5)
+    # 3 --- volumes per task (5)
+    # 4 --- volumes at start (5)
+    # 5 --- start with (0)
+    foreach line $lines {
+        set pair [split $line ":"]
+        lappend FMRIEngine(paradigm) [lindex $pair 1]
+    }
+    close $fp
+
+    FMRIEngineCreateStimulusArray
+
+    return 1 
+}
+
+
+#-------------------------------------------------------------------------------
+# .PROC FFMRIEngineCreateStimulusArray 
+# Creates a stimulus array 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
+proc FMRIEngineCreateStimulusArray {} {
+    global FMRIEngine
+
+    # if the stimulus array exists, remove it
+    if {[info exists FMRIEngine(stimulus)]} {
+        $FMRIEngine(stimulus) Delete
+    }
+
+    set totalVolumes [lindex $FMRIEngine(paradigm) 0]
+    vtkFloatArray stimulus     
+    stimulus SetNumberOfTuples $totalVolumes 
+    stimulus SetNumberOfComponents 1
+    set FMRIEngine(stimulus) stimulus
+
+    set count 0
+    set doTask [lindex $FMRIEngine(paradigm) 5]
+    while {$count < $totalVolumes} {
+        if {$count == 0} {
+            set length [lindex $FMRIEngine(paradigm) 4] 
+        } else {
+            set length [expr {$doTask ? [lindex $FMRIEngine(paradigm) 3] : \
+                       [lindex $FMRIEngine(paradigm) 2] }]
+        }
+
+        set i 0
+        while {$i < $length && $count < $totalVolumes} {
+            stimulus SetComponent $count 0 [expr {$doTask ? 1.0 : 0.0}] 
+            incr count 
+            incr i
+        }
+        set doTask [expr {!$doTask}] 
+    }
+}
+
+ 
