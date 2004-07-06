01b47d5856e9dc479b713b836291ffa2724f81f5
pieper <pieper>
2004-07-06 15:55:58 +0000

    improved drawing and other changes

--- Modules/iSlicer/tcl/is3d.tcl
+++ Modules/iSlicer/tcl/is3d.tcl
@@ -38,10 +38,13 @@ option add *is3d.background #000000 widgetDefault
 option add *is3d.longitude 0 widgetDefault
 option add *is3d.latitude 0 widgetDefault
 option add *is3d.distance 800 widgetDefault
+option add *is3d.focalpoint "0 0 0" widgetDefault
 option add *is3d.colorscheme "gray" widgetDefault
 option add *is3d.rotationgain "1." widgetDefault
 option add *is3d.scalegain "1." widgetDefault
+option add *is3d.pangain "1." widgetDefault
 option add *is3d.linked3d "" widgetDefault
+option add *is3d.linkedexpose "" widgetDefault
 
 #
 # The class definition - define if needed (not when re-sourcing)
@@ -62,14 +65,17 @@ if { [itcl::find class is3d] == "" } {
       # inherited or composed as part of other widgets
       # or become part of the option database
       #
-      itk_option define -background background Background {}
-      itk_option define -longitude longitude Longitude {}
-      itk_option define -latitude latitude Latitude {}
-      itk_option define -distance distance Distance {}
-      itk_option define -colorscheme colorscheme Colorscheme {}
-      itk_option define -rotationgain rotationgain Rotationgain {}
-      itk_option define -scalegain scalegain Scalegain {}
+      itk_option define -background background Background {#000000}
+      itk_option define -longitude longitude Longitude {0}
+      itk_option define -latitude latitude Latitude {0}
+      itk_option define -distance distance Distance {800}
+      itk_option define -focalpoint focalpoint Focalpoint {0 0 0}
+      itk_option define -colorscheme colorscheme Colorscheme {gray}
+      itk_option define -rotationgain rotationgain Rotationgain {1}
+      itk_option define -scalegain scalegain Scalegain {1}
+      itk_option define -pangain pangain Pangain {1}
       itk_option define -linked3d linked3d Linked3d {}
+      itk_option define -linkedexpose linkedexpose Linkedexpose {}
 
       # widgets for the control area
       variable _controls
@@ -79,7 +85,9 @@ if { [itcl::find class is3d] == "" } {
       variable _dragpos ""
       variable _draglong ""
       variable _draglat ""
+      variable _dragfocal ""
       variable _dragdist ""
+      variable _render_pending 0
 
       # vtk objects in the render
       variable _name
@@ -100,9 +108,10 @@ if { [itcl::find class is3d] == "" } {
       variable _vol
 
       # methods
-      method expose {} {}
+      method expose {} {} ;# queues a render for idle time
+      method render {} {} ;# immediate mode rendering
       method winresize {} {}
-      method bindings {} {}
+      method bindings { {action "set"} } {}
       method pickcb {cmd x y} {}
       method dragcb {state x y} {}
       method actor {} {return $_actor}
@@ -111,6 +120,7 @@ if { [itcl::find class is3d] == "" } {
       method tkrw {} {return $_tkrw}
       method rw {} {return [$_tkrw GetRenderWindow]}
       method camera {} {return [$_ren GetActiveCamera]}
+      method controls {} { return $_controls } 
       method longlatdist { {long 0} {lat 0} {dist 0} } {}
       method screensave { filename {imagetype "PNM"}} {} ;# TODO should be moved to superclass
     }
@@ -135,14 +145,9 @@ itcl::body is3d::constructor {args} {
     #
 
     set _controls $itk_interior.controls_$_name
-    ::iwidgets::collapsablewidget $_controls -labeltext "3d Controls"
-    pack $_controls -side top -expand false -fill x
+    ::iwidgets::collapsablewidget $_controls -labeltext "Controls"
+    pack $_controls -side bottom -expand false -fill x
     set cs [$_controls childsite]
-
-    set _slider $cs.slider
-    scale $_slider -from -180 -to 180 -orient horizontal -command "$this configure -longitude"
-    pack $_slider -side top -expand true -fill x
-
     
     #
     # build the vtk 3d viewer
@@ -399,7 +404,36 @@ itcl::configbody is3d::isvolume {
 
 
 itcl::body is3d::expose {} {
+
+    if { $_render_pending } {
+        return
+    }
+    after idle "$this render"
+    set _render_pending 1
+    foreach is3d $itk_option(-linked3d) {
+        if { $is3d != $this } {
+            $is3d expose
+        }
+    }
+    foreach is3d $itk_option(-linkedexpose) {
+        if { $is3d != $this } {
+            $is3d expose
+        }
+    }
+}
+
+itcl::body is3d::render {} {
+
+    set viewWin [$_tkrw GetRenderWindow]
+    set rens [$viewWin GetRenderers]
+    set rencount [$rens GetNumberOfItems] 
+    for {set r 0} {$r < $rencount} {incr r} {
+        set ren [$rens GetItemAsObject $r]
+        $ren ResetCameraClippingRange    
+    }
+
     $_tkrw Render
+    set _render_pending 0
 }
 
 itcl::body is3d::winresize {} {
@@ -410,24 +444,65 @@ itcl::body is3d::winresize {} {
 
 }
 
-itcl::body is3d::bindings {} {
+itcl::body is3d::bindings { {action "set"} } {
 
-    bind $_tkrw <Control-ButtonPress-1> "$this pickcb pick %x %y"
-    bind $_tkrw <Control-B1-Motion> "$this pickcb pick %x %y"
-    bind $_tkrw <Control-ButtonRelease-1> "$this pickcb noop %x %y"
+    switch $action {
+        "set" {
+            #
+            # mouse bindings
+            #
 
-    bind $_tkrw <ButtonPress-1> "$this dragcb rotstart %x %y"
-    bind $_tkrw <B1-Motion> "$this dragcb rot %x %y"
-    bind $_tkrw <ButtonRelease-1> "$this dragcb rotend %x %y"
+            bind $_tkrw <Control-ButtonPress-1> "$this pickcb pick %x %y"
+            bind $_tkrw <Control-B1-Motion> "$this pickcb pick %x %y"
+            bind $_tkrw <Control-ButtonRelease-1> "$this pickcb noop %x %y"
 
-    #bind $_tkrw <Control-ButtonPress-3> "$this dragcb hscalestart %x %y"
-    #bind $_tkrw <Control-B3-Motion> "$this dragcb hscale %x %y"
-    #bind $_tkrw <Control-ButtonRelease-3> "$this dragcb hscaleend %x %y"
+            bind $_tkrw <ButtonPress-1> "$this dragcb rotstart %x %y"
+            bind $_tkrw <B1-Motion> "$this dragcb rot %x %y"
+            bind $_tkrw <ButtonRelease-1> "$this dragcb rotend %x %y"
 
-    bind $_tkrw <ButtonPress-3> "$this dragcb scalestart %x %y"
-    bind $_tkrw <B3-Motion> "$this dragcb scale %x %y"
-    bind $_tkrw <ButtonRelease-3> "$this dragcb scaleend %x %y"
+            bind $_tkrw <ButtonPress-2> "$this dragcb panstart %x %y"
+            bind $_tkrw <B2-Motion> "$this dragcb pan %x %y"
+            bind $_tkrw <ButtonRelease-2> "$this dragcb panend %x %y"
 
+            bind $_tkrw <ButtonPress-3> "$this dragcb scalestart %x %y"
+            bind $_tkrw <B3-Motion> "$this dragcb scale %x %y"
+            bind $_tkrw <ButtonRelease-3> "$this dragcb scaleend %x %y"
+
+            #
+            # key bindings
+            # TODO - key bindings only work on toplevel windows?
+            #
+
+            bind $_tkrw r "$this dragcb reset"
+        }
+        "clear" {
+            #
+            # mouse bindings
+            #
+            bind $_tkrw <Control-ButtonPress-1> ""
+            bind $_tkrw <Control-B1-Motion> ""
+            bind $_tkrw <Control-ButtonRelease-1> ""
+
+            bind $_tkrw <ButtonPress-1> ""
+            bind $_tkrw <B1-Motion> ""
+            bind $_tkrw <ButtonRelease-1> ""
+
+            bind $_tkrw <ButtonPress-2> ""
+            bind $_tkrw <B2-Motion> ""
+            bind $_tkrw <ButtonRelease-2> ""
+
+            bind $_tkrw <ButtonPress-3> ""
+            bind $_tkrw <B3-Motion> ""
+            bind $_tkrw <ButtonRelease-3> ""
+
+            #
+            # key bindings
+            # TODO - key bindings only work on toplevel windows?
+            #
+
+            bind $_tkrw r ""
+        }
+    }
 }
 
 itcl::body is3d::pickcb {cmd x y} {
@@ -488,6 +563,26 @@ itcl::body is3d::dragcb {state x y} {
         "rotend" {
             set _dragstate ""
         }
+        "panstart" {
+            set _dragpos "$x $y"
+            set _dragstate "pan"
+            set _dragfocal $itk_option(-focalpoint)
+        }
+        "pan" {
+            set _dragstate "pan"
+            set focalx [lindex $_dragfocal 0]
+            set focaly [lindex $_dragfocal 1]
+            set focalz [lindex $_dragfocal 2]
+            set xx [expr $focalx + \
+                ($x - [lindex $_dragpos 0]) * $itk_option(-pangain)]
+            set zz [expr $focalz + \
+                ($y - [lindex $_dragpos 1]) * $itk_option(-pangain)]
+
+            $this configure -focalpoint "$xx $focaly $zz"
+        }
+        "panend" {
+            set _dragstate ""
+        }
         "scalestart" {
             set _dragpos "$x $y"
             set _dragstate "scale"
@@ -502,12 +597,15 @@ itcl::body is3d::dragcb {state x y} {
         "scaleend" {
             set _dragstate ""
         }
+        "reset" {
+            $this configure -longitude 0 -latitude 0 -distance 800 -focalpoint "0 0 0"
+        }
     }
     $this expose
     
     foreach is3d $itk_option(-linked3d) {
         if { $is3d != $this } {
-            $is3d configure -longitude $itk_option(-longitude) -latitude $itk_option(-latitude) -distance $itk_option(-distance)
+            $is3d configure -longitude $itk_option(-longitude) -latitude $itk_option(-latitude) -distance $itk_option(-distance) -focalpoint $itk_option(-focalpoint)
             $is3d expose
         }
     }
@@ -529,6 +627,7 @@ itcl::body is3d::longlatdist { {long 0} {lat 0} {dist 0} } {
     set y [expr $dist * cos($long) * cos($lat)]
     set z [expr $dist * sin($lat)]
 
+    eval [$this camera] SetFocalPoint $itk_option(-focalpoint)
     [$this camera] SetPosition $x $y $z
     [$this camera] SetViewUp 0 0 1
 
