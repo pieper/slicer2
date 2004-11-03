8d491e64e7ac69e1b472c7a4640c86094578d664
rjosest <rjosest>
2004-11-03 22:20:49 +0000

    ENH: Added Tensor arry in UpdateNodeSelectButton

--- Base/tcl/tcl-shared/Developer.tcl
+++ Base/tcl/tcl-shared/Developer.tcl
@@ -414,7 +414,7 @@ proc DevAddSelectButton { TabName f aLabel message pack {tooltip ""} \
 #-------------------------------------------------------------------------------
 proc DevUpdateNodeSelectButton { type ArrayName Label Name { CommandSet "DevSelect" } { None 1 } { New 0 } { LabelMap 1 } {Command2Set ""} } {
 
-    global Volume Model
+    global Volume Model Tensor
     upvar $ArrayName LocalArray
     upvar 0 $type NodeType
 
