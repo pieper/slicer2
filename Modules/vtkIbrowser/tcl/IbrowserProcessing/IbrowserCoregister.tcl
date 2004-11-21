b17371bba661eb85d4c220812e7765501c33cf05
wjp <wjp>
2004-11-21 14:11:44 +0000

    early-version

---
+++ Modules/vtkIbrowser/tcl/IbrowserProcessing/IbrowserCoregister.tcl
@@ -0,0 +1,8 @@
+
+
+proc IbrowserBuildCoregisterGUI { f  master} {
+
+    place $f -in $master -relheight 1.0 -relwidth 1.0
+    #--- set a global variable for frame
+    set ::Ibrowser(fProcessCoregister) $f
+}
