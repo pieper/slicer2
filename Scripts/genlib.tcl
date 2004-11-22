20836a6842dc5c5e1d9a94b41a2eddf4f4449eae
nicole <nicole>
2004-11-22 17:30:07 +0000

    use variables for checkout tags, use variables to define which files to test for each package when checking to see if it's built already, collect commands that must be run under windows. Todo: fix calling cmake on windows, generator is broken

--- Scripts/genlib.tcl
+++ Scripts/genlib.tcl
@@ -14,7 +14,7 @@ exec tclsh "$0" "$@"
 # - configure (or cmake) with needed options
 # - build for this platform
 #
-# Packages: cmake, tcl, itcl, ITK, VTK, gsl
+# Packages: cmake, tcl, itcl, ITK, VTK, gsl, blt
 # 
 # Usage:
 #   genlib [options] [target]
@@ -25,6 +25,20 @@ exec tclsh "$0" "$@"
 #
 # - sp - 2004-06-20
 #
+
+# set up the versions to check out
+set cmakeTag "CMake-2-0-1"
+set vtkTag "Slicer-2-4"
+set itkTag "Slicer-2-4"
+set tclTag "core-8-4-6"
+set tkTag "core-8-4-6"
+set itclTag "itcl-3-2-1"
+set iwidgetsTag "iwidgets-4-0-1"
+set bltTag "blt24z"
+
+# when using this on window, some things will have to be run from the cygwin terminal
+set winMsg "Sorry, this isn't all automated for windows. Open a cygwin terminal and do the following:\n"
+
 
 
 ################################################################################
@@ -110,30 +124,25 @@ cd ..
 set SLICER_HOME [pwd]
 cd $cwd
 
-switch $::tcl_platform(os) {
-    "Linux" {
-        switch $::tcl_platform(machine) {
-            "ia64" {
-                set BUILD linux-ia64
-            }
-            default {
-                set BUILD linux-x86
-            }
-       }
-    }
-    "Darwin" {
-        set BUILD darwin-ppc
-    }
-    "windows" {
-        set BUILD win32
-    }
-    default {
-        error "unknown platform $::tcl_platform(os)"
-    }
+#######
+#
+# Note: the local vars file, slicer2/slicer_variables.tcl, overrides the default values in this script
+# - use it to set your local environment and then your change won't 
+#   be overwritten when this file is updated
+#
+set localvarsfile ${SLICER_HOME}/slicer_variables.tcl
+catch {set localvarsfile [file normalize $localvarsfile]}
+if { [file exists $localvarsfile] } {
+    puts "Sourcing $localvarsfile"
+    source $localvarsfile
+} else {
+    puts "stderr: $localvarsfile not found - use this file to set up your build"
+    exit 1
 }
 
+
 if { $argc == 0 } {
-    set LIB $SLICER_HOME/Lib/$BUILD
+    set LIB $SLICER_HOME/Lib/$env(BUILD)
 } else {
     set LIB $argv
 }
@@ -146,24 +155,62 @@ if { ![file exists $LIB] } {
     file mkdir $LIB
 }
 
+# set up cross platform files to check for existence
+switch $tcl_platform(os) {
+    "SunOS" -
+    "Linux" -
+    "Darwin" {
+        set isWindows 0
+        set tclTestFile $LIB/tcl-build/bin/tclsh8.4
+        set tkTestFile  $LIB/tcl-build/bin/wish8.4
+        set itclTestFile $LIB/tcl-build/lib/libitclstub3.2.so
+        set iwidgetsTestFile $LIB/tcl-build/lib/iwidgets4.0.1/iwidgets.tcl
+        set bltTestFile $LIB/tcl-build/bin/bltwish
+        set vtkTestFile $LIB/VTK-build/bin/vtk
+        set vtkTclLib ../tcl-build/lib/libtcl8.4.so 
+        set vtkTkLib ../tcl-build/lib/libtk8.4.so
+        set vtkTclsh ../tcl-build/bin/tclsh8.4
+        set itkTestFile $LIB/Insight-build/lib/vtk
+    }
+    default {
+        # different windows machines return different values, assume if none of the above, windows
+        set isWindows 1
+        set tclTestFile $LIB/tcl-build/bin/tclsh84.exe
+        set tkTestFile $LIB/tcl-build/bin/wish84.exe
+        set itclTestFile  $LIB/tcl-build/lib/itcl3.2/itcl32.dll
+        set iwidgetsTestFile $LIB/tcl-build/lib/iwidgets4.0.2/iwidgets.tcl
+        set bltTestFile $LIB/tcl-build/bin/bltwish.exe
+        set vtkTestFile $LIB/VTK-build/bin/vtk.exe
+        set vtkTclLib ../tcl-build/lib/tcl84.lib
+        set vtkTkLib ../tcl-build/lib/tk84.lib
+        set vtkTclsh ../tcl-build/bin/tclsh84.exe
+        set itkTestFile $LIB/Insight-build/bin/libITKCommon.dll
+    }
+}
 
 ################################################################################
 # Get and build CMake
 #
 
-set CMAKE $LIB/cmake/CMake-build/bin/cmake
+# set in slicer_vars
+# set CMAKE $LIB/cmake/CMake-build/bin/cmake
 if { ![file exists $CMAKE] } {
     file mkdir $LIB/cmake
     cd $LIB/cmake
 
     runcmd cvs -d :pserver:anonymous:cmake@www.cmake.org:/cvsroot/CMake login
-    runcmd cvs -z3 -d :pserver:anonymous@www.cmake.org:/cvsroot/CMake checkout -r CMake-2-0-1 CMake
+    runcmd cvs -z3 -d :pserver:anonymous@www.cmake.org:/cvsroot/CMake checkout -r $cmakeTag CMake
 
     file mkdir $LIB/cmake/CMake-build
-    cd $LIB/cmake/CMake-build
 
-    runcmd ../CMake/bootstrap
-    runcmd make
+    if {$isWindows} {
+        append winMsg "cd $LIB/cmake/CMake-build\n../CMake/bootstrap\nmake\n"
+
+    } else {
+        cd $LIB/cmake/CMake-build
+        runcmd ../CMake/bootstrap
+        runcmd make
+    }
 }
 
 
@@ -171,74 +218,119 @@ if { ![file exists $CMAKE] } {
 # Get and build tcl, tk, itcl, widgets
 #
 
-if { ![file exists $LIB/tcl-build/bin/tclsh8.4] } {
+# on windows, tcl won't build right, as can't configure, so save commands have to run
+if { ![file exists $tclTestFile] } {
     file mkdir $LIB/tcl
     cd $LIB/tcl
 
     runcmd cvs -d :pserver:anonymous:@cvs.sourceforge.net:/cvsroot/tcl login
-    runcmd cvs -z3 -d :pserver:anonymous@cvs.sourceforge.net:/cvsroot/tcl checkout -r core-8-4-6 tcl
+    runcmd cvs -z3 -d :pserver:anonymous@cvs.sourceforge.net:/cvsroot/tcl checkout -r $tclTag tcl
 
-    cd $LIB/tcl/tcl/unix
+    if {$isWindows} {
+        append winMsg "cd $LIB/tcl/tcl/unix\n./configure --enable-threads --prefix=$LIB/tcl-build\nmake\nmake install\n"
+    } else {
+        cd $LIB/tcl/tcl/unix
 
-    runcmd ./configure --enable-threads --prefix=$LIB/tcl-build
-    runcmd make
-    runcmd make install
+        runcmd ./configure --enable-threads --prefix=$LIB/tcl-build
+        runcmd make
+        runcmd make install
+    }
 }
 
-if { ![file exists $LIB/tcl-build/bin/wish8.4] } {
+if { ![file exists $tkTestFile] } {
     cd $LIB/tcl
 
     runcmd cvs -d :pserver:anonymous:@cvs.sourceforge.net:/cvsroot/tktoolkit login
-    runcmd cvs -z3 -d :pserver:anonymous@cvs.sourceforge.net:/cvsroot/tktoolkit checkout -r core-8-4-6 tk
+    runcmd cvs -z3 -d :pserver:anonymous@cvs.sourceforge.net:/cvsroot/tktoolkit checkout -r $tkTag tk
 
-    cd $LIB/tcl/tk/unix
+    if {$isWindows} {
+        append winMsg "cd $LIB/tcl/tk/unix\n./configure --with-tcl=$LIB/tcl-build/lib --prefix=$LIB/tcl-build\nmake\make install"
+    } else {
+        cd $LIB/tcl/tk/unix
 
-    runcmd ./configure --with-tcl=$LIB/tcl-build/lib --prefix=$LIB/tcl-build
-    runcmd make
-    runcmd make install
+        runcmd ./configure --with-tcl=$LIB/tcl-build/lib --prefix=$LIB/tcl-build
+        runcmd make
+        runcmd make install
+    }
 }
 
-if { ![file exists $LIB/tcl-build/lib/libitclstub3.2.so] } {
+if { ![file exists $itclTestFile] } {
     cd $LIB/tcl
 
     runcmd cvs -d :pserver:anonymous:@cvs.sourceforge.net:/cvsroot/incrtcl login
-    runcmd cvs -z3 -d :pserver:anonymous@cvs.sourceforge.net:/cvsroot/incrtcl checkout -r itcl-3-2-1 incrTcl
+    runcmd cvs -z3 -d :pserver:anonymous@cvs.sourceforge.net:/cvsroot/incrtcl checkout -r $itclTag incrTcl
 
     cd $LIB/tcl/incrTcl
 
     exec chmod +x ../incrTcl/configure 
-    runcmd ../incrTcl/configure --with-tcl=$LIB/tcl-build/lib --with-tk=$LIB/tcl-build/lib --prefix=$LIB/tcl-build
-    runcmd make all
-    runcmd make install
+    if {$isWindows} {
+        append winMsg "cd $LIB/tcl/incrTcl\n./configure --with-tcl=$LIB/tcl-build/lib --with-tk=$LIB/tcl-build/lib --prefix=$LIB/tcl-build\nmake all\nmake install\n"
+    } else {
+        runcmd ../incrTcl/configure --with-tcl=$LIB/tcl-build/lib --with-tk=$LIB/tcl-build/lib --prefix=$LIB/tcl-build
+        runcmd make all
+        runcmd make install
+    }
 }
 
-if { ![file exists $LIB/tcl-build/lib/iwidgets4.0.1/iwidgets.tcl] } {
+if { ![file exists $iwidgetsTestFile] } {
     cd $LIB/tcl
 
     runcmd cvs -d :pserver:anonymous:@cvs.sourceforge.net:/cvsroot/incrtcl login
-    runcmd cvs -z3 -d :pserver:anonymous@cvs.sourceforge.net:/cvsroot/incrtcl checkout -r iwidgets-4-0-1 iwidgets
+    runcmd cvs -z3 -d :pserver:anonymous@cvs.sourceforge.net:/cvsroot/incrtcl checkout -r $iwidgetsTag iwidgets
 
-    cd $LIB/tcl/iwidgets
-    runcmd ../iwidgets/configure --with-tcl=$LIB/tcl-build/lib --with-tk=$LIB/tcl-build/lib --with-itcl=$LIB/tcl/incrTcl --prefix=$LIB/tcl-build
-    runcmd make all
-    runcmd make install
+
+    if {$isWindows} {
+        append winMsg "cd $LIB/tcl/iwidgets\n./configure --with-tcl=$LIB/tcl-build/lib --with-tk=$LIB/tcl-build/lib --with-itcl=$LIB/tcl/incrTcl --prefix=$LIB/tcl-build\nmake install\n"
+    } else {
+        cd $LIB/tcl/iwidgets
+        runcmd ../iwidgets/configure --with-tcl=$LIB/tcl-build/lib --with-tk=$LIB/tcl-build/lib --with-itcl=$LIB/tcl/incrTcl --prefix=$LIB/tcl-build
+        # make all doesn't do anything... 
+        runcmd make all
+        runcmd make install
+    }
+}
+
+
+################################################################################
+# Get and build blt
+#
+
+if {![file exists $bltTestFile] } {
+    cd $LIB/tcl
+    
+    puts "blt password is empty, just hit return"
+    runcmd cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/blt login
+    runcmd cvs -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/blt co -r $bltTag blt
+
+    if {$isWindows} {
+        append winMsg "cd $LIB/tcl/blt\n./configure --with-tcl=$LIB/tcl-build --with-tk=$LIB/tcl-build --prefix=$LIB/tcl-build\nmake\nmake install"
+    } else {
+        cd $LIB/tcl/blt
+        runcmd ./configure --with-tcl=$LIB/tcl-build --with-tk=$LIB/tcl-build --prefix=$LIB/tcl-build 
+        runcmd make
+        runcmd make install
+    }
+
 }
 
 ################################################################################
 # Get and build vtk
 #
 
-if { ![file exists $LIB/VTK-build/bin/vtk] } {
+if { ![file exists $vtkTestFile] } {
     cd $LIB
 
     runcmd cvs -d :pserver:anonymous:vtk@public.kitware.com:/cvsroot/VTK login
-    #runcmd cvs -z3 -d :pserver:anonymous@public.kitware.com:/cvsroot/VTK checkout -r release-4-2-6 VTK
-    runcmd cvs -z3 -d :pserver:anonymous@public.kitware.com:/cvsroot/VTK checkout -r release-4-4-2 VTK
+    runcmd cvs -z3 -d :pserver:anonymous@public.kitware.com:/cvsroot/VTK checkout -r $vtkTag VTK
 
     file mkdir $LIB/VTK-build
     cd $LIB/VTK-build
 
+    if {$isWindows} {
+    append winMsg "cd $LIB/VTK-build\n$CMAKE -G$GENERATOR -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DVTK_USE_CARBON:BOOL=OFF -DVTK_USE_X:BOOL=ON -DVTK_WRAP_TCL:BOOL=ON -DVTK_USE_HYBRID:BOOL=ON -DVTK_USE_PATENTED:BOOL=ON -DTCL_INCLUDE_PATH:PATH=../tcl-build/include -DTK_INCLUDE_PATH:PATH=../tcl-build/include -DTCL_LIBRARY:FILEPATH=$vtkTclLib -DTK_LIBRARY:FILEPATH=$vtkTkLib -DTCL_TCLSH:FILEPATH=$vtkTclsh ../VTK make -j4\n"
+    } else {
     runcmd $CMAKE \
+        -G$GENERATOR \
         -DBUILD_SHARED_LIBS:BOOL=ON \
         -DBUILD_TESTING:BOOL=OFF \
         -DVTK_USE_CARBON:BOOL=OFF \
@@ -248,12 +340,12 @@ if { ![file exists $LIB/VTK-build/bin/vtk] } {
         -DVTK_USE_PATENTED:BOOL=ON \
         -DTCL_INCLUDE_PATH:PATH=../tcl-build/include \
         -DTK_INCLUDE_PATH:PATH=../tcl-build/include \
-        -DTCL_LIBRARY:FILEPATH=../tcl-build/lib/libtcl8.4.so \
-        -DTK_LIBRARY:FILEPATH=../tcl-build/lib/libtk8.4.so \
-        -DTCL_TCLSH:FILEPATH=../tcl-build/bin/tclsh8.4 \
+        -DTCL_LIBRARY:FILEPATH=$vtkTclLib \
+        -DTK_LIBRARY:FILEPATH=$vtkTkLib \
+        -DTCL_TCLSH:FILEPATH=$vtkTclsh \
         ../VTK
-
     runcmd make -j4
+    }
 
 }
 
@@ -261,21 +353,30 @@ if { ![file exists $LIB/VTK-build/bin/vtk] } {
 # Get and build itk
 #
 
-if { ![file exists $LIB/Insight-build/lib/vtk] } {
+if { ![file exists $itkTestFile] } {
     cd $LIB
 
     runcmd cvs -d :pserver:anoncvs:@www.itk.org:/cvsroot/Insight login
-    runcmd cvs -z3 -d :pserver:anoncvs@www.itk.org:/cvsroot/Insight checkout -r ITK-1-8 Insight
+    runcmd cvs -z3 -d :pserver:anoncvs@www.itk.org:/cvsroot/Insight checkout -r $itkTag Insight
 
     file mkdir $LIB/Insight-build
     cd $LIB/Insight-build
 
+
+    if {$isWindows} {
+    append winMsg "cd $LIB/Insight-build\nruncmd $CMAKE -G$GENERATOR -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_EXAMPLES:BOOL=OFF -DBUILD_TESTING:BOOL=OFF ../Insight\nmake -j4\n"
+    } else {
     runcmd $CMAKE \
+        -G$GENERATOR \
         -DBUILD_SHARED_LIBS:BOOL=ON \
         -DBUILD_EXAMPLES:BOOL=OFF \
         -DBUILD_TESTING:BOOL=OFF \
         ../Insight
-
     runcmd make -j4
+    }
 
+}
+
+if {$isWindows} {
+    puts $winMsg
 }
