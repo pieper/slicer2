0014ac62422d6713ece16f50027e75400532a7e1
nicole <nicole>
2004-12-10 18:44:33 +0000

    Check for symlinks in the shared library paths, and relative paths in those symlinks, and changed names, so that copy them properly. Not checking for symlinks on Windows, though

--- Scripts/tarup.tcl
+++ Scripts/tarup.tcl
@@ -192,6 +192,7 @@ proc tarup { {destdir "auto"} } {
     #
     puts " -- copying shared development libraries"
     set sharedLibDir $destdir/Lib/$::env(BUILD)/VTK-build/bin
+    set checkForSymlinks 1
     switch $::tcl_platform(os) {
       "SunOS" {
           set sharedLibs [list libgcc_s.so.1 libstdc++.so.3]
@@ -210,30 +211,51 @@ proc tarup { {destdir "auto"} } {
           set sharedLibs [list msvci70d.dll msvci70.dll msvcp70d.dll msvcp70.dll msvcr70d.dll msvcr70.dll]
           set sharedSearchPath [concat [split $::env(PATH) ";"] $::env(LD_LIBRARY_PATH)]
           set sharedLibDir $destdir/Lib/$::env(BUILD)/VTK-build/bin/$::env(VTK_BUILD_TYPE)
+          set checkForSymlinks 0
       }
     }
     foreach slib $sharedLibs { 
         if {$::Module(verbose)} { puts "LIB $slib"  }
         # don't copy if it's already in the dest dir
-        if {![file exists $sharedLibDir/$slib]} {
+        if {![file exists [file join $sharedLibDir $slib]]} {
             set slibFound 0
             foreach spath $sharedSearchPath { 
-                if {$::Module(verbose)} { puts "checking dir $spath" }
-                if {!$slibFound && [file exists $spath/$slib]} { 
+                if {$::Module(verbose) && !$slibFound} { puts "checking dir $spath" }
+                if {!$slibFound && [file exists [file join $spath $slib]]} { 
                     if {$::Module(verbose)} { puts "found $slib in dir $spath, copying to $sharedLibDir" }
-                    # copy it into vtk bin dir
-                    file copy $spath/$slib $sharedLibDir
+                    # check if it's a symlink (but not on windows)
+                    if {$checkForSymlinks} {
+                        set checkpath [file join $spath $slib]
+                        while {[file type $checkpath] == "link"} {
+                            # need to resolve it to a real file so that file copy will work
+                            set sympath [file readlink $checkpath]
+                            if {[file pathtype $sympath] == "relative"} {
+                                # if the link is relative to the last path, take the dirname of the last one 
+                                # and append the new path to it, then normalize it
+                                set checkpath [file normalize [file join [file dirname $checkpath] $sympath]]
+                            }
+                        }
+                        # the links may have changed the name of the library file 
+                        # (ie adding minor version numbers onto the end)
+                        # so copy the new file into the old file name
+                        file copy $checkpath [file join $sharedLibDir $slib]
+                        if {$::Module(verbose)} { puts "copied checkpath $checkpath to $sharedLibDir/$slib" }
+                    } else {
+                        # copy it into the shared vtk bin dir
+                        file copy [file join $spath $slib]  $sharedLibDir
+                    }
                     set slibFound 1
                 }
             }
             if {!$slibFound} {
                 puts "WARNING: $slib not found, tarup may be incomplete. Place it in one of these directories and rerun tarup: \n $sharedSearchPath"
+                DevErrorWindow "WARNING: $slib not found, tarup may be incomplete. Place it in a directory in your search path and rerun tarup."
             }
         } else { 
             if {$::Module(verbose)} { puts "$slib is already in $sharedLibDir" } 
         }
     }
-    
+
     #
     # grab the itk libraries 
     #
@@ -248,7 +270,7 @@ proc tarup { {destdir "auto"} } {
                 file copy $lib $destdir/Lib/$::env(BUILD)/Insight-build/bin
                 set ll [file tail $lib]
                 exec strip $destdir/Lib/$::env(BUILD)/Insight-build/bin/$ll
-            }
+            } 
         }
         "Darwin" {
             set libs [glob -nocomplain $::env(ITK_BINARY_PATH)/bin/*.dylib]
