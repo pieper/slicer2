42f103e404b1473582c515d22be698251b4e84f4
hayes <hayes>
2004-12-21 21:47:16 +0000

    defined GL_GLEXT_PROTOTYPES to fix problems with compilation on Linux.

--- Modules/vtkVolumeTextureMapping/cxx/vtkOpenGLVolumeTextureMapper3D.h
+++ Modules/vtkVolumeTextureMapping/cxx/vtkOpenGLVolumeTextureMapper3D.h
@@ -2,12 +2,15 @@
 #define __vtkOpenGLVolumeTextureMapper3D_h
 
 #include "vtkVolumeTextureMapper3D.h"
+#define GL_GLEXT_PROTOTYPES // to declare extension prototypes in GL/gl.h
+                            // (linux and mac get compile errors if you don't)
 #include <GL/gl.h>
+#include "glext.h" // include extension defs and decls that may be newer
+                   // than the ones that are in either GL/gl.h or GL/glext.h
 
 //------------------
 #include <math.h>
 #include <stdlib.h>
-#define GL_GLEXT_LEGACY
 #include <stdio.h>
 
 //WINDOWS
@@ -16,8 +19,6 @@
 #else
 #  include <io.h>
 #endif
-
-#include "glext.h"
 
 
 #if !defined(HAVE_GLCOLORTABLE) && defined(HAVE_GLCOLORTABLEEXT)
