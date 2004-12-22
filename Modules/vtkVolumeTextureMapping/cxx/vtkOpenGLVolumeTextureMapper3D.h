db050783113f66be44776a40195e0ce394aa5d4c
pieper <pieper>
2004-12-22 23:42:39 +0000

    fix for including older nvidia gl.h

--- Modules/vtkVolumeTextureMapping/cxx/vtkOpenGLVolumeTextureMapper3D.h
+++ Modules/vtkVolumeTextureMapping/cxx/vtkOpenGLVolumeTextureMapper3D.h
@@ -5,8 +5,10 @@
 #define GL_GLEXT_PROTOTYPES // to declare extension prototypes in GL/gl.h
                             // (linux and mac get compile errors if you don't)
 #include <GL/gl.h>
+#ifndef GL_EXT_color_table                1
 #include "glext.h" // include extension defs and decls that may be newer
                    // than the ones that are in either GL/gl.h or GL/glext.h
+#endif
 
 //------------------
 #include <math.h>
