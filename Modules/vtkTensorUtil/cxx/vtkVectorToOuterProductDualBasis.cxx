7706658990b81f6eef8467f0a17f1e90dd6ac0f6
pieper <pieper>
2004-12-23 00:05:48 +0000

    switch to standard new macro

--- Modules/vtkTensorUtil/cxx/vtkVectorToOuterProductDualBasis.cxx
+++ Modules/vtkTensorUtil/cxx/vtkVectorToOuterProductDualBasis.cxx
@@ -5,18 +5,9 @@
 
 #define VTK_VECTOR_LENGTH 3
 
-//-------------------------------------------------------------------------
-vtkVectorToOuterProductDualBasis* vtkVectorToOuterProductDualBasis::New()
-{
-  // First try to create the object from the vtkObjectFactory
-  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkVectorToOuterProductDualBasis");
-  if(ret)
-    {
-      return (vtkVectorToOuterProductDualBasis*)ret;
-    }
-  // If the factory was unable to create the object, then create it here.
-  return new vtkVectorToOuterProductDualBasis;
-}
+
+vtkStandardNewMacro(vtkVectorToOuterProductDualBasis);
+
 
 //-------------------------------------------------------------------------
 vtkVectorToOuterProductDualBasis::vtkVectorToOuterProductDualBasis()
