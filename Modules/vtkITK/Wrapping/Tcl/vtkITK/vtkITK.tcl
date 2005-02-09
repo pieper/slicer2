fd5ca984519af5421cced8f2d3ece5c88c4e9c08
pieper <pieper>
2005-02-09 00:53:16 +0000

    added new editor effect

--- Modules/vtkITK/Wrapping/Tcl/vtkITK/vtkITK.tcl
+++ Modules/vtkITK/Wrapping/Tcl/vtkITK/vtkITK.tcl
@@ -9,6 +9,7 @@ if {[info commands vtkITK] != "" ||
     [::vtk::load_component vtkITKTCL] == ""} {
     global PACKAGE_DIR_VTKITK
     source  [file join $PACKAGE_DIR_VTKITK/../../../tcl/EdWatershed.tcl]
+    source  [file join $PACKAGE_DIR_VTKITK/../../../tcl/EdConfidenceConnected.tcl]
     source  [file join $PACKAGE_DIR_VTKITK/../../../tcl/ITKFilters.tcl]
     source  [file join $PACKAGE_DIR_VTKITK/../../../tcl/VolGeneric.tcl]
     global Module
