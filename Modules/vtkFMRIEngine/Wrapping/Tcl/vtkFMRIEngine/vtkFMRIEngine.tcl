02850268edcef8ea7421c120f5e26ed789a20d51
hliu <hliu>
2005-03-28 16:19:20 +0000

    replaced FMRIEngine.tcl by fMRIEngine.tcl.

--- Modules/vtkFMRIEngine/Wrapping/Tcl/vtkFMRIEngine/vtkFMRIEngine.tcl
+++ Modules/vtkFMRIEngine/Wrapping/Tcl/vtkFMRIEngine/vtkFMRIEngine.tcl
@@ -9,11 +9,11 @@ package require vtkAnalyze
 if {[info commands vtkActivationDetector] != "" ||
     [::vtk::load_component vtkFMRIEngineTCL] == ""} {
     global PACKAGE_DIR_VTKFMRIENGINE
-    source  $PACKAGE_DIR_VTKFMRIENGINE/../../../tcl/FMRIEngine.tcl
+    source  $PACKAGE_DIR_VTKFMRIENGINE/../../../tcl/fMRIEngine.tcl
     package provide vtkFMRIEngine 1.0
 
     # source the Module's tcl file that contains it's init procedure
-    #source [file join $PACKAGE_DIR_VTKFMRIEngine/../../../tcl/FMRIEngine.tcl]
+    #source [file join $PACKAGE_DIR_VTKFMRIEngine/../../../tcl/fMRIEngine.tcl]
     # add this module's name to the list of custom modules in order 
     # to have it's init procedure get called, @ModuleName@Init will be 
     # called by the Slicer Base code
