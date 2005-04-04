161d6fcffc4633a2d4a1a620254f665eb5850a7e
nicole <nicole>
2005-04-04 17:01:59 +0000

    Added copy right and comments

--- Modules/vtkFreeSurferReaders/tcl/regions.tcl
+++ Modules/vtkFreeSurferReaders/tcl/regions.tcl
@@ -1,3 +1,45 @@
+#=auto==========================================================================
+# (c) Copyright 2005 Massachusetts Institute of Technology (MIT) All Rights Reserved.
+#
+# This software ("3D Slicer") is provided by The Brigham and Women's 
+# Hospital, Inc. on behalf of the copyright holders and contributors. 
+# Permission is hereby granted, without payment, to copy, modify, display 
+# and distribute this software and its documentation, if any, for 
+# research purposes only, provided that (1) the above copyright notice and 
+# the following four paragraphs appear on all copies of this software, and 
+# (2) that source code to any modifications to this software be made 
+# publicly available under terms no more restrictive than those in this 
+# License Agreement. Use of this software constitutes acceptance of these 
+# terms and conditions.
+# 
+# 3D Slicer Software has not been reviewed or approved by the Food and 
+# Drug Administration, and is for non-clinical, IRB-approved Research Use 
+# Only.  In no event shall data or images generated through the use of 3D 
+# Slicer Software be used in the provision of patient care.
+# 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+# ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+# DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+# EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+# OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+# NON-INFRINGEMENT.
+# 
+# THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+# IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+# 
+#
+#===============================================================================
+# FILE:        regions.tcl
+# PROCEDURES:  
+#   QueryAtlas_fdemo
+#   QueryAtlas_custom_view
+#   QueryAtlas_mdemo
+#==========================================================================auto=
 package require Iwidgets
 package require http
 #
@@ -837,6 +879,13 @@ itcl::body regions::refresh {args} {
     }
 }
 
+#-------------------------------------------------------------------------------
+# .PROC QueryAtlas_fdemo
+# Uses some standard data to demonstrate the query atlas.
+# .ARGS
+# path demodata path to the directory contain demonstration data
+# .END
+#-------------------------------------------------------------------------------
 proc QueryAtlas_fdemo { {demodata c:/pieper/bwh/data/fbirn-phantom-staple/average7} } {
     
     set _fstcldir [file normalize $::PACKAGE_DIR_VTKFREESURFERREADERS/../../../tcl]
@@ -909,6 +958,13 @@ proc QueryAtlas_fdemo { {demodata c:/pieper/bwh/data/fbirn-phantom-staple/averag
     QueryAtlas_custom_view $modelid
 }
 
+#-------------------------------------------------------------------------------
+# .PROC QueryAtlas_custom_view
+# Sets up a custom view for the query atlas.
+# .ARGS
+# modelid the id of the model for which we're setting the scalar range
+# .END
+#-------------------------------------------------------------------------------
 proc QueryAtlas_custom_view {modelid} {
 
     set lutid [MainLutsGetLutIDByName "InvGray"]
@@ -932,6 +988,13 @@ proc QueryAtlas_custom_view {modelid} {
     MainAnnoSetVisibility
 }
 
+#-------------------------------------------------------------------------------
+# .PROC QueryAtlas_mdemo
+# Does a demo of the query atlas, using standard data
+# .ARGS
+# path demodata Path to a directory holding demonstration data
+# .END
+#-------------------------------------------------------------------------------
 proc QueryAtlas_mdemo { {demodata c:/pieper/bwh/data/MGH-Siemens15-SP.1-uw} } {
     
     set _fstcldir [file normalize $::PACKAGE_DIR_VTKFREESURFERREADERS/../../../tcl]
--- Modules/vtkFreeSurferReaders/tcl/scalarbar.tcl
+++ Modules/vtkFreeSurferReaders/tcl/scalarbar.tcl
@@ -1,9 +1,65 @@
+#=auto==========================================================================
+# (c) Copyright 2005 Massachusetts Institute of Technology (MIT) All Rights Reserved.
+#
+# This software ("3D Slicer") is provided by The Brigham and Women's 
+# Hospital, Inc. on behalf of the copyright holders and contributors. 
+# Permission is hereby granted, without payment, to copy, modify, display 
+# and distribute this software and its documentation, if any, for 
+# research purposes only, provided that (1) the above copyright notice and 
+# the following four paragraphs appear on all copies of this software, and 
+# (2) that source code to any modifications to this software be made 
+# publicly available under terms no more restrictive than those in this 
+# License Agreement. Use of this software constitutes acceptance of these 
+# terms and conditions.
+# 
+# 3D Slicer Software has not been reviewed or approved by the Food and 
+# Drug Administration, and is for non-clinical, IRB-approved Research Use 
+# Only.  In no event shall data or images generated through the use of 3D 
+# Slicer Software be used in the provision of patient care.
+# 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+# ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+# DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+# EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+# OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+# NON-INFRINGEMENT.
+# 
+# THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+# IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+# 
+#
+#===============================================================================
+# FILE:        scalarbar.tcl
+# PROCEDURES:  
+#   uniqueCommandName
+#   addScalarBar
+#==========================================================================auto=
 
 
+#-------------------------------------------------------------------------------
+# .PROC uniqueCommandName
+# Returns a string made up from a standard string (name) followed by a time stamp
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc uniqueCommandName {} {
     return name.[clock clicks]
 }
 
+#-------------------------------------------------------------------------------
+# .PROC addScalarBar
+# Adds a scalar bar to a render window.
+# .ARGS
+# LUT vlt look up table
+# renderer ren1 renderer to add the scalar bar to
+# window renderWindow the window the renderer displays in
+# .END
+#-------------------------------------------------------------------------------
 proc addScalarBar { vlt ren1 renderWindow } {
   set scalarBar [uniqueCommandName]
   vtkScalarBarActor $scalarBar
