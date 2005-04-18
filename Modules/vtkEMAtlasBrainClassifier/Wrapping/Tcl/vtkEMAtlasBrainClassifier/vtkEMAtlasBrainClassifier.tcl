db4e56a6a7d012420afa16dd5aa6bd9ca24d72bd
nicole <nicole>
2005-04-18 20:30:07 +0000

    added the global declaration of the package directory, and included the package provide line so that it can be found on start up

--- Modules/vtkEMAtlasBrainClassifier/Wrapping/Tcl/vtkEMAtlasBrainClassifier/vtkEMAtlasBrainClassifier.tcl
+++ Modules/vtkEMAtlasBrainClassifier/Wrapping/Tcl/vtkEMAtlasBrainClassifier/vtkEMAtlasBrainClassifier.tcl
@@ -6,7 +6,12 @@ package require vtk
 #
 # Because we do not have any vtk code we changed this a little bit
 # source the Module's tcl file that contains it's init procedure
+
+global PACKAGE_DIR_VTKEMAtlasBrainClassifier
+
 source [file join $PACKAGE_DIR_VTKEMAtlasBrainClassifier/../../../tcl/EMAtlasBrainClassifier.tcl]
+
+package provide vtkEMAtlasBrainClassifier 1.0
 
 # add this module's name to the list of custom modules in order 
 # to have it's init procedure get called, @ModuleName@Init will be 
