7db06d3d478a689f6f01c1ea1cadb7c6226332d6
nicole <nicole>
2005-04-19 14:59:16 +0000

    updated copyright, comments

--- Modules/vtkCorCTA/tcl/CorCTA.tcl
+++ Modules/vtkCorCTA/tcl/CorCTA.tcl
@@ -1,31 +1,46 @@
 #=auto==========================================================================
-# (c) Copyright 2002 Massachusetts Institute of Technology
+# (c) Copyright 2005 Massachusetts Institute of Technology (MIT) All Rights Reserved.
 #
+# This software ("3D Slicer") is provided by The Brigham and Women's 
+# Hospital, Inc. on behalf of the copyright holders and contributors. 
 # Permission is hereby granted, without payment, to copy, modify, display 
-# and distribute this software and its documentation, if any, for any purpose, 
-# provided that the above copyright notice and the following three paragraphs 
-# appear on all copies of this software.  Use of this software constitutes 
-# acceptance of these terms and conditions.
-#
-# IN NO EVENT SHALL MIT BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
-# INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE 
-# AND ITS DOCUMENTATION, EVEN IF MIT HAS BEEN ADVISED OF THE POSSIBILITY OF 
-# SUCH DAMAGE.
-#
-# MIT SPECIFICALLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTIES INCLUDING, 
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR 
-# A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
-#
-# THE SOFTWARE IS PROVIDED "AS IS."  MIT HAS NO OBLIGATION TO PROVIDE 
-# MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+# and distribute this software and its documentation, if any, for 
+# research purposes only, provided that (1) the above copyright notice and 
+# the following four paragraphs appear on all copies of this software, and 
+# (2) that source code to any modifications to this software be made 
+# publicly available under terms no more restrictive than those in this 
+# License Agreement. Use of this software constitutes acceptance of these 
+# terms and conditions.
+# 
+# 3D Slicer Software has not been reviewed or approved by the Food and 
+# Drug Administration, and is for non-clinical, IRB-approved Research Use 
+# Only.  In no event shall data or images generated through the use of 3D 
+# Slicer Software be used in the provision of patient care.
+# 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+# ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+# DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+# EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+# OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+# NON-INFRINGEMENT.
+# 
+# THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+# IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+# 
 #
 #===============================================================================
-# FILE:        vtkCorCTA.tcl
+# FILE:        CorCTA.tcl
 # PROCEDURES:  
 #   CorCTAInit
 #   CorCTABuildGUI
 #   CorCTAUpdateGUI
 #   CorCTABuildVTK
+#   CorCTAEnter
 #   CorCTAEnter
 #   CorCTAExit
 #   CorCTAThinning
@@ -137,7 +152,7 @@ proc CorCTAInit {} {
     #   appropriate revision number and date when the module is checked in.
     #   
     lappend Module(versions) [ParseCVSInfo $m \
-        {$Revision: 1.6 $} {$Date: 2005/01/28 22:44:27 $}]
+        {$Revision: 1.7 $} {$Date: 2005/04/19 14:59:16 $}]
 
     # Initialize module-level variables
     #------------------------------------
@@ -179,6 +194,12 @@ proc CorCTAInit {} {
 #-------------------------------------------------------------------------------
 
 
+#-------------------------------------------------------------------------------
+# .PROC CorCTABuildGUI
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc CorCTABuildGUI {} {
     global Gui CorCTA Module Volume
     
@@ -334,6 +355,12 @@ a label map as an input and generates a model.
 }
 
 
+#-------------------------------------------------------------------------------
+# .PROC CorCTAUpdateGUI
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc CorCTAUpdateGUI {} {
     global CorCTA Volume
 
@@ -342,6 +369,12 @@ proc CorCTAUpdateGUI {} {
 }
 
 
+#-------------------------------------------------------------------------------
+# .PROC CorCTABuildVTK
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc CorCTABuildVTK {} {
     global CorCTA
     
@@ -375,7 +408,6 @@ proc CorCTABuildVTK {} {
 # .ARGS
 # .END
 #-------------------------------------------------------------------------------
-
 proc CorCTAEnter {} {
     global CorCTA Gui
     
@@ -476,6 +508,12 @@ proc CorCTAThinning {} {
     RenderAll
 }
 
+#-------------------------------------------------------------------------------
+# .PROC CorCTAConvert
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc CorCTAConvert {} {
     global CorCTA Volume Model Module Gui
     
@@ -554,12 +592,26 @@ proc CorCTAConvert {} {
     Render3D
 }
 
+#-------------------------------------------------------------------------------
+# .PROC CorCTACenterlines
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc CorCTACenterlines {} {
     CorCTAThinning
     CorCTAConvert
 }
 
 
+#-------------------------------------------------------------------------------
+# .PROC CorCTAPickLine
+# 
+# .ARGS
+# vtkActor Actor 
+# int CellId
+# .END
+#-------------------------------------------------------------------------------
 proc CorCTAPickLine {Actor CellId} {
     global CorCTA
     
