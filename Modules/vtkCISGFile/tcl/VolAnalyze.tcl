f2b87d22f0184bfb5de8eacdd49c1e499014760b
nicole <nicole>
2005-04-19 14:57:06 +0000

    Updated copyright, comments

--- Modules/vtkCISGFile/tcl/VolAnalyze.tcl
+++ Modules/vtkCISGFile/tcl/VolAnalyze.tcl
@@ -1,35 +1,58 @@
 #=auto==========================================================================
-# (c) Copyright 2002 Massachusetts Institute of Technology
+# (c) Copyright 2005 Massachusetts Institute of Technology (MIT) All Rights Reserved.
 #
+# This software ("3D Slicer") is provided by The Brigham and Women's 
+# Hospital, Inc. on behalf of the copyright holders and contributors. 
 # Permission is hereby granted, without payment, to copy, modify, display 
-# and distribute this software and its documentation, if any, for any purpose, 
-# provided that the above copyright notice and the following three paragraphs 
-# appear on all copies of this software.  Use of this software constitutes 
-# acceptance of these terms and conditions.
-#
-# IN NO EVENT SHALL MIT BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
-# INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE 
-# AND ITS DOCUMENTATION, EVEN IF MIT HAS BEEN ADVISED OF THE POSSIBILITY OF 
-# SUCH DAMAGE.
-#
-# MIT SPECIFICALLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTIES INCLUDING, 
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR 
-# A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
-#
-# THE SOFTWARE IS PROVIDED "AS IS."  MIT HAS NO OBLIGATION TO PROVIDE 
-# MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS. 
+# and distribute this software and its documentation, if any, for 
+# research purposes only, provided that (1) the above copyright notice and 
+# the following four paragraphs appear on all copies of this software, and 
+# (2) that source code to any modifications to this software be made 
+# publicly available under terms no more restrictive than those in this 
+# License Agreement. Use of this software constitutes acceptance of these 
+# terms and conditions.
+# 
+# 3D Slicer Software has not been reviewed or approved by the Food and 
+# Drug Administration, and is for non-clinical, IRB-approved Research Use 
+# Only.  In no event shall data or images generated through the use of 3D 
+# Slicer Software be used in the provision of patient care.
+# 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+# ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+# DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+# EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+# OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+# NON-INFRINGEMENT.
+# 
+# THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+# IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+# 
 #
 #===============================================================================
 # FILE:        VolAnalyze.tcl
 # PROCEDURES:  
+#   VolAnalyzeInit
 #   VolAnalyzeBuildGUI
 #   VolAnalyzeEnter
 #   VolAnalyzeExit
 #   VolAnalyzeSetFileName
 #   VolAnalyzeApply
+#   VolAnalyzeHandleCompressed
+#   VolAnalyzeCleanupCompressed
 #   VolAnalyzeMainFileCloseUpdate
+#   VolAnalyzetempdir
 #==========================================================================auto=
 
+#-------------------------------------------------------------------------------
+# .PROC VolAnalyzeInit
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc VolAnalyzeInit {} {
     global Gui VolAnalyze Volume Slice Module
 
@@ -60,7 +83,7 @@ proc VolAnalyzeInit {} {
 # .PROC VolAnalyzeBuildGUI
 # Builds the GUI for the free surfer readers, as a submodule of the Volumes module
 # .ARGS
-# parentFrame the frame in which to build this Module's GUI
+# window parentFrame the frame in which to build this Module's GUI
 # .END
 #-------------------------------------------------------------------------------
 proc VolAnalyzeBuildGUI {parentFrame} {
@@ -211,6 +234,7 @@ proc VolAnalyzeSetFileName {} {
 # .PROC VolAnalyzeApply
 # Check the file type variable and build the appropriate model or volume
 # .ARGS
+# string EPIReconOrder optional
 # .END
 #-------------------------------------------------------------------------------
 proc VolAnalyzeApply {{EPIReconOrder ""}} {
@@ -385,6 +409,14 @@ proc VolAnalyzeApply {{EPIReconOrder ""}} {
     return $i
 }
 
+#-------------------------------------------------------------------------------
+# .PROC VolAnalyzeHandleCompressed
+# 
+# .ARGS
+# path filename
+# path rfilename
+# .END
+#-------------------------------------------------------------------------------
 proc VolAnalyzeHandleCompressed {filename rfilename} {
 
     upvar $rfilename retfilename
@@ -415,6 +447,13 @@ proc VolAnalyzeHandleCompressed {filename rfilename} {
     return $compressed
 }
 
+#-------------------------------------------------------------------------------
+# .PROC VolAnalyzeCleanupCompressed
+# 
+# .ARGS
+# path filename
+# .END
+#-------------------------------------------------------------------------------
 proc VolAnalyzeCleanupCompressed { filename } {
 
     set tmp [VolAnalyzetempdir]
@@ -493,6 +532,12 @@ proc VolAnalyzeMainFileCloseUpdate {} {
 # Results:
 #    The directory for temporary files.
 
+#-------------------------------------------------------------------------------
+# .PROC VolAnalyzetempdir
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc VolAnalyzetempdir {} {
     global tcl_platform env
     set attempdirs [list]
