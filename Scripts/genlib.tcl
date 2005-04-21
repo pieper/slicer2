934a24854b756cba3804846b7edcea0d494c4fa6
hayes <hayes>
2005-04-21 21:28:50 +0000

    Added configure.ac patch for building gsl release-1-6 on Solaris.

--- Scripts/genlib.tcl
+++ Scripts/genlib.tcl
@@ -403,13 +403,17 @@ if { ![file exists $::GSL_TEST_FILE] } {
             runcmd automake --add-missing --gnu
             runcmd autoconf
         } else {
+            if { $isSolaris } {
+                # patch configure.ac
+                runcmd /usr/bin/patch -i $SLICER_HOME/Scripts/gslConfigureAc.diff $SLICER_LIB/gsl-build/gsl/configure.ac
+            }
             # equivalent of autogen.sh with hack to make autoconf run on Solaris and RH 7.3.  
             runcmd libtoolize --automake
             runcmd aclocal
             runcmd automake --add-missing --gnu
-        # autoconf will fail the first time, then succeed on RH 7.3 and Solaris 
-        catch "runcmd autoconf -I ."
-        runcmd autoconf -I .
+            # autoconf will fail the first time, then succeed on RH 7.3 and Solaris 
+            catch "runcmd autoconf -I ."
+            runcmd autoconf -I .
         }   
         runcmd ./configure --prefix=$SLICER_LIB/gsl
         runcmd touch doc/version-ref.texi
