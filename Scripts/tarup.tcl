974e741aba8feb744348b198e94472f49e2ba17a
hayes <hayes>
2005-05-05 19:53:38 +0000

    Changed scp upload directory to slicer2.5

--- Scripts/tarup.tcl
+++ Scripts/tarup.tcl
@@ -437,7 +437,7 @@ proc tarup { {destdir "auto"} } {
     }
 
     if { $do_upload == "true" } {
-    set scpdestination "$::env(USER)@slicerl.bwh.harvard.edu:/usr/local/apache2/htdocs/snapshots/slicer2.4"
+    set scpdestination "$::env(USER)@slicerl.bwh.harvard.edu:/usr/local/apache2/htdocs/snapshots/slicer2.5"
         puts " -- upload to $scpdestination"
         switch $::tcl_platform(os) {
             "SunOS" -
