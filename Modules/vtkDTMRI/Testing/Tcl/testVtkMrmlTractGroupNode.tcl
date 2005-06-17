02d0e7a850936bff7f00933155c1f8df7071c81c
odonnell <odonnell>
2005-06-17 00:57:34 +0000

    rm calls to tk_messageBox and exit

--- Modules/vtkDTMRI/Testing/Tcl/testVtkMrmlTractGroupNode.tcl
+++ Modules/vtkDTMRI/Testing/Tcl/testVtkMrmlTractGroupNode.tcl
@@ -7,11 +7,11 @@ node SetTractGroupID 1
 node AddTractToGroup 2
 node AddTractToGroup 3
 
-tk_messageBox -message [node Print]
+puts [node Print]
 
 vtkMrmlTree tree
 tree AddItem node
 tree Write "test.mrml"
 
-exit
+
 
