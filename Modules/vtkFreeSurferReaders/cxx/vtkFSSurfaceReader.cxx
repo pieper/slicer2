dd864bfb4f163c5559c4889386a0b8b3c379537e
nicole <nicole>
2005-08-05 19:21:15 +0000

    Cleaned up some indenting, first pass at putting in progress monitoring for loading files

--- Modules/vtkFreeSurferReaders/cxx/vtkFSSurfaceReader.cxx
+++ Modules/vtkFreeSurferReaders/cxx/vtkFSSurfaceReader.cxx
@@ -40,8 +40,8 @@ PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkFSSurfaceReader.cxx,v $
   Language:  C++
-  Date:      $Date: 2005/04/04 15:35:46 $
-  Version:   $Revision: 1.7 $
+  Date:      $Date: 2005/08/05 19:21:15 $
+  Version:   $Revision: 1.8 $
 
 =========================================================================*/
 #include "vtkFSSurfaceReader.h"
@@ -164,7 +164,9 @@ void vtkFSSurfaceReader::Execute()
   float faceNormal[3];
   float length;
 #endif
-
+  int totalSteps = 1;
+  int thisStep = 0;
+  
   vtkDebugMacro(<<"Reading vtk polygonal data...");
 
   // Try to open the file.
@@ -276,47 +278,64 @@ void vtkFSSurfaceReader::Execute()
   }
 #endif
 
+  totalSteps = numVertices + (numFaces*faceMultiplier/faceIncrement * numVerticesPerFace);
+#if FS_CALC_NORMALS
+  if (NULL != vertices && NULL != faces) {
+      for (vIndex = 0; vIndex < numVertices; vIndex++) {
+          fv1 = &vertices[vIndex];
+          for (fvIndex = 0; fvIndex < fv1->numFaces; fvIndex++) {
+              totalSteps++;
+          }
+      }
+  }
+#endif
+  vtkDebugMacro(<<"Got total steps = " << totalSteps);
+  
   // For each vertex...
   for (vIndex = 0; vIndex < numVertices; vIndex++) {
 
-    // Depending on the file type, read in three two bytes ints and
-    // convert them from meters to millimeters or read in three floats
-    // in millimeters. Insert them into the vertices array.  The old
-    // quad format uses the ints and the new quad and triangle formats
-    // use floats.
-    switch (magicNumber) {
-    case FS_QUAD_FILE_MAGIC_NUMBER: 
-      vtkFSIO::ReadInt2 (surfaceFile, tmpX);
-      vtkFSIO::ReadInt2 (surfaceFile, tmpY);
-      vtkFSIO::ReadInt2 (surfaceFile, tmpZ);
-      locations[0] = (float)tmpX / 100.0;
-      locations[1] = (float)tmpY / 100.0;
-      locations[2] = (float)tmpZ / 100.0;
-      break;
-    case FS_NEW_QUAD_FILE_MAGIC_NUMBER: 
-    case FS_TRIANGLE_FILE_MAGIC_NUMBER: 
-      vtkFSIO::ReadFloat (surfaceFile, locations[0]);
-      vtkFSIO::ReadFloat (surfaceFile, locations[1]);
-      vtkFSIO::ReadFloat (surfaceFile, locations[2]);
-      break;
-    }
-    
-    outputVertices->InsertNextPoint (locations);
+      thisStep++; 
+
+      // Depending on the file type, read in three two bytes ints and
+      // convert them from meters to millimeters or read in three floats
+      // in millimeters. Insert them into the vertices array.  The old
+      // quad format uses the ints and the new quad and triangle formats
+      // use floats.
+      switch (magicNumber) {
+      case FS_QUAD_FILE_MAGIC_NUMBER: 
+          vtkFSIO::ReadInt2 (surfaceFile, tmpX);
+          vtkFSIO::ReadInt2 (surfaceFile, tmpY);
+          vtkFSIO::ReadInt2 (surfaceFile, tmpZ);
+          locations[0] = (float)tmpX / 100.0;
+          locations[1] = (float)tmpY / 100.0;
+          locations[2] = (float)tmpZ / 100.0;
+          break;
+      case FS_NEW_QUAD_FILE_MAGIC_NUMBER: 
+      case FS_TRIANGLE_FILE_MAGIC_NUMBER: 
+          vtkFSIO::ReadFloat (surfaceFile, locations[0]);
+          vtkFSIO::ReadFloat (surfaceFile, locations[1]);
+          vtkFSIO::ReadFloat (surfaceFile, locations[2]);
+          break;
+      }
+      
+      outputVertices->InsertNextPoint (locations);
 
 #if FS_CALC_NORMALS
     // If we have connectivity info, fill out the location and
     // initialize the normals to 0 for this vertex.
-    if (NULL != vertices && NULL != faces) {
-      v = &vertices[vIndex];
-      v->x = locations[0];
-      v->y = locations[1];
-      v->z = locations[2];
-      v->nx = 0;
-      v->ny = 0;
-      v->nz = 0;
-      v->numFaces = 0;
-    }
+      if (NULL != vertices && NULL != faces) {
+          v = &vertices[vIndex];
+          v->x = locations[0];
+          v->y = locations[1];
+          v->z = locations[2];
+          v->nx = 0;
+          v->ny = 0;
+          v->nz = 0;
+          v->numFaces = 0;
+      }
+      
 #endif
+      this->UpdateProgress(1.0*thisStep/totalSteps);
   }
 
   // For each face...
@@ -327,45 +346,49 @@ void vtkFSSurfaceReader::Execute()
     // For each vertex in the face...
     for (fvIndex = 0; fvIndex < numVerticesPerFace; fvIndex++) {
 
-      // Read in a vertex index. Triangle format gets a normal int,
-      // quad formats get three byte ints.
-      switch (magicNumber) {
-      case FS_QUAD_FILE_MAGIC_NUMBER: 
-      case FS_NEW_QUAD_FILE_MAGIC_NUMBER: 
-    vtkFSIO::ReadInt3 (surfaceFile, tmpfIndex);
-    break;
-      case FS_TRIANGLE_FILE_MAGIC_NUMBER: 
-    fread (&tmpfIndex, sizeof(int), 1, surfaceFile);
-    vtkByteSwap::Swap4BE (&tmpfIndex);
-    break;
-      }
-
-      faceIndices[fvIndex] = tmpfIndex;
-
+        thisStep++;
+        
+        // Read in a vertex index. Triangle format gets a normal int,
+        // quad formats get three byte ints.
+        switch (magicNumber) {
+        case FS_QUAD_FILE_MAGIC_NUMBER: 
+        case FS_NEW_QUAD_FILE_MAGIC_NUMBER: 
+            vtkFSIO::ReadInt3 (surfaceFile, tmpfIndex);
+            break;
+        case FS_TRIANGLE_FILE_MAGIC_NUMBER: 
+            fread (&tmpfIndex, sizeof(int), 1, surfaceFile);
+            vtkByteSwap::Swap4BE (&tmpfIndex);
+            break;
+        }
+        
+        faceIndices[fvIndex] = tmpfIndex;
+        
 #if FS_CALC_NORMALS
-      // Fill out connectivity info for this face. Get the vertex from
-      // the vertex index and add this face index to its list of
-      // faces. Then add this vertex index to the list of indicies in
-      // the face.
-      if (NULL != vertices && NULL != faces) {
-    v = &vertices[tmpfIndex];
-    v->faces[v->numFaces] = fIndex;
-    v->indicesInFace[v->numFaces] = fvIndex;
-    v->numFaces++;
-    
-    f = &faces[fIndex];
-    f->vertices[fvIndex] = tmpfIndex;
-      }
+        // Fill out connectivity info for this face. Get the vertex from
+        // the vertex index and add this face index to its list of
+        // faces. Then add this vertex index to the list of indicies in
+        // the face.
+        if (NULL != vertices && NULL != faces) {
+            v = &vertices[tmpfIndex];
+            v->faces[v->numFaces] = fIndex;
+            v->indicesInFace[v->numFaces] = fvIndex;
+            v->numFaces++;
+            
+            f = &faces[fIndex];
+            f->vertices[fvIndex] = tmpfIndex;
+        }
 #endif
     }
-
+    
     // Add the face to the list.
     outputFaces->InsertNextCell (numVerticesPerFace, faceIndices);
+    this->UpdateProgress(1.0*thisStep/totalSteps);
+    
   }
   
   // Close the surface file.
   fclose (surfaceFile);
-
+  
 #if FS_DEBUG
   cerr << "Done reading surface." << endl;
 #endif
@@ -376,94 +399,98 @@ void vtkFSSurfaceReader::Execute()
   // calculate the normals.
   if (NULL != vertices && NULL != faces) {
 
-    // For each vertex...
-    for (vIndex = 0; vIndex < numVertices; vIndex++) {
-      
-      // For each face it is a part of...
-      fv1 = &vertices[vIndex];
-      for (fIndex = 0; fIndex < fv1->numFaces; fIndex++) {
+      // For each vertex...
+      for (vIndex = 0; vIndex < numVertices; vIndex++) {
+          
+          // For each face it is a part of...
+          fv1 = &vertices[vIndex];
+          for (fIndex = 0; fIndex < fv1->numFaces; fIndex++) {
 
-    // Get this face. fv1->indicesInFace tells us which index,
-    // 0 - numVerticesPerFace-1, it is in the face. Get the two
-    // indicies surrounding it so we can get the vertices
-    // surrounding it. Now we have fv0, fv1, and fv2 which are all
-    // adjacent in the face, with fv1 being the orignal vertex.
-    f = &faces[fv1->faces[fIndex]];
-    fvIndex1 = fv1->indicesInFace[fIndex];
-    if (0 == fvIndex1) 
-      fvIndex0 = numVerticesPerFace - 1;
-    else
-      fvIndex0 = fvIndex1 - 1;
-    if (numVerticesPerFace == fvIndex1) 
-      fvIndex2 = 0;
-    else
-      fvIndex2 = fvIndex1 + 1;
-    
-    fv0 = &vertices[f->vertices[fvIndex0]];
-    fv2 = &vertices[f->vertices[fvIndex2]];
-    
-    // Get two vectors from these points and normalize them. Then
-    // just cross them to get the perpendicular vector. This is
-    // the normal for the face. Add this to the normal for the
-    // vertex. 
-    faceVector0[0] = fv1->x - fv0->x;
-    faceVector0[1] = fv1->y - fv0->y;
-    faceVector0[2] = fv1->z - fv0->z;
-    faceVector1[0] = fv2->x - fv1->x;
-    faceVector1[1] = fv2->y - fv1->y;
-    faceVector1[2] = fv2->z - fv1->z;
-    length = sqrt( faceVector0[0]*faceVector0[0] +
-               faceVector0[1]*faceVector0[1] +
-               faceVector0[2]*faceVector0[2] );
-    if (length > 0) {
-      faceVector0[0] /= length;
-      faceVector0[1] /= length;
-      faceVector0[2] /= length;
-    }
-    length = sqrt( faceVector1[0]*faceVector1[0] +
-               faceVector1[1]*faceVector1[1] +
-               faceVector1[2]*faceVector1[2] );
-    if (length > 0) {
-      faceVector1[0] /= length;
-      faceVector1[1] /= length;
-      faceVector1[2] /= length;
-    }
+              thisStep++;
+              
+              // Get this face. fv1->indicesInFace tells us which index,
+              // 0 - numVerticesPerFace-1, it is in the face. Get the two
+              // indicies surrounding it so we can get the vertices
+              // surrounding it. Now we have fv0, fv1, and fv2 which are all
+              // adjacent in the face, with fv1 being the orignal vertex.
+              f = &faces[fv1->faces[fIndex]];
+              fvIndex1 = fv1->indicesInFace[fIndex];
+              if (0 == fvIndex1) 
+                  fvIndex0 = numVerticesPerFace - 1;
+              else
+                  fvIndex0 = fvIndex1 - 1;
+              if (numVerticesPerFace == fvIndex1) 
+                  fvIndex2 = 0;
+              else
+                  fvIndex2 = fvIndex1 + 1;
+              
+              fv0 = &vertices[f->vertices[fvIndex0]];
+              fv2 = &vertices[f->vertices[fvIndex2]];
+              
+              // Get two vectors from these points and normalize them. Then
+              // just cross them to get the perpendicular vector. This is
+              // the normal for the face. Add this to the normal for the
+              // vertex. 
+              faceVector0[0] = fv1->x - fv0->x;
+              faceVector0[1] = fv1->y - fv0->y;
+              faceVector0[2] = fv1->z - fv0->z;
+              faceVector1[0] = fv2->x - fv1->x;
+              faceVector1[1] = fv2->y - fv1->y;
+              faceVector1[2] = fv2->z - fv1->z;
+              length = sqrt( faceVector0[0]*faceVector0[0] +
+                             faceVector0[1]*faceVector0[1] +
+                             faceVector0[2]*faceVector0[2] );
+              if (length > 0) {
+                  faceVector0[0] /= length;
+                  faceVector0[1] /= length;
+                  faceVector0[2] /= length;
+              }
+              length = sqrt( faceVector1[0]*faceVector1[0] +
+                             faceVector1[1]*faceVector1[1] +
+                             faceVector1[2]*faceVector1[2] );
+              if (length > 0) {
+                  faceVector1[0] /= length;
+                  faceVector1[1] /= length;
+                  faceVector1[2] /= length;
+              }
+              
+              // get the cross product
+              faceNormal[0] = -faceVector1[1]*faceVector0[2] + 
+                  faceVector0[1]*faceVector1[2];
+              faceNormal[1] = faceVector1[0]*faceVector0[2] - 
+                  faceVector0[0]*faceVector1[2];
+              faceNormal[2] = -faceVector1[0]*faceVector0[1] + 
+                  faceVector0[0]*faceVector1[1];
+              
+              // add it to the normal vector at this vertex.
+              fv1->nx += faceNormal[0];
+              fv1->ny += faceNormal[1];
+              fv1->nz += faceNormal[2];
+          }
+          
+          // When all the faces for this vertex have been processed, the
+          // normal at the vertex is the sum of all the normals for the
+          // adjacent faces. Normalize it and we're done.
+          faceNormal[0] = fv1->nx;
+          faceNormal[1] = fv1->ny;
+          faceNormal[2] = fv1->nz;
+          length = sqrt( faceNormal[0]*faceNormal[0] +
+                         faceNormal[1]*faceNormal[1] +
+                         faceNormal[2]*faceNormal[2] );
+          if (length > 0) {
+              faceNormal[0] /= length;
+              faceNormal[1] /= length;
+              faceNormal[2] /= length;
+          }
 
-    // get the cross product
-    faceNormal[0] = -faceVector1[1]*faceVector0[2] + 
-      faceVector0[1]*faceVector1[2];
-    faceNormal[1] = faceVector1[0]*faceVector0[2] - 
-      faceVector0[0]*faceVector1[2];
-    faceNormal[2] = -faceVector1[0]*faceVector0[1] + 
-      faceVector0[0]*faceVector1[1];
-    
-    // add it to the normal vector at this vertex.
-    fv1->nx += faceNormal[0];
-    fv1->ny += faceNormal[1];
-    fv1->nz += faceNormal[2];
+          this->UpdateProgress(1.0*thisStep/totalSteps);
+          
+          // Add the final normal to the array.
+          outputNormals->InsertNextTuple(faceNormal);
       }
       
-      // When all the faces for this vertex have been processed, the
-      // normal at the vertex is the sum of all the normals for the
-      // adjacent faces. Normalize it and we're done.
-      faceNormal[0] = fv1->nx;
-      faceNormal[1] = fv1->ny;
-      faceNormal[2] = fv1->nz;
-      length = sqrt( faceNormal[0]*faceNormal[0] +
-             faceNormal[1]*faceNormal[1] +
-             faceNormal[2]*faceNormal[2] );
-      if (length > 0) {
-    faceNormal[0] /= length;
-    faceNormal[1] /= length;
-    faceNormal[2] /= length;
-      }
-      
-      // Add the final normal to the array.
-      outputNormals->InsertNextTuple(faceNormal);
-    }
-    
-    free (vertices);
-    free (faces);
+      free (vertices);
+      free (faces);
   }
 #endif
 
@@ -471,11 +498,15 @@ void vtkFSSurfaceReader::Execute()
   output->SetPoints (outputVertices);
   outputVertices->Delete();
 
+  this->SetProgressText("");
+  this->UpdateProgress(0.0);
+  
 #if FS_CALC_NORMALS
   output->GetPointData()->SetNormals (outputNormals);
   outputNormals->Delete();
 #endif
 
+  
   outputFaces->Squeeze(); 
   output->SetPolys(outputFaces);
   outputFaces->Delete();
