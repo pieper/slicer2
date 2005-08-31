1b6089bd37aaa1d6d55612e87a90c712c1114ebc
pohl <pohl>
2005-08-31 21:48:38 +0000

    Archieve CXX Version from vtkEMLocalSegmenter

--- Modules/vtkEMAtlasBrainClassifier/Wrapping/Tcl/vtkEMAtlasBrainClassifier/vtkEMAtlasBrainClassifier.tcl
+++ Modules/vtkEMAtlasBrainClassifier/Wrapping/Tcl/vtkEMAtlasBrainClassifier/vtkEMAtlasBrainClassifier.tcl
@@ -4,18 +4,17 @@ package require vtk
 # rely on the fact that a class loaded from the shared
 # library is uniquely available through this module
 #
-# Because we do not have any vtk code we changed this a little bit
-# source the Module's tcl file that contains it's init procedure
 
-global PACKAGE_DIR_VTKEMAtlasBrainClassifier
+if {[info commands vtkEMAtlasBrainClassifier] != "" ||
+    [::vtk::load_component vtkEMAtlasBrainClassifierTCL] == ""} {
+    global PACKAGE_DIR_VTKEMAtlasBrainClassifier
+    package provide vtkEMAtlasBrainClassifier 1.0
 
-source [file join $PACKAGE_DIR_VTKEMAtlasBrainClassifier/../../../tcl/EMAtlasBrainClassifier.tcl]
-
-package provide vtkEMAtlasBrainClassifier 1.0
-
-# add this module's name to the list of custom modules in order 
-# to have it's init procedure get called, @ModuleName@Init will be 
-# called by the Slicer Base code
-global Module
-lappend Module(customModules) EMAtlasBrainClassifier
-
+    # source the Module's tcl file that contains it's init procedure
+    source [file join $PACKAGE_DIR_VTKEMAtlasBrainClassifier/../../../tcl/EMAtlasBrainClassifier.tcl]
+    # add this module's name to the list of custom modules in order 
+    # to have it's init procedure get called, @ModuleName@Init will be 
+    # called by the Slicer Base code
+    global Module
+    lappend Module(customModules) EMAtlasBrainClassifier
+}
