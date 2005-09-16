506fdb1ccf1b61093b8185decdf7fdee89566711
pieper <pieper>
2005-09-16 22:19:13 +0000

    new class vtkTclHelper to share binary data between tcl and vtk

---
+++ Modules/vtkQueryAtlas/cxx/vtkTclHelper.h
@@ -0,0 +1,56 @@
+// .NAME vtkTclHelper - a bridge between tcl and vtk
+
+// .SECTION Description
+
+// .SECTION See Also
+// vtkTkRenderWidget 
+
+
+#ifndef __vtkTclHelper_h
+#define __vtkTclHelper_h
+
+#include "vtkTcl.h"
+#include "vtkObject.h"
+#include "vtkImageData.h"
+
+#include <vtkQueryAtlasConfigure.h>
+
+
+class VTK_QUERYATLAS_EXPORT vtkTclHelper : public vtkObject
+{
+public:
+    void PrintSelf(ostream& os, vtkIndent indent);
+    vtkTypeRevisionMacro(vtkTclHelper, vtkObject);
+
+    static vtkTclHelper *New();
+
+    /*
+    vtkGetMacro(AlwaysResort, int);  
+    vtkSetMacro(AlwaysResort, int);  
+    */
+
+    void SetInterpFromCommand(unsigned long tag);
+
+    vtkSetObjectMacro(ImageData, vtkImageData);
+    vtkGetObjectMacro(ImageData, vtkImageData);
+
+    const char *Execute (char *Command);
+
+    /*
+    vtkGetObjectMacro(Cards, vtkCollection);  
+    vtkGetObjectMacro(TextureTexts, vtkCollection);  
+
+    void DepthSort();
+    */
+
+
+protected:
+    vtkTclHelper();
+    ~vtkTclHelper(); 
+
+    vtkImageData *ImageData;           
+    Tcl_Interp *Interp;           /* Tcl interpreter */
+};
+
+
+#endif
