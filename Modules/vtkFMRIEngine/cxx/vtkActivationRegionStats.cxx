054095c90e939bd5a9bb4045e8362e38a0ab3285
hliu <hliu>
2005-10-25 19:31:02 +0000

    initial version.

---
+++ Modules/vtkFMRIEngine/cxx/vtkActivationRegionStats.cxx
@@ -0,0 +1,170 @@
+/*=auto=========================================================================
+
+(c) Copyright 2005 Massachusetts Institute of Technology (MIT) All Rights Reserved.
+
+This software ("3D Slicer") is provided by The Brigham and Women's 
+Hospital, Inc. on behalf of the copyright holders and contributors.
+Permission is hereby granted, without payment, to copy, modify, display 
+and distribute this software and its documentation, if any, for  
+research purposes only, provided that (1) the above copyright notice and 
+the following four paragraphs appear on all copies of this software, and 
+(2) that source code to any modifications to this software be made 
+publicly available under terms no more restrictive than those in this 
+License Agreement. Use of this software constitutes acceptance of these 
+terms and conditions.
+
+3D Slicer Software has not been reviewed or approved by the Food and 
+Drug Administration, and is for non-clinical, IRB-approved Research Use 
+Only.  In no event shall data or images generated through the use of 3D 
+Slicer Software be used in the provision of patient care.
+
+IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+
+THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+NON-INFRINGEMENT.
+
+THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================auto=*/
+/*==============================================================================
+(c) Copyright 2004 Massachusetts Institute of Technology (MIT) All Rights Reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+==============================================================================*/
+
+
+#include "vtkActivationRegionStats.h"
+#include "vtkObjectFactory.h"
+#include "vtkImageData.h"
+#include "vtkPointData.h"
+#include "vtkCommand.h"
+
+
+vtkStandardNewMacro(vtkActivationRegionStats);
+
+
+vtkActivationRegionStats::vtkActivationRegionStats()
+{
+    this->Count = 0;
+    this->Max = 0.0; 
+    this->Min = 0.0;
+    this->Mean = 0.0;
+    this->TimeCourse = NULL; 
+    this->Points = NULL;
+}
+
+
+vtkActivationRegionStats::~vtkActivationRegionStats()
+{
+    if (this->TimeCourse != NULL)
+    {
+        this->TimeCourse->Delete();
+    }
+    if (this->Points != NULL)
+    {
+        this->Points->Delete();
+    }
+}
+
+
+vtkFloatArray *vtkActivationRegionStats::GetAveragedTimecourse()
+{
+    return NULL;
+}
+
+
+void vtkActivationRegionStats::SimpleExecute(vtkImageData *inputs, vtkImageData* output)
+{
+    if (this->NumberOfInputs < 2)
+    {
+        vtkErrorMacro( << "This filter needs more inputs of image data.");
+        return;
+    }
+
+    // Set up properties for output vtkImageData
+    // However, we are not going to use the output 
+    int imgDim[3];  
+    this->GetInput(0)->GetDimensions(imgDim);
+    output->SetScalarType(VTK_SHORT);
+    output->SetOrigin(this->GetInput(0)->GetOrigin());
+    output->SetSpacing(this->GetInput(0)->GetSpacing());
+    output->SetNumberOfScalarComponents(1);
+    output->SetDimensions(imgDim[0], imgDim[1], imgDim[2]);
+    output->AllocateScalars();
+   
+    // Set all values to 0 in the output
+    int dim = imgDim[0]*imgDim[1]*imgDim[2];
+    short *ptr = (short *) output->GetScalarPointer();
+    memset(ptr, 0, dim*sizeof(short));
+
+    // Array holding time course of a voxel
+    if (this->Points == NULL)
+    {
+        this->Points = vtkFloatArray::New();
+        this->Points->SetNumberOfTuples(dim);
+        this->Points->SetNumberOfComponents(1);
+    }
+
+    // Number of inputs == 2 means we are going to compute stats 
+    // for t volume: the first volume is the label map volume and 
+    // and the second is the t volume.
+    int indx = 0;
+    float total = 0.0;
+    if (this->NumberOfInputs == 2)
+    {
+        // Voxel iteration through the entire image volume
+        for (int kk = 0; kk < imgDim[2]; kk++)
+        {
+            for (int jj = 0; jj < imgDim[1]; jj++)
+            {
+                for (int ii = 0; ii < imgDim[0]; ii++)
+                {
+                    short *l  
+                        = (short *)this->GetInput(0)->GetScalarPointer(ii, jj, kk);
+                    if (*l == this->Label)
+                    {
+                        float *t  
+                            = (float *)this->GetInput(1)->GetScalarPointer(ii, jj, kk);
+                        this->Points->SetComponent(indx++, 0, *t);
+                        total += *t;
+                    }
+                }
+            } 
+        }
+    }
+    else 
+    {
+    }
+
+    this->Points->Resize(indx);
+
+    // ROI stats
+    double range[2];
+    this->Points->GetRange(range, 0);
+    this->Min = (float) range[0];
+    this->Max = (float) range[1];
+    this->Mean = total / indx;
+    this->Count = indx;
+}
+
