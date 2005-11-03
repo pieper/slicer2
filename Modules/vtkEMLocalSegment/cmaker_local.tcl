afbed4c50754a0573fe4447f003188da01e5b800
pohl <pohl>
2005-11-03 17:07:21 +0000

    Modified script to be more general

--- Modules/vtkEMLocalSegment/cmaker_local.tcl
+++ Modules/vtkEMLocalSegment/cmaker_local.tcl
@@ -3,31 +3,25 @@
 set SLICER_MODULE_ARG "-DVTKEMATLAS_SOURCE_DIR:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier"
 lappend SLICER_MODULE_ARG "-DVTKEMLOCAL_BUILD_DIR:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)" 
 
-switch $tcl_platform(os) {
-    "Linux" -
-    "SunOS" 
-      {
-       lappend SLICER_MODULE_ARG "-DVTKEMATLAS_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)/bin/libvtkEMAtlasBrainClassifier.so"
-       lappend SLICER_MODULE_ARG "-DVTKEMATLAS_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)/bin/libvtkEMAtlasBrainClassifierTCL.lib" 
-    }
-    default {
-         lappend SLICER_MODULE_ARG "-DVTKEMATLAS_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)/bin/debug/vtkEMAtlasBrainClassifier.lib"
-         lappend SLICER_MODULE_ARG "-DVTKEMATLAS_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)/bin/debug/vtkEMAtlasBrainClassifierTCL.lib" 
-    } 
+if {[file exists $SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)/bin/libvtkEMAtlasBrainClassifier.so] == 1} {
+     lappend SLICER_MODULE_ARG "-DVTKEMATLAS_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)/bin/libvtkEMAtlasBrainClassifier.so"
+     lappend SLICER_MODULE_ARG "-DVTKEMATLAS_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)/bin/libvtkEMAtlasBrainClassifierTCL.lib" 
+} else {
+     lappend SLICER_MODULE_ARG "-DVTKEMATLAS_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)/bin/debug/vtkEMAtlasBrainClassifier.lib"
+     lappend SLICER_MODULE_ARG "-DVTKEMATLAS_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkEMAtlasBrainClassifier/builds/$env(BUILD)/bin/debug/vtkEMAtlasBrainClassifierTCL.lib" 
 }
-
 
 # Necessary step after the '$Dir' directory is erased 
 set Dir $SLICER_HOME/Modules/vtkEMLocalSegment/builds/$env(BUILD)
 catch "file mkdir $Dir"
 
-if {[file exists $Dir/vtkEMAtlasBrainClassifier.h] == 0 } {
+if {[file exists $Dir/vtkEMAtlasBrainClassifierConfigure.h] == 0 } {
     if {[catch "file copy ${Dir}/../vtkEMAtlasBrainClassifierConfigure.h ${Dir}/."]} {
-    puts "Error: Failed copying  ${Dir}/../vtkEMAtlasBrainClassifierConfigure.h to ${Dir}/."
+        puts "Error: Failed copying  ${Dir}/../vtkEMAtlasBrainClassifierConfigure.h to ${Dir}/."
     }  else {
         puts ""
         puts "========================= Warning ===================================== " 
-    puts "Copied ${Dir}/../vtkEMAtlasBrainClassifierConfigure.h"
+        puts "Copied ${Dir}/../vtkEMAtlasBrainClassifierConfigure.h"
         puts "to ${Dir}/. successfull !"
         puts "The version of vtkEMAtlasBrainClassifierConfigure.h might be an old version !"  
         puts "If you get strange error messages compiling vtkEMLocalSegment do the following:"
@@ -38,7 +32,7 @@ if {[file exists $Dir/vtkEMAtlasBrainClassifier.h] == 0 } {
         puts "- compile vtkEMLocalSegment"
         puts "If this fixes the compilation error please do a cvs commit"
         puts "to $SLICER_HOME/Modules/vtkEMLocalSegment/builds/vtkEMAtlasBrainClassifierConfigure.h" 
-    puts "======================================================================= " 
+        puts "======================================================================= " 
         puts ""
     }
 }
