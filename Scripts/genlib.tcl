7ef95894d138b43829a914ce9478170233e99b80
hayes <hayes>
2005-11-08 19:41:58 +0000

    Generalized gsl file copy

--- Scripts/genlib.tcl
+++ Scripts/genlib.tcl
@@ -462,7 +462,7 @@ if { ![file exists $::GSL_TEST_FILE] } {
     # check if gsl_multifit.h is in the right location
     if { ![file exists $SLICER_LIB/gsl/include/gsl/gsl_multifit.h] } {
         puts "GSL built in the wrong place; moving file to $SLICER_LIB/gsl/include/gsl/gsl_multifit.h."
-        file copy $SLICER_LIB/gsl/include/gsl_multifit.h $SLICER_LIB/gsl/include/gsl/gsl_multifit.h
+        file copy $SLICER_LIB/gsl/include/*.h $SLICER_LIB/gsl/include/gsl/
     }
 
 }
