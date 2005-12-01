11a4e322660107405f56b512f3c4d241a050c4c3
nicole <nicole>
2005-12-01 23:17:52 +0000

    Check for unsaved models

--- Base/tcl/tcl-main/MainFile.tcl
+++ Base/tcl/tcl-main/MainFile.tcl
@@ -89,7 +89,7 @@ proc MainFileInit {} {
 
         # Set version info
         lappend Module(versions) [ParseCVSInfo MainFile \
-        {$Revision: 1.65 $} {$Date: 2005/11/25 19:36:28 $}]
+        {$Revision: 1.66 $} {$Date: 2005/12/01 23:17:52 $}]
 
     set File(filePrefix) data
 }
@@ -339,13 +339,32 @@ proc MainFileSaveAs {} {
 # .END
 #-------------------------------------------------------------------------------
 proc MainFileSaveAsApply {} {
-    global File Mrml
+    global File Mrml Model
 
     # Prefix cannot be blank
     if {$File(filePrefix) == ""} {
         tk_messageBox -message "A file name must be specified"
     }
-    
+
+    # are all the models saved?
+    set unsavedModels ""
+    foreach m $Model(idList) {
+        if {[info exists Model($m,dirty)] == 1} {
+            if {$Model($m,dirty) == 1} {
+                lappend unsavedModels [Model($m,node) GetName]
+            }
+        }
+    }
+    if {[llength $unsavedModels] != 0} {
+        set msg "The polygon data for the following surface models are unsaved:\n$unsavedModels\nDo you wish to save the scene without the model(s)?"
+        if {[tk_messageBox -message $msg -type yesno] == no} {
+            if {!$::Module(verbose)} {
+                DevInfoWindow "You can save your models in the ModelMaker module, in the Save tab"
+            }
+            return
+        }
+    }
+
     # Relative to root
     set filename [file join $Mrml(dir) $File(filePrefix).xml]
 
