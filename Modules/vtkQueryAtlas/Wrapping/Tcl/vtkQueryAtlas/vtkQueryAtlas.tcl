d4283d5f38730c776ad655fe0b8d4ad2bbf74e69
pieper <pieper>
2005-12-04 20:00:25 +0000

    automatically load the slicer daemon

--- Modules/vtkQueryAtlas/Wrapping/Tcl/vtkQueryAtlas/vtkQueryAtlas.tcl
+++ Modules/vtkQueryAtlas/Wrapping/Tcl/vtkQueryAtlas/vtkQueryAtlas.tcl
@@ -12,6 +12,7 @@ if {[info commands vtkQueryAtlas] != "" ||
 
     # source the Module's tcl file that contains it's init procedure
     source [file join $PACKAGE_DIR_VTKQueryAtlas/../../../tcl/QueryAtlas.tcl]
+    source [file join $PACKAGE_DIR_VTKQueryAtlas/../../../tcl/slicerd.tcl]
     # add this module's name to the list of custom modules in order 
     # to have it's init procedure get called, @ModuleName@Init will be 
     # called by the Slicer Base code
