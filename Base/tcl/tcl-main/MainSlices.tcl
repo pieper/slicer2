1823015443307e97b8006a9eee029139077ac32a
pieper <pieper>
2005-12-11 17:34:09 +0000

    restoring Karl/John changes after vtkMrmlSlicer fix

--- Base/tcl/tcl-main/MainSlices.tcl
+++ Base/tcl/tcl-main/MainSlices.tcl
@@ -109,7 +109,7 @@ proc MainSlicesInit {} {
 
         # Set version info
         lappend Module(versions) [ParseCVSInfo MainSlices \
-        {$Revision: 1.59 $} {$Date: 2005/12/09 21:49:06 $}]
+        {$Revision: 1.60 $} {$Date: 2005/12/11 17:35:21 $}]
 
     # Initialize Variables
     set Slice(idList) "0 1 2"
@@ -1204,7 +1204,9 @@ proc MainSlicesSetZoomAll {zoom} {
         set Slice($s,zoom) $zoom
 #>> Bouix 4/23/03 put the old version of zoom to solve the drawing problem    
         # Attila's new zooming function
-#Slicer SetZoomNew $s $zoom
+        if {[Slicer GetDisplayMethod] ==2} {
+            Slicer SetZoomNew $s $zoom
+        }
     }
     Slicer SetZoom $zoom
 #<< Bouix
@@ -1262,9 +1264,15 @@ proc MainSlicesSetZoom {s {zoom ""}} {
     set Slice($s,zoom) $zoom
 #>> Bouix 4/23/03 Back to old zoom
     # Use Attila's new zooming code
-#    Slicer SetZoomNew $s $zoom
+    
+    if {[Slicer GetDisplayMethod] ==2} {
+        # Use Attila's new zooming code
+        Slicer SetZoomNew $s $zoom
+    }
 
-    Slicer SetZoom $s $zoom
+    if {[Slicer GetDisplayMethod] ==1 || [Slicer GetDisplayMethod] ==3} {
+        Slicer SetZoom $s $zoom
+    }
 #<< Bouix 
     Slicer Update
 }
