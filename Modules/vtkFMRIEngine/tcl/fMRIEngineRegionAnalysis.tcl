3497174e1f7cca22d04471c2fbdd806480aa1f9e
hliu <hliu>
2005-12-19 14:42:48 +0000

    call proc fMRIEngineDrawPlotLong using right parameters.

--- Modules/vtkFMRIEngine/tcl/fMRIEngineRegionAnalysis.tcl
+++ Modules/vtkFMRIEngine/tcl/fMRIEngineRegionAnalysis.tcl
@@ -637,12 +637,8 @@ proc fMRIEngineSelectCondForROIPlot {cond count} {
 
     if {[info exists fMRIEngine(timeCourseToplevel)] &&
         $fMRIEngine(tcPlottingOption) == "Long"} {
-            set x $fMRIEngine(x,voxelIndex)
-            set y $fMRIEngine(y,voxelIndex)
-            set z $fMRIEngine(z,voxelIndex)
-
             # re-plot due to condition switch
-            fMRIEngineDrawPlotLong $x $y $z
+            fMRIEngineDrawPlotLong
     }
 }
 
