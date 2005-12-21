850e862e2a34f1fc28e5b859c0941feda0039107
rjosest <rjosest>
2005-12-21 22:59:01 +0000

    FIX: Tensor(activeID) did not get udpated. Doing different callbacks for each menu button so we set there the activeID and call DTMRISetActive

--- Modules/vtkDTMRI/tcl/DTMRI.tcl
+++ Modules/vtkDTMRI/tcl/DTMRI.tcl
@@ -1,43 +1,25 @@
 #=auto==========================================================================
-# (c) Copyright 2005 Massachusetts Institute of Technology (MIT) All Rights Reserved.
-#
-# This software ("3D Slicer") is provided by The Brigham and Women's 
-# Hospital, Inc. on behalf of the copyright holders and contributors. 
-# Permission is hereby granted, without payment, to copy, modify, display 
-# and distribute this software and its documentation, if any, for 
-# research purposes only, provided that (1) the above copyright notice and 
-# the following four paragraphs appear on all copies of this software, and 
-# (2) that source code to any modifications to this software be made 
-# publicly available under terms no more restrictive than those in this 
-# License Agreement. Use of this software constitutes acceptance of these 
-# terms and conditions.
+#   Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
 # 
-# 3D Slicer Software has not been reviewed or approved by the Food and 
-# Drug Administration, and is for non-clinical, IRB-approved Research Use 
-# Only.  In no event shall data or images generated through the use of 3D 
-# Slicer Software be used in the provision of patient care.
+#   See Doc/copyright/copyright.txt
+#   or http://www.slicer.org/copyright/copyright.txt for details.
 # 
-# IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
-# ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
-# DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
-# EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
-# POSSIBILITY OF SUCH DAMAGE.
+#   Program:   3D Slicer
+#   Module:    $RCSfile: DTMRI.tcl,v $
+#   Date:      $Date: 2005/12/21 22:59:01 $
+#   Version:   $Revision: 1.122 $
 # 
-# THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
-# OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
-# NON-INFRINGEMENT.
-# 
-# THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
-# IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
-# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-# 
-#
 #===============================================================================
 # FILE:        DTMRI.tcl
 # PROCEDURES:  
 #   TensorInit
 #   TensorCreateNew t
+#   TensorUpdateMRML
+#   TensorCreate
+#   TensorRead
+#   TensorDelete
+#   TensorSetActive
+#   MainTensorSetActive
 #   DTMRIInit
 #   DTMRIUpdateMRML
 #   DTMRIEnter
@@ -50,13 +32,14 @@
 #   DTMRIMakeVTKObject object
 #   DTMRIAddObjectProperty object parameter value type desc
 #   DTMRIBuildVTK
-#   DTMRIWriteStructuredPoints filename
 #   DTMRIGetScaledIjkCoordinatesFromWorldCoordinates x y z
 #   DTMRICalculateActorMatrix transform t
 #   DTMRICalculateIJKtoRASRotationMatrix transform t
+#   DTMRISetTensor
 #   DTMRISetActive n
 #   DTMRIUpdateLabelWidgetFromShowLabels
 #   DTMRIUpdateLabelWidget
+#   DTMRIGetVersionInfo
 #==========================================================================auto=
 
 
@@ -499,7 +482,7 @@ proc DTMRIInit {} {
     # Version info (just of this file, not submodule files)
     #------------------------------------
     lappend Module(versions) [ParseCVSInfo $m \
-                  {$Revision: 1.121 $} {$Date: 2005/12/17 23:41:06 $}]
+                  {$Revision: 1.122 $} {$Date: 2005/12/21 22:59:01 $}]
 
     # Define Tabs
     # Many of these correspond to submodules.
@@ -626,11 +609,11 @@ proc DTMRIUpdateMRML {} {
      # Do MRML update of Tensor nodes.
      TensorUpdateMRML Tensor
 
-     DevUpdateNodeSelectButton Tensor DTMRI ActiveGlyph ActiveGlyph DevSelectNode 0 0 0 DTMRISetTensor
-     DevUpdateNodeSelectButton Tensor DTMRI ActiveTract ActiveTract DevSelectNode 0 0 0 DTMRISetTensor
-     DevUpdateNodeSelectButton Tensor DTMRI ActiveMask ActiveMask DevSelectNode 0 0 0 DTMRISetTensor
-     DevUpdateNodeSelectButton Tensor DTMRI ActiveScalars ActiveScalars DevSelectNode 0 0 0 DTMRISetTensor
-     DevUpdateNodeSelectButton Tensor DTMRI ActiveSave ActiveSave DevSelectNode 0 0 0 DTMRISetTensor
+     DevUpdateNodeSelectButton Tensor DTMRI ActiveGlyph ActiveGlyph DevSelectNode 0 0 0 DTMRISetTensorGlyph
+     DevUpdateNodeSelectButton Tensor DTMRI ActiveTract ActiveTract DevSelectNode 0 0 0 DTMRISetTensorTract
+     DevUpdateNodeSelectButton Tensor DTMRI ActiveMask ActiveMask DevSelectNode 0 0 0 DTMRISetTensorMask
+     DevUpdateNodeSelectButton Tensor DTMRI ActiveScalars ActiveScalars DevSelectNode 0 0 0 DTMRISetTensorScalars
+     DevUpdateNodeSelectButton Tensor DTMRI ActiveSave ActiveSave DevSelectNode 0 0 0 DTMRISetTensorSave
      
      # Do MRML update for Tensor Registration tab. Necessary because
      # multiple lists are used.
@@ -1606,6 +1589,12 @@ proc DTMRICalculateIJKtoRASRotationMatrix {transform t} {
 
 }
 
+#-------------------------------------------------------------------------------
+# .PROC DTMRISetTensor
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc DTMRISetTensor {} {
   global Tensor
   
@@ -1613,6 +1602,45 @@ proc DTMRISetTensor {} {
   
 }  
 
+proc DTMRISetTensorGlyph {} {
+  global DTMRI Tensor
+  
+  set Tensor(activeID) $DTMRI(ActiveGlyph)
+  DTMRISetActive $Tensor(activeID)
+  
+}  
+
+proc DTMRISetTensorTract {} {
+  global DTMRI Tensor
+  
+  set Tensor(activeID) $DTMRI(ActiveTract)
+  DTMRISetActive $Tensor(activeID)
+  
+}  
+
+proc DTMRISetTensorMask {} {
+  global DTMRI Tensor
+  
+  set Tensor(activeID) $DTMRI(ActiveMask)
+  DTMRISetActive $Tensor(activeID)
+  
+}  
+
+proc DTMRISetTensorSave {} {
+  global DTMRI Tensor
+  
+  set Tensor(activeID) $DTMRI(ActiveSave)
+  DTMRISetActive $Tensor(activeID)
+  
+}  
+
+proc DTMRISetTensorScalars {} {
+  global DTMRI Tensor
+  
+  set Tensor(activeID) $DTMRI(ActiveScalars)
+  DTMRISetActive $Tensor(activeID)
+  
+}  
 
 #-------------------------------------------------------------------------------
 # .PROC DTMRISetActive
@@ -1764,6 +1792,12 @@ proc DTMRIUpdateLabelWidget {label} {
 }
 
 
+#-------------------------------------------------------------------------------
+# .PROC DTMRIGetVersionInfo
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
 proc DTMRIGetVersionInfo {} {
 
     global DTMRI
