31a7e300082c5262ae7bfa1cf7ace37321c49719
wjp <wjp>
2005-12-22 16:24:58 +0000

    commented, and backed out motion correction for now

--- Modules/vtkIbrowser/tcl/Ibrowser.tcl
+++ Modules/vtkIbrowser/tcl/Ibrowser.tcl
@@ -1,13 +1,37 @@
 #=auto==========================================================================
-#   Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+# (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
 # 
-#   See Doc/copyright/copyright.txt
-#   or http://www.slicer.org/copyright/copyright.txt for details.
+# This software ("3D Slicer") is provided by The Brigham and Women's 
+# Hospital, Inc. on behalf of the copyright holders and contributors.
+# Permission is hereby granted, without payment, to copy, modify, display 
+# and distribute this software and its documentation, if any, for  
+# research purposes only, provided that (1) the above copyright notice and 
+# the following four paragraphs appear on all copies of this software, and 
+# (2) that source code to any modifications to this software be made 
+# publicly available under terms no more restrictive than those in this 
+# License Agreement. Use of this software constitutes acceptance of these 
+# terms and conditions.
 # 
-#   Program:   3D Slicer
-#   Module:    $RCSfile: Ibrowser.tcl,v $
-#   Date:      $Date: 2005/12/20 22:55:59 $
-#   Version:   $Revision: 1.14.2.4 $
+# 3D Slicer Software has not been reviewed or approved by the Food and 
+# Drug Administration, and is for non-clinical, IRB-approved Research Use 
+# Only.  In no event shall data or images generated through the use of 3D 
+# Slicer Software be used in the provision of patient care.
+# 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+# ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+# DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+# EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+# OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+# NON-INFRINGEMENT.
+# 
+# THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+# IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+# 
 # 
 #===============================================================================
 # FILE:        Ibrowser.tcl
@@ -76,7 +100,7 @@ proc IbrowserInit {} {
     #---Set category and version info
     set Module($m,category) "Alpha"
        lappend Module(versions) [ParseCVSInfo $m \
-        {$Revision: 1.14.2.4 $} {$Date: 2005/12/20 22:55:59 $}]
+        {$Revision: 1.14.2.5 $} {$Date: 2005/12/22 16:24:58 $}]
 
     #---Initialize module-level variables
     #---Global array with the same name as the module. Ibrowser()
@@ -166,7 +190,7 @@ proc IbrowserInit {} {
     source ${modulePath}IbrowserInspectGUI.tcl
     
     #--- Developers: ADD NEW PROCESSES TO THIS GLOBAL LIST
-    set ::Ibrowser(Process,AllProcesses) "Reorient MotionCorrect Smooth Reassemble KeyframeRegister"
+    set ::Ibrowser(Process,AllProcesses) "Reorient Smooth Reassemble KeyframeRegister"
     #--- These contain extra procs for
     #--- IO / processing / visualization
     foreach process $::Ibrowser(Process,AllProcesses) {
