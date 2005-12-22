d9136ffec710912cc4c8a607742d17ae71bd88b4
wjp <wjp>
2005-12-22 16:25:29 +0000

    commented

--- Modules/vtkIbrowser/tcl/IbrowserHelpGUI.tcl
+++ Modules/vtkIbrowser/tcl/IbrowserHelpGUI.tcl
@@ -1,13 +1,37 @@
 #=auto==========================================================================
-#   Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+# (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
 # 
-#   See Doc/copyright/copyright.txt
-#   or http://www.slicer.org/copyright/copyright.txt for details.
+# This software ("3D Slicer") is provided by The Brigham and Women's 
+# Hospital, Inc. on behalf of the copyright holders and contributors.
+# Permission is hereby granted, without payment, to copy, modify, display 
+# and distribute this software and its documentation, if any, for  
+# research purposes only, provided that (1) the above copyright notice and 
+# the following four paragraphs appear on all copies of this software, and 
+# (2) that source code to any modifications to this software be made 
+# publicly available under terms no more restrictive than those in this 
+# License Agreement. Use of this software constitutes acceptance of these 
+# terms and conditions.
 # 
-#   Program:   3D Slicer
-#   Module:    $RCSfile: IbrowserHelpGUI.tcl,v $
-#   Date:      $Date: 2005/12/20 22:56:01 $
-#   Version:   $Revision: 1.8.2.2 $
+# 3D Slicer Software has not been reviewed or approved by the Food and 
+# Drug Administration, and is for non-clinical, IRB-approved Research Use 
+# Only.  In no event shall data or images generated through the use of 3D 
+# Slicer Software be used in the provision of patient care.
+# 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+# ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+# DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+# EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+# OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+# NON-INFRINGEMENT.
+# 
+# THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+# IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+# 
 # 
 #===============================================================================
 # FILE:        IbrowserHelpGUI.tcl
@@ -43,13 +67,12 @@ proc IbrowserBuildHelpFrame { } {
     bind $::Module(Ibrowser,bHelp) <ButtonPress-1> "IbrowserUpdateHelpTab"
     
     set help " The <B> Interval Browser</B> or <B>Ibrowser </B> is a developing module for loading, organizing 
-and processing mutl-volume sequences. Bug fixes, new viewing and recording functions, processing options, more 
+and processing mutl-volume sequences. Additional viewing and processing options, more 
 flexible load and populate options, and the ability to load models, label maps and other 
-data will be added in the future. The Ibrowser build has been tested on Windows and Solaris, not yet under Linux or OS X. 
-Some known bugs exist in the canvas scrolling, animation controls, and slice visibility options. Multi-volume sequences 
-loaded through the Ibrowser are selectable in the <B> fMRIEngine </B> module for statistical processing of 
-functional MRI datasets.<P> 
- The <B> Ibrowser </B> loads multi-volume datasets into <I> intervals </I>, which are represented 
+data will be added in the future. Multi-volume sequences loaded through the Ibrowser are selectable in the
+<B> fMRIEngine </B> module for statistical processing of 
+functional MRI datasets.
+<P>  The <B> Ibrowser </B> loads multi-volume datasets into <I> intervals </I>, which are represented 
 as individual tracks on an independent pop-up control panel. Individual volumes are shown arrayed along 
 an ordinal scale that registers all intervals. Selected data is displayed, manipulated and animated in the 
 Slicer's Main Viewer. 
@@ -65,20 +88,18 @@ enable (from left to right) single frame stepping; zoom in and out (not yet work
 backward; stop; record; pause; play forward; goto last volume; continuous animation loop; 
 and continous animation ping-pong (both of which are stopped with the stop button). The behavior of 
 animation controls depends on the number of volumes in the current active interval. <P> 
-The controller also has a scrolled <B> message box </B> in which system messages are displayed. 
+The controller also has a scrolled <B> message box </B> into which messages are logged. 
 <P> <B>New</B> lets you create and populate intervals. The Ibrowser currently reads 
 Analyze, DICOM and BXH format images and will create a sequence out of either a single file, or multiple files from a 
 single directory, specified using a filter. The assemble tab allows you to assign a set of volumes already loaded 
 in Slicer to a new interval. 
 <P> <B>Display</B> options allow you to adjust window, level and threshold for the entire sequence on 'mouse UP'. 
-<P> <B>Process</B> options allow you to perform general processing 
-operations on the sequence.  A suite of options is being developed. 
+<P> <B>Process</B> options allow you to perform general processing operations on the sequence.  A suite of options is being developed. 
 <P> <B>Inspect</B> options allows you to click on a voxel in the slice windows and view a plot of the 
-corresponding voxel in all volumes in the selected interval."
+corresponding voxel's intensity in all volumes in the selected interval."
     
     regsub -all "\n" $help {} help
     MainHelpApplyTags Ibrowser $help
     MainHelpBuildGUI Ibrowser
 
 }
-
--- Modules/vtkIbrowser/tcl/IbrowserProcessGUI.tcl
+++ Modules/vtkIbrowser/tcl/IbrowserProcessGUI.tcl
@@ -1,13 +1,37 @@
 #=auto==========================================================================
-#   Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+# (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
 # 
-#   See Doc/copyright/copyright.txt
-#   or http://www.slicer.org/copyright/copyright.txt for details.
+# This software ("3D Slicer") is provided by The Brigham and Women's 
+# Hospital, Inc. on behalf of the copyright holders and contributors.
+# Permission is hereby granted, without payment, to copy, modify, display 
+# and distribute this software and its documentation, if any, for  
+# research purposes only, provided that (1) the above copyright notice and 
+# the following four paragraphs appear on all copies of this software, and 
+# (2) that source code to any modifications to this software be made 
+# publicly available under terms no more restrictive than those in this 
+# License Agreement. Use of this software constitutes acceptance of these 
+# terms and conditions.
 # 
-#   Program:   3D Slicer
-#   Module:    $RCSfile: IbrowserProcessGUI.tcl,v $
-#   Date:      $Date: 2005/12/20 22:56:02 $
-#   Version:   $Revision: 1.7.2.5 $
+# 3D Slicer Software has not been reviewed or approved by the Food and 
+# Drug Administration, and is for non-clinical, IRB-approved Research Use 
+# Only.  In no event shall data or images generated through the use of 3D 
+# Slicer Software be used in the provision of patient care.
+# 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
+# ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
+# DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
+# EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
+# OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
+# NON-INFRINGEMENT.
+# 
+# THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+# IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
+# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+# 
 # 
 #===============================================================================
 # FILE:        IbrowserProcessGUI.tcl
@@ -19,6 +43,7 @@
 #   IbrowserProcessingSelectExternalReference
 #   IbrowserResetSelectSequence
 #   IbrowserResetInternalReference
+#   IbrowserAddSingleTransform
 #   IbrowserAddTransforms
 #   IbrowserAddNonReferenceTransforms
 #   IbrowserAddWholeIntervalTransform
@@ -222,6 +247,21 @@ proc IbrowserResetInternalReference { } {
 
 
 
+#-------------------------------------------------------------------------------
+# .PROC IbrowserAddSingleTransform
+# 
+# .ARGS
+# .END
+#-------------------------------------------------------------------------------
+proc IbrowserAddSingleTransform { iid vid drop } {
+global Volume
+    
+        set mid [ DataAddTransform 0 Volume($vid,node) Volume($vid,node) ]
+        #--- save a way to find this matrix later.
+        set ::Ibrowser($iid,$drop,matrixID) $mid
+        set ::Ibrowser($iid,$drop,transformID) [ expr $::Transform(nextID) - 1 ]
+}
+
 
 #-------------------------------------------------------------------------------
 # .PROC IbrowserAddTransforms
@@ -251,10 +291,7 @@ proc IbrowserAddTransforms { } {
         }
         
         set vid $::Ibrowser($id,$i,MRMLid)
-        set mid [ DataAddTransform 0 Volume($vid,node) Volume($vid,node) ]
-        #--- save a way to find this matrix later.
-        set ::Ibrowser($id,$i,matrixID) $mid
-        set ::Ibrowser($id,$i,transformID) [ expr $::Transform(nextID) - 1 ]
+        IbrowserAddSingleTransform $id $vid $i
         incr pcount
     }
     IbrowserEndProgressFeedback
@@ -295,10 +332,7 @@ proc IbrowserAddNonReferenceTransforms { } {
         set vid $::Ibrowser($id,$i,MRMLid)
         #--- exclude the reference volume, which should not have a transform.
         if { $vid != $refvol } {
-            set mid [ DataAddTransform 0 Volume($vid,node) Volume($vid,node) ]
-            #--- save a way to find this matrix and transform later.
-            set ::Ibrowser($id,$i,matrixID) $mid
-            set ::Ibrowser($id,$i,transformID) [ expr $::Transform(nextID) - 1 ]
+            IbrowserAddSingleTransform $id $vid $i
             incr pcount
         }
     }
