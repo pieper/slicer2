1d304aa22f917707127947c76a69de668343c693
rjosest <rjosest>
2005-12-22 15:13:18 +0000

    EHN: Adding a transform filter to transform the streamlines from scaledIJk to World. The tube filter now is applied in world space. This should fix the problem with dark fibers due to inward normals.

--- Modules/vtkDTMRI/cxx/vtkDisplayTracts.cxx
+++ Modules/vtkDTMRI/cxx/vtkDisplayTracts.cxx
@@ -1,38 +1,14 @@
 /*=auto=========================================================================
 
-(c) Copyright 2005 Massachusetts Institute of Technology (MIT) All Rights Reserved.
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
 
-This software ("3D Slicer") is provided by The Brigham and Women's 
-Hospital, Inc. on behalf of the copyright holders and contributors.
-Permission is hereby granted, without payment, to copy, modify, display 
-and distribute this software and its documentation, if any, for  
-research purposes only, provided that (1) the above copyright notice and 
-the following four paragraphs appear on all copies of this software, and 
-(2) that source code to any modifications to this software be made 
-publicly available under terms no more restrictive than those in this 
-License Agreement. Use of this software constitutes acceptance of these 
-terms and conditions.
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
 
-3D Slicer Software has not been reviewed or approved by the Food and 
-Drug Administration, and is for non-clinical, IRB-approved Research Use 
-Only.  In no event shall data or images generated through the use of 3D 
-Slicer Software be used in the provision of patient care.
-
-IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
-ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
-DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
-EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
-POSSIBILITY OF SUCH DAMAGE.
-
-THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
-OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
-NON-INFRINGEMENT.
-
-THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
-IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
-PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkDisplayTracts.cxx,v $
+  Date:      $Date: 2005/12/22 15:13:18 $
+  Version:   $Revision: 1.7 $
 
 =========================================================================auto=*/
 #include "vtkDisplayTracts.h"
@@ -40,6 +16,7 @@ PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #include "vtkRenderer.h"
 #include "vtkAppendPolyData.h"
 #include "vtkTubeFilter.h"
+#include "vtkTransformPolyDataFilter.h"
 #include "vtkClipPolyData.h"
 
 
@@ -73,6 +50,7 @@ vtkDisplayTracts::vtkDisplayTracts()
   // internal/output collections
   this->ClippedStreamlines = vtkCollection::New();
   this->Mappers = vtkCollection::New();
+  this->TransformFilters = vtkCollection::New();
   this->TubeFilters = vtkCollection::New();
   this->Actors = vtkCollection::New();
 
@@ -287,6 +265,7 @@ void vtkDisplayTracts::CreateGraphicsObjects()
   vtkActor *currActor;
   vtkTransform *currTransform;
   vtkRenderer *currRenderer;
+  vtkTransformPolyDataFilter *currTransFilter;
   vtkTubeFilter *currTubeFilter;
 
   // Find out how many streamlines we have, and if they all have actors
@@ -318,6 +297,8 @@ void vtkDisplayTracts::CreateGraphicsObjects()
       this->Actors->AddItem((vtkObject *)currActor);
       currMapper = vtkPolyDataMapper::New();
       this->Mappers->AddItem((vtkObject *)currMapper);
+          currTransFilter = vtkTransformPolyDataFilter::New();
+          this->TransformFilters->AddItem((vtkObject *) currTransFilter);
       currTubeFilter = vtkTubeFilter::New();
       this->TubeFilters->AddItem((vtkObject *)currTubeFilter);
 
@@ -325,15 +306,17 @@ void vtkDisplayTracts::CreateGraphicsObjects()
       this->ApplyUserSettingsToGraphicsObject(numActorsCreated);
 
       // Hook up the pipeline
-      currTubeFilter->SetInput(currStreamline->GetOutput());
+          currTransFilter->SetInput(currStreamline->GetOutput());
+      currTubeFilter->SetInput(currTransFilter->GetOutput());
       currMapper->SetInput(currTubeFilter->GetOutput());
       currMapper->SetLookupTable(this->StreamlineLookupTable);
       currMapper->UseLookupTableScalarRangeOn();
       currActor->SetMapper(currMapper);
       
       // Place the actor correctly in the scene
-      currActor->SetUserMatrix(currTransform->GetMatrix());
-
+      //currActor->SetUserMatrix(currTransform->GetMatrix());
+      currTransFilter->SetTransform(currTransform);
+          
       // add to the scene (to each renderer)
       // This is the same as MainAddActor in Main.tcl.
       this->Renderers->InitTraversal();
@@ -428,6 +411,7 @@ void vtkDisplayTracts::DeleteStreamline(int index)
   vtkRenderer *currRenderer;
   //vtkLookupTable *currLookupTable;
   vtkPolyDataMapper *currMapper;
+  vtkTransformPolyDataFilter *currTransFilter;
   vtkTubeFilter *currTubeFilter;
   vtkHyperStreamline *currStreamline;
   vtkActor *currActor;
@@ -465,6 +449,14 @@ void vtkDisplayTracts::DeleteStreamline(int index)
     {
       this->Mappers->RemoveItem(index);
       currMapper->Delete();
+    }
+
+  vtkDebugMacro( << "Delete transformFilter" );
+  currTransFilter = (vtkTransformPolyDataFilter *) this->TransformFilters->GetItemAsObject(index);
+  if (currTransFilter != NULL)
+    {
+      this->TransformFilters->RemoveItem(index);
+      currTransFilter->Delete();
     }
 
   vtkDebugMacro( << "Delete tubeFilter" );
