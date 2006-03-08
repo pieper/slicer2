64b77cd7f2c26240fd6168bb30394f480c106c41
hliu <hliu>
2006-03-08 18:39:22 +0000

    refined the help text.

--- Modules/vtkMRAblation/tcl/MRAblationHelpText.tcl
+++ Modules/vtkMRAblation/tcl/MRAblationHelpText.tcl
@@ -6,8 +6,8 @@
 # 
 #   Program:   3D Slicer
 #   Module:    $RCSfile: MRAblationHelpText.tcl,v $
-#   Date:      $Date: 2006/03/08 15:19:12 $
-#   Version:   $Revision: 1.1.2.1 $
+#   Date:      $Date: 2006/03/08 18:39:22 $
+#   Version:   $Revision: 1.1.2.2 $
 # 
 #===============================================================================
 # FILE:        MRAblationHelpText.tcl
@@ -37,11 +37,11 @@ proc MRAblationHelpComputing { } {
     set txt "<H3>Computing thermal volumes</H3>
 <P> Thermal volumes are computed upon the following settings: 
 <P> <B>Cold images</B>: In general, images in phase 1 are cold images. However, you can choose cold images from any available phases. 
-<P> <B>Hot images</B>: You may pick up any phase (except that for cold images) for hot images or just choose \"all\" on the list. If a specific phase is chosen, only one thermal volume will be computed. If \"all\" is selected, multiple thermal volumes will be computed, one for each phase (except for cold images).
+<P> <B>Hot images</B>: You may pick up any phase (except that for cold images) for hot images or just choose \"all\" on the list. If a specific phase is chosen, only one thermal volume will be computed. If \"all\" is selected, multiple thermal volumes will be computed, one for each hot phase.
 <P> <B>Real images</B>: Just verify your real images. 
 <P> <B>Imaginary images</B>: Just verify your imaginary images. 
 
-<P> If button <I>Automatic</I> is checked, phase 1 will be automatically chosen for cold images and \"all\" selected for hot images if at least 3 phases are available within the experiment." 
+<P> If button <I>One volume per phase</I> is checked, phase 1 will be automatically chosen for cold images and \"all\" selected for hot images if at least 3 phases are available within the experiment." 
 
 
     DevCreateTextPopup infowin$i "MRAblation information" 100 100 18 $txt
