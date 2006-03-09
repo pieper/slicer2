8b6bad9e3d3f64bdf16fdb4775b15e9dfea6a872
hliu <hliu>
2006-03-09 17:20:43 +0000

    refined help text.

--- Modules/vtkMRAblation/tcl/MRAblationHelpText.tcl
+++ Modules/vtkMRAblation/tcl/MRAblationHelpText.tcl
@@ -6,8 +6,8 @@
 # 
 #   Program:   3D Slicer
 #   Module:    $RCSfile: MRAblationHelpText.tcl,v $
-#   Date:      $Date: 2006/03/08 18:39:22 $
-#   Version:   $Revision: 1.1.2.2 $
+#   Date:      $Date: 2006/03/09 17:20:43 $
+#   Version:   $Revision: 1.1.2.3 $
 # 
 #===============================================================================
 # FILE:        MRAblationHelpText.tcl
@@ -35,14 +35,16 @@ proc MRAblationHelpComputing { } {
     #--- compute thermal volumes 
     set i [ MRAblationGetHelpWinID ]
     set txt "<H3>Computing thermal volumes</H3>
-<P> Thermal volumes are computed upon the following settings: 
-<P> <B>Cold images</B>: In general, images in phase 1 are cold images. However, you can choose cold images from any available phases. 
-<P> <B>Hot images</B>: You may pick up any phase (except that for cold images) for hot images or just choose \"all\" on the list. If a specific phase is chosen, only one thermal volume will be computed. If \"all\" is selected, multiple thermal volumes will be computed, one for each hot phase.
-<P> <B>Real images</B>: Just verify your real images. 
-<P> <B>Imaginary images</B>: Just verify your imaginary images. 
 
-<P> If button <I>One volume per phase</I> is checked, phase 1 will be automatically chosen for cold images and \"all\" selected for hot images if at least 3 phases are available within the experiment." 
-
+<P> Thermal volumes are computed from the input images at different timepoints.
+<BR>
+<P> <B>Cold images</B>: In general, images in timepoint 1 are cold images. However, you can choose cold images from any available timepoints. 
+<P> <B>Hot images</B>: You may pick up any timepoint (except that for cold images) for hot images or just choose \"all\" (if any) on the list. If a specific timepoint is chosen, only one thermal volume will be computed. If \"all\" is selected, multiple thermal volumes will be generated, one for each hot timepoint.
+<P> <B>Magnitude images</B>: Select your magnitude images. 
+<P> <B>Real images</B>: Select your real images. 
+<P> <B>Imaginary images</B>: Select your imaginary images. 
+<P> <B>Volume prefix</B>: Select a name prefix for your thermal volume(s). 
+<P> If button <B>One volume per timepoint</B> is checked, timepoint 1 will be automatically chosen for cold images and \"all\" selected for hot images if at least 3 phases are available within the experiment." 
 
     DevCreateTextPopup infowin$i "MRAblation information" 100 100 18 $txt
 }
