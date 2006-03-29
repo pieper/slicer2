427d518ba1337b724192c7a78f845ac69028268c
mathieu <mathieu>
2006-03-29 16:28:33 +0000

    ENH: Dummy test

--- Modules/vtkTensorUtil/Testing/Cxx/TestInstance.cxx
+++ Modules/vtkTensorUtil/Testing/Cxx/TestInstance.cxx
@@ -1,8 +1,43 @@
 #include "vtkBSplineInterpolateImageFunction.h"
+#include "vtkHyperPointandArray.h"
+#include "vtkHyperStreamlineDTMRI.h"
+//#include "vtkHyperStreamlineTeem.h"
+#include "vtkImageGetTensorComponents.h"
+#include "vtkImageSetTensorComponents.h"
+#include "vtkInteractiveTensorGlyph.h"
+#include "vtkPreciseHyperArray.h"
+#include "vtkPreciseHyperPoint.h"
+#include "vtkPreciseHyperStreamline.h"
+#include "vtkPreciseHyperStreamlinePoints.h"
+#include "vtkSuperquadricSource2.h"
+#include "vtkSuperquadricTensorGlyph.h"
+#include "vtkTensorFlip.h"
+#include "vtkTensorImplicitFunctionToFunctionSet.h"
+#include "vtkTensorMask.h"
+#include "vtkTensorMathematics.h"
+#include "vtkVectorToOuterProductDualBasis.h"
 
 int TestInstance(int argc, char *argv[])
 {
-  vtkBSplineInterpolateImageFunction *f = vtkBSplineInterpolateImageFunction::New();
-  f->Delete();
+  vtkBSplineInterpolateImageFunction::New()->Delete();
+  //vtkHyperPointandArray::New()->Delete();
+  vtkHyperStreamlineDTMRI::New()->Delete();
+  //vtkHyperStreamlineTeem::New()->Delete();
+  vtkImageGetTensorComponents::New()->Delete();
+  vtkImageSetTensorComponents::New()->Delete();
+  vtkInteractiveTensorGlyph::New()->Delete();
+  //vtkPreciseHyperArray::New()->Delete();
+  //vtkPreciseHyperPoint::New()->Delete();
+  vtkPreciseHyperStreamline::New()->Delete();
+  vtkPreciseHyperStreamlinePoints::New()->Delete();
+  vtkSuperquadricSource2::New()->Delete();
+  vtkSuperquadricTensorGlyph::New()->Delete();
+  vtkTensorFlip::New()->Delete();
+  vtkTensorImplicitFunctionToFunctionSet::New()->Delete();
+  vtkTensorMask::New()->Delete();
+  vtkTensorMathematics::New()->Delete();
+  vtkVectorToOuterProductDualBasis::New()->Delete();
+
   return 0;
 }
+
