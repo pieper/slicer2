17f42116ee036b7f5348ff11f9ad4a34143ac8c6
pieper <pieper>
2006-04-19 21:39:22 +0000

    BUG: remove comment header so scripts can execute

--- Modules/vtkQueryAtlas/tcl/slicerput.tcl
+++ Modules/vtkQueryAtlas/tcl/slicerput.tcl
@@ -1,18 +1,3 @@
-#=auto==========================================================================
-#   Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
-# 
-#   See Doc/copyright/copyright.txt
-#   or http://www.slicer.org/copyright/copyright.txt for details.
-# 
-#   Program:   3D Slicer
-#   Module:    $RCSfile: slicerput.tcl,v $
-#   Date:      $Date: 2006/01/06 17:58:01 $
-#   Version:   $Revision: 1.3 $
-# 
-#===============================================================================
-# FILE:        slicerput.tcl
-# PROCEDURES:  
-#==========================================================================auto=
 #!/bin/sh
 # the next line restarts using tclsh \
 exec tclsh "$0" "$@"
