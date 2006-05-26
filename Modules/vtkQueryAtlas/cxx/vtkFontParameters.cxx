63293e32017edb0789f5a9fbc06b0761a7edb6b9
nicole <nicole>
2006-05-26 19:59:40 +0000

    Merging from release-2-6 (took out old copyright)

--- Modules/vtkQueryAtlas/cxx/vtkFontParameters.cxx
+++ Modules/vtkQueryAtlas/cxx/vtkFontParameters.cxx
@@ -7,52 +7,12 @@
 
   Program:   3D Slicer
   Module:    $RCSfile: vtkFontParameters.cxx,v $
-  Date:      $Date: 2006/01/06 17:58:00 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2006/05/26 19:59:43 $
+  Version:   $Revision: 1.3 $
 
 =========================================================================auto=*/
+
 /*===========================================================
-
-3D Slicer Software Licence Agreement(c)
-
-Copyright 2003-2004 Massachusetts Institute of Technology 
-(MIT) All Rights Reserved.
-
-This software ("3D Slicer") is provided by The Brigham and 
-Women's Hospital, Inc. on behalf of the copyright holders 
-and contributors. Permission is hereby granted, without 
-payment, to copy, modify, display and distribute this 
-software and its documentation, if any, for research 
-purposes only, provided that (1) the above copyright notice
-and the following four paragraphs appear on all copies of 
-this software, and (2) that source code to any 
-modifications to this software be made publicly available 
-under terms no more restrictive than those in this License 
-Agreement. Use of this software constitutes acceptance of 
-these terms and conditions.
-
-3D Slicer Software has not been reviewed or approved by 
-the Food and Drug Administration, and is for non-clinical, 
-IRB-approved Research Use Only. In no event shall data or 
-images generated through the use of 3D Slicer Software be 
-used in the provision of patient care.
-
-IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
-INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE 
-USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE 
-COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF 
-THE POSSIBILITY OF SUCH DAMAGE.
-
-THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY 
-DISCLAIM ANY EXPRESS OR IMPLIED WARRANTIES INCLUDING, BUT 
-NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
-
-THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-CONTRIBUTORS "AS IS." THE COPYRIGHT HOLDERS AND 
-CONTRIBUTORS HAVE NO OBLIGATION TO PROVIDE MAINTENANCE, 
-SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 Authors: Michael McKenna, David Small, Steve Pieper.
     Small Design Firm, Inc.,
@@ -69,7 +29,7 @@ Authors: Michael McKenna, David Small, Steve Pieper.
 #include "vtkTextureText.h"
 
 
-vtkCxxRevisionMacro(vtkFontParameters, "$Revision: 1.2 $");
+vtkCxxRevisionMacro(vtkFontParameters, "$Revision: 1.3 $");
 vtkStandardNewMacro(vtkFontParameters);
 
 
