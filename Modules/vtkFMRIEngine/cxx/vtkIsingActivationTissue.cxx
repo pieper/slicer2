c15a65f1ad29e19ecbc48839864f7bc885efdce8
nicole <nicole>
2006-06-01 21:54:01 +0000

    DOC: updated copyright. BUG: updated to build with both vtk 5 and 4

--- Modules/vtkFMRIEngine/cxx/vtkIsingActivationTissue.cxx
+++ Modules/vtkFMRIEngine/cxx/vtkIsingActivationTissue.cxx
@@ -1,57 +1,17 @@
 /*=auto=========================================================================
 
-(c) Copyright 2005 Massachusetts Institute of Technology (MIT) All Rights Reserved.
+  Portions (c) Copyright 2006 Brigham and Women's Hospital (BWH) All Rights Reserved.
 
-This software ("3D Slicer") is provided by The Brigham and Women's 
-Hospital, Inc. on behalf of the copyright holders and contributors.
-Permission is hereby granted, without payment, to copy, modify, display 
-and distribute this software and its documentation, if any, for  
-research purposes only, provided that (1) the above copyright notice and 
-the following four paragraphs appear on all copies of this software, and 
-(2) that source code to any modifications to this software be made 
-publicly available under terms no more restrictive than those in this 
-License Agreement. Use of this software constitutes acceptance of these 
-terms and conditions.
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
 
-3D Slicer Software has not been reviewed or approved by the Food and 
-Drug Administration, and is for non-clinical, IRB-approved Research Use 
-Only.  In no event shall data or images generated through the use of 3D 
-Slicer Software be used in the provision of patient care.
-
-IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE TO 
-ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL 
-DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, 
-EVEN IF THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF THE 
-POSSIBILITY OF SUCH DAMAGE.
-
-THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS 
-OR IMPLIED WARRANTIES INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND 
-NON-INFRINGEMENT.
-
-THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
-IS." THE COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE NO OBLIGATION TO 
-PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkIsingActivationTissue.cxx,v $
+  Date:      $Date: 2006/06/01 21:54:02 $
+  Version:   $Revision: 1.2 $
 
 =========================================================================auto=*/
-/*==============================================================================
-(c) Copyright 2004 Massachusetts Institute of Technology (MIT) All Rights Reserved.
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-==============================================================================*/
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
 //               File: vtkIsingActivationTissue.cxx                           //
@@ -88,7 +48,12 @@ void vtkIsingActivationTissue::SimpleExecute(vtkImageData *input, vtkImageData *
   dims[1] = y;
   dims[2] = z;
   size = x*y*z;
-  
+  int numberOfInputs;
+#if (VTK_MAJOR_VERSION >= 5)
+  numberOfInputs = this->GetNumberOfInputConnections(0);
+#else  
+  numberOfInputs = this->NumberOfInputs;
+#endif  
   // initialization of activation Frequence
   for (int i=0; i<nType; i++){
     activationFrequence->InsertNextValue(0.0);
@@ -100,7 +65,7 @@ void vtkIsingActivationTissue::SimpleExecute(vtkImageData *input, vtkImageData *
   output->AllocateScalars();
 
   // in case of anatomical label map input
-  if (this->NumberOfInputs == 2){
+  if (numberOfInputs == 2){
     segMArray = vtkIntArray::New();
     for (unsigned long int i=0; i<size; i++){
       segMArray->InsertNextValue(0);
@@ -135,7 +100,7 @@ void vtkIsingActivationTissue::SimpleExecute(vtkImageData *input, vtkImageData *
   pospp = 0.0;
   negpp = 0.0;     
   for (unsigned long int i=0; i<size; i++){
-    if (this->NumberOfInputs == 2){
+    if (numberOfInputs == 2){
       classIndex = ((activation->GetValue(i)) * segInput) + (segMArray->GetValue(i));
       outputArray->InsertNextValue(classIndex);
       if ((segMArray->GetValue(i)) == nGreyValue){
