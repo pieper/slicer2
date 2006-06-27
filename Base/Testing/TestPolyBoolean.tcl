c41361c42b2c782a6c90258a8cd1294dd9186f32
mathieu <mathieu>
2006-06-27 19:56:03 +0000

    ENH: Giving up for now in vtkPolyBoolean. Can only get seg faults

--- Base/Testing/TestPolyBoolean.tcl
+++ Base/Testing/TestPolyBoolean.tcl
@@ -1,111 +1,64 @@
+package require vtk
+package require vtkSlicerBase
 
-proc TestPolyBoolean_3spheres {} {
+catch "s1 Delete"
+catch "s2 Delete"
+catch "s3 Delete"
 
-    foreach i {1 2 3} {
-        catch "sm$i Delete"
-        vtkPolyDataMapper sm$i
+vtkSphereSource s1
+vtkSphereSource s2
+vtkSphereSource s3
 
-        if {0} {
-            catch "s$i Delete"
-            vtkSphereSource s$i
-            sm$i SetInput [s$i GetOutput]
-        } else {
-            catch "s$i Delete"
-            vtkCubeSource s$i
-            catch "tri$i Delete"
-            vtkTriangleFilter tri$i 
-            tri$i SetInput [s$i GetOutput]
-            sm$i SetInput [tri$i GetOutput]
-        }
+catch "s1m Delete"
+catch "s2m Delete"
+catch "s3m Delete"
 
-        catch "sa$i Delete"
-        vtkActor sa$i
-        sa$i SetMapper sm$i
+vtkPolyDataMapper s1m
+vtkPolyDataMapper s2m
+vtkPolyDataMapper s3m
+s1m SetInput [s1 GetOutput]
+s2m SetInput [s2 GetOutput]
+s3m SetInput [s3 GetOutput]
 
-        catch "viewRen RemoveActor sa$i"
-        viewRen AddActor sa$i
+catch "s1a Delete"
+catch "s2a Delete"
+catch "s3a Delete"
 
-        catch "m$i Delete"
-        vtkMatrix4x4 m$i
-        for {set ii 0} {$ii < 3} {incr ii} {
-            m$i SetElement $ii $ii 50
-            m$i SetElement 0 3 [expr $i * 20]
-            m$i SetElement 2 3 [expr $i * 20]
-        }
-        sa$i SetUserMatrix m$i
-    }
+vtkActor s1a
+vtkActor s2a
+vtkActor s3a
+s1a SetMapper s1m
+s2a SetMapper s2m
+s3a SetMapper s3m
 
-    m$i SetElement 0 3 100
+vtkRenderer viewRen
+catch "viewRen RemoveActor s1a"
+catch "viewRen RemoveActor s2a"
+catch "viewRen RemoveActor s3a"
+viewRen AddActor s1a
+viewRen AddActor s2a
+viewRen AddActor s3a
 
-    Render3D
-}
+s1a SetScale 75 75 75
+s2a SetScale 75 75 75
+s3a SetScale 75 75 75
 
-proc TestPolyBoolean_cutter {} {
-    catch "pb Delete"
-    vtkPolyBoolean pb
-    pb SetOperation 0
-
-    pb SetInput [tri1 GetOutput]
-    pb SetXformA m1
-    pb SetPolyDataB [tri2 GetOutput]
-    pb SetXformB m2
-    pb Update
-    pb UpdateCutter
-
-    set npts [[[pb GetOutput] GetPoints] GetNumberOfPoints]
-
-    set ncells [[pb GetOutput] GetNumberOfCells]
-    for {set cell 0} {$cell < $ncells} {incr cell} {
-        set c [[pb GetOutput] GetCell $cell]
-        set nids [[$c GetPointIds] GetNumberOfIds]
-        for {set n 0} {$n < $nids} {incr n} {
-            if { [[$c GetPointIds] GetId $n] < 0 } {
-                [$c GetPointIds] SetId $n 0
-                puts "bad cell neg id $cell"
-            }
-            if { [[$c GetPointIds] GetId $n] >= $npts } {
-                [$c GetPointIds] SetId $n 0
-                puts "bad cell big id $cell"
-            }
-        }
-    }
-
-    if {0} {
-        catch "pdn Delete"
-        vtkPolyDataNormals pdn 
-        pdn SetInput [pb GetOutput]
-        sm3 SetInput [pdn GetOutput]
-    } else {
-        sm3 SetInput [pb GetOutput]
-    }
-
-    Render3D
-}
-
-proc TestPolyBoolean_print {} {
-
-    set npts [[[pb GetOutput] GetPoints] GetNumberOfPoints]
-
-    for {set pt 0} {$pt < $npts} {incr pt} {
-        puts "$pt: [[[pb GetOutput] GetPoints] GetPoint $pt]"
-    }
-
-    set ncells [[pb GetOutput] GetNumberOfCells]
-    for {set cell 0} {$cell < $ncells} {incr cell} {
-        set c [[pb GetOutput] GetCell $cell]
-        puts "$cell: [$c GetCellType]"
-        set nids [[$c GetPointIds] GetNumberOfIds]
-        for {set n 0} {$n < $nids} {incr n} {
-            puts -nonewline " [[$c GetPointIds] GetId $n]"
-        }
-        puts ""
-    }
-}
-
-proc test {} {
-    TestPolyBoolean_3spheres
-    TestPolyBoolean_cutter
-}
+s1a SetPosition 30 0 -50
+s2a SetPosition -30 0 -50
+s3a SetPosition 0 0 50
 
 
+#Render3D
 
+catch "pb Delete"
+vtkPolyBoolean pb
+pb SetOperation 0
+
+pb SetInput [s1 GetOutput]
+#pb Update
+pb SetPolyDataB [s2 GetOutput]
+pb Update
+##pb UpdateCutter
+#s3m SetInput [pb GetOutput]
+#
+#
