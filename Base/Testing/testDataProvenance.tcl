6dfd128073f64065f927b875f1ae705cec7f5758
nicole <nicole>
2006-07-11 20:23:16 +0000

    Write data prov information to a string, so can write out to file and on stdout for backup viewing

--- Base/Testing/testDataProvenance.tcl
+++ Base/Testing/testDataProvenance.tcl
@@ -51,21 +51,16 @@ if {[info command vtkITKVersion] == ""} {
     catch "vtkitkver Delete"
 }
 
-puts $fd "ProgramName: $execName\nProgramArguments: $argv\nTimeStamp: [clock format [clock seconds] -format "%D-%T-%Z"]\nUser: $::env(USER)\nMachine: $tcl_platform(machine)\nPlatform: $tcl_platform(os) PlatformVersion: $tcl_platform(osVersion)"
-puts $fd "Version: [ParseCVSInfo "" {$Name:  $}]"
-puts $fd "CVS: [ParseCVSInfo "" {$Id: testDataProvenance.tcl,v 1.1 2006/06/09 20:01:41 nicole Exp $}]"
-puts $fd "CompilerName: ${compilerName}\nCompilerVersion: $compilerVersion"
-puts $fd "LibName: VTK LibVersion: ${vtkVersion}\nLibName: TCL LibVersion: ${tcl_patchLevel}\nLibName: TK LibVersion: ${tk_patchLevel}\nLibName: ITK LibVersion: ${itkVersion}"
+set dpString "ProgramName: $execName\nProgramArguments: $argv\nTimeStamp: [clock format [clock seconds] -format "%D-%T-%Z"]\nUser: $::env(USER)\nMachine: $tcl_platform(machine)\nPlatform: $tcl_platform(os) PlatformVersion: $tcl_platform(osVersion)"
+set dpString "${dpString}\nVersion: [ParseCVSInfo "" {$Name:  $}]"
+set dpString "${dpString}\nCVS: [ParseCVSInfo "" {$Id: testDataProvenance.tcl,v 1.2 2006/07/11 20:23:16 nicole Exp $}]"
+set dpString "${dpString}\nCompilerName: ${compilerName} CompilerVersion: $compilerVersion"
+set dpString "${dpString}\nLibName: VTK LibVersion: ${vtkVersion}\nLibName: TCL LibVersion: ${tcl_patchLevel}\nLibName: TK LibVersion: ${tk_patchLevel}\nLibName: ITK LibVersion: ${itkVersion}"
 
+puts "$dpString"
+puts $fd "$dpString"
 close $fd
 
-if {0} {
-set ::SLICER(versionInfo)  "ProgramName: $execName ProgramArguments: $argv\nTimeStamp: [clock format [clock seconds] -format "%D-%T-%Z"] User: $::env(USER) Machine: $tcl_platform(machine) Platform: $tcl_platform(os) PlatformVersion: $tcl_platform(osVersion)"
-set libVersions "LibName: VTK LibVersion: ${vtkVersion} LibName: TCL LibVersion: ${tcl_patchLevel} LibName: TK LibVersion: ${tk_patchLevel} LibName: ITK LibVersion: ${itkVersion}"
-set SLICER(versionInfo) "$SLICER(versionInfo)  Version: $SLICER(version) CompilerName: ${compilerName} CompilerVersion: $compilerVersion ${libVersions} CVS: [ParseCVSInfo "" {$Id: testDataProvenance.tcl,v 1.1 2006/06/09 20:01:41 nicole Exp $}] "
-
-puts "$SLICER(versionInfo)"
-}
 
 puts "0"
 exit 0
