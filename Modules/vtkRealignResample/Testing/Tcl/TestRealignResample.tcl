a94d8dadea6c7caf5170c19124cce0f1c3265b24
kquintus <kquintus>
2006-08-29 14:03:04 +0000

    added some debug output and CTEST_FULL_OUTPUT to enable the full output for ctest

--- Modules/vtkRealignResample/Testing/Tcl/TestRealignResample.tcl
+++ Modules/vtkRealignResample/Testing/Tcl/TestRealignResample.tcl
@@ -1,103 +1,86 @@
 package require vtkSlicerBase
-#package require vtkAG
-#package require vtkITK
-#package require vtkTeem 
 package require vtkRealignResample 
 
-#for the NRRD Reader to load Reference Volume
-
-
-
-
-#proc RunTest {{init 0}} {
-#    global Path Volume Lut Module Model Volumes Slicer Slice exitCode Matrix
-
-   # if {$init==1} {
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainFile.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainMrml.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/Gui.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MRMLapi.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/Main.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-modules/Volumes.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainVolumes.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainColors.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainLuts.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainModels.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainModelGroups.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainTetraMesh.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainAlignments.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/Parse.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-modules/Alignments.tcl]
-        source [file join $::env(SLICER_HOME) Base/tcl/tcl-shared/Developer.tcl]
-        source [file join $::env(SLICER_HOME) Modules/vtkTeem/tcl/VolNrrd.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainFile.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainMrml.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/Gui.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MRMLapi.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/Main.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-modules/Volumes.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainVolumes.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainColors.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainLuts.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainModels.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainModelGroups.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainTetraMesh.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainAlignments.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/Parse.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-modules/Alignments.tcl]
+source [file join $::env(SLICER_HOME) Base/tcl/tcl-shared/Developer.tcl]
+source [file join $::env(SLICER_HOME) Modules/vtkTeem/tcl/VolNrrd.tcl]
 source [file join $::env(SLICER_HOME) Modules/vtkRealignResample/tcl/RealignResample.tcl]               
 source [file join $::env(SLICER_HOME) Base/tcl/tcl-modules/Fiducials.tcl]                
 source [file join $::env(SLICER_HOME) Base/tcl/tcl-main/MainOptions.tcl]                
 
-        set Module(procMRML) ""
-        set Path(program) [file join $::env(SLICER_HOME) Base/tcl]
+set Module(procMRML) ""
+set Path(program) [file join $::env(SLICER_HOME) Base/tcl]
 
-        MainMrmlInit
+MainMrmlInit
 
-        MainVolumesInit
-        set Module(verbose) 0
+MainVolumesInit
+set Module(verbose) 0
 
-        MainColorsInit
-        MainLutsInit
-        MainLutsBuildVTK
+MainColorsInit
+MainLutsInit
+MainLutsBuildVTK
 
-        # Make the None Volume, which can never be deleted
-        #---------------------------------------------------
+# Make the None Volume, which can never be deleted
+#---------------------------------------------------
 
-        set v $Volume(idNone)
+set v $Volume(idNone)
 
-        vtkMrmlVolumeNode Volume($v,node)
-        set n Volume($v,node)
-        $n SetID $v
-        $n SetName "None"
-        $n SetDescription "NoneVolume=$v"
-        $n SetLUTName 0
+vtkMrmlVolumeNode Volume($v,node)
+set n Volume($v,node)
+$n SetID $v
+$n SetName "None"
+$n SetDescription "NoneVolume=$v"
+$n SetLUTName 0
 
-        vtkMrmlDataVolume Volume($v,vol)
-        Volume($v,vol) SetMrmlNode         Volume($v,node)
+vtkMrmlDataVolume Volume($v,vol)
+Volume($v,vol) SetMrmlNode         Volume($v,node)
 
-        #could not find a more pretty solution for now:
-        set Module(idList) Data
+#could not find a more pretty solution for now:
+set Module(idList) Data
 
-        MainModelsInit
+MainModelsInit
 
-        #just to have a volume
-        set Model(fScrolledGUI) dummy 
+#just to have a volume
+set Model(fScrolledGUI) dummy 
 
-        MainTetraMeshInit
+MainTetraMeshInit
 
-        MainAlignmentsInit
-        MainAlignmentsBuildVTK
+MainAlignmentsInit
+MainAlignmentsBuildVTK
         
-        vtkMrmlSlicer Slicer   
-        proc MainSlicesSetVolumeAll {Layer v} {
-        }
-        set Slice(idList) ""
-        AlignmentsInit
-        
-    #}
+vtkMrmlSlicer Slicer   
+proc MainSlicesSetVolumeAll {Layer v} {
+}
+set Slice(idList) ""
+AlignmentsInit
 
-    # stubs
-    proc MainShowProgress {filter} {}
-    proc MainEndProgress {} {}
-    proc RenderAll { {scale ""}} {}
-    FiducialsInit
-    RealignResampleInit
-    set RealignResample(MidlineList) "midsag"
-    set RealignResample(ACPCList) "acpc"
+# stubs
+proc MainShowProgress {filter} {}
+proc MainEndProgress {} {}
+proc RenderAll { {scale ""}} {}
 
-    MainMrmlRead [file join $::env(SLICER_HOME) Modules/vtkRealignResample/Testing/TestInput/RealignResample.xml]
-    MainUpdateMRML
-###brauchst man das?
-#set Module(ignoredList) {}
-#set Module(supList) {Custom CrossSection}
-#MainOptionsInit
-#####
+FiducialsInit
+RealignResampleInit
+set RealignResample(MidlineList) "midsag"
+set RealignResample(ACPCList) "acpc"
+
+MainMrmlRead [file join $::env(SLICER_HOME) Modules/vtkRealignResample/Testing/TestInput/RealignResample.xml]
+MainUpdateMRML
+
 proc FiducialsSliceNumberToRendererName { s } {}
 FiducialsBuildVTK
 set Fiducials(renList) ""
@@ -115,14 +98,10 @@ proc FiducialsConfigScrolledGUI {canvasScrolledGUI fScrolledGUI} {}
 FiducialsUpdateMRML
 RealignCalculate
 
-#}
-#RunTest 1
-#puts "Result: $exitCode"
-#exit $exitCode
-
 MainMrmlImport [file join $::env(SLICER_HOME) Modules/vtkRealignResample/Testing/TestInput/baseline_matrix.xml]
 MainUpdateMRML
 
+puts "CTEST_FULL_OUTPUT"
 puts "\nBaseline matrix:"
 puts "----------------------------"
 set baseline [[Matrix(1,node) GetTransform] GetMatrix] 
@@ -155,12 +134,19 @@ set exitCode 0
 for {set i 0} {$i < 4} {incr i} {
     for {set j 0} {$j < 4} {incr j} {
         set diff [expr {[$baseline GetElement $i $j]-[$nightly_matrix GetElement $i $j]}] 
-        puts -nonewline [format "%.2f " $diff]
+        puts -nonewline $diff
         if {$diff!=0} {
             set exitCode 1
         }
     }
     puts ""    
 }
-
+puts ""
+puts "Exit code: $exitCode"
+puts ""    
+if {$exitCode == 0} {
+    puts "Test passed."
+} else {
+    puts "Test failed."
+}
 exit $exitCode
