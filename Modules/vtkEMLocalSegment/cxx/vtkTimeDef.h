8b69b45ece6f3776758b6562839afc7c4e7ad953
hliu <hliu>
2007-04-18 18:20:57 +0000

    commented out declaration of preciseTimeStart and preciseTimeEnd.

--- Modules/vtkEMLocalSegment/cxx/vtkTimeDef.h
+++ Modules/vtkEMLocalSegment/cxx/vtkTimeDef.h
@@ -7,8 +7,8 @@
 
   Program:   3D Slicer
   Module:    $RCSfile: vtkTimeDef.h,v $
-  Date:      $Date: 2005/12/20 22:55:25 $
-  Version:   $Revision: 1.1.2.1 $
+  Date:      $Date: 2007/04/18 18:20:57 $
+  Version:   $Revision: 1.1.2.1.2.1 $
 
 =========================================================================auto=*/
 #ifndef __vtkTimeDef_h
@@ -42,8 +42,8 @@ extern time_t timeCount; /* In seconds */
 extern time_t timer_Start;
 extern time_t timer_Stop;
 
-extern struct timeval preciseTimeStart;
-extern struct timeval preciseTimeEnd;
+// extern struct timeval preciseTimeStart;
+// extern struct timeval preciseTimeEnd;
 extern long milliSecondDifferences;
 
 #define START_PRECISE_TIMING \
