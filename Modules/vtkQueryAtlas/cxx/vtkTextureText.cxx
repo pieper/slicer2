1a30dadbe516d22413c09327d1a824a727858fb6
hliu <hliu>
2007-10-29 15:23:00 +0000

    Merged from Slicer2.7.

--- Modules/vtkQueryAtlas/cxx/vtkTextureText.cxx
+++ Modules/vtkQueryAtlas/cxx/vtkTextureText.cxx
@@ -7,53 +7,11 @@
 
   Program:   3D Slicer
   Module:    $RCSfile: vtkTextureText.cxx,v $
-  Date:      $Date: 2006/07/07 19:10:11 $
-  Version:   $Revision: 1.1.8.1.2.2 $
+  Date:      $Date: 2007/10/29 15:23:01 $
+  Version:   $Revision: 1.1.8.1.2.3 $
 
 =========================================================================auto=*/
-/*===========================================================
-
-3D Slicer Software Licence Agreement(c)
-
-Copyright 2003-2004 Massachusetts Institute of Technology 
-(MIT) All Rights Reserved.
-
-This software ("3D Slicer") is provided by The Brigham and 
-Women's Hospital, Inc. on behalf of the copyright holders 
-and contributors. Permission is hereby granted, without 
-payment, to copy, modify, display and distribute this 
-software and its documentation, if any, for research 
-purposes only, provided that (1) the above copyright notice
-and the following four paragraphs appear on all copies of 
-this software, and (2) that source code to any 
-modifications to this software be made publicly available 
-under terms no more restrictive than those in this License 
-Agreement. Use of this software constitutes acceptance of 
-these terms and conditions.
-
-3D Slicer Software has not been reviewed or approved by 
-the Food and Drug Administration, and is for non-clinical, 
-IRB-approved Research Use Only. In no event shall data or 
-images generated through the use of 3D Slicer Software be 
-used in the provision of patient care.
-
-IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
-INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE 
-USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE 
-COPYRIGHT HOLDERS AND CONTRIBUTORS HAVE BEEN ADVISED OF 
-THE POSSIBILITY OF SUCH DAMAGE.
-
-THE COPYRIGHT HOLDERS AND CONTRIBUTORS SPECIFICALLY 
-DISCLAIM ANY EXPRESS OR IMPLIED WARRANTIES INCLUDING, BUT 
-NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
-
-THE SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-CONTRIBUTORS "AS IS." THE COPYRIGHT HOLDERS AND 
-CONTRIBUTORS HAVE NO OBLIGATION TO PROVIDE MAINTENANCE, 
-SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-
+/*
 Authors: Michael McKenna, David Small, Steve Pieper.
     Small Design Firm, Inc.,
     in association with Isomics, Inc.,
@@ -99,7 +57,7 @@ characterPosition::characterPosition(unsigned char c)
 
 
 
-vtkCxxRevisionMacro(vtkTextureText, "$Revision: 1.1.8.1.2.2 $");
+vtkCxxRevisionMacro(vtkTextureText, "$Revision: 1.1.8.1.2.3 $");
 vtkStandardNewMacro(vtkTextureText);
 
 
@@ -241,13 +199,13 @@ void vtkTextureText::SetText(char *string)
     int i = 0;
     for(str = this->Text; str[i] != '\0'; i++) { 
         c = (int)str[i];
-        if ((c > 32) && (c <= 127)) {
+        if (c > 32 && c <= 127) {
             // normal character
             characterPosition *cl = new characterPosition(c);
             mCharacterPositions.push_back(cl);
             mCharCount++;
         }
-        else if ((c > 127) && (c <= 255)) {  
+        else if (c > 127 /*&& c <= 255*/) {
             // printf("Special char %d\n", c);
             characterPosition *cl = new characterPosition(c);
             mCharacterPositions.push_back(cl);
