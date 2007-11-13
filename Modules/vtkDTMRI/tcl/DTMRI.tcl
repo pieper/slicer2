551bb46afea54a57620e8197222947a5c0df1ad3
rjosest <rjosest>
2007-11-13 23:48:57 +0000

    ENH: Initializating the transform to identity

--- Modules/vtkDTMRI/tcl/DTMRI.tcl
+++ Modules/vtkDTMRI/tcl/DTMRI.tcl
@@ -6,8 +6,8 @@
 # 
 #   Program:   3D Slicer
 #   Module:    $RCSfile: DTMRI.tcl,v $
-#   Date:      $Date: 2007/11/06 23:43:10 $
-#   Version:   $Revision: 1.133.2.1 $
+#   Date:      $Date: 2007/11/13 23:48:57 $
+#   Version:   $Revision: 1.133.2.2 $
 # 
 #===============================================================================
 # FILE:        DTMRI.tcl
@@ -481,7 +481,7 @@ proc DTMRIInit {} {
     # Version info (just of this file, not submodule files)
     #------------------------------------
     lappend Module(versions) [ParseCVSInfo $m \
-                  {$Revision: 1.133.2.1 $} {$Date: 2007/11/06 23:43:10 $}]
+                  {$Revision: 1.133.2.2 $} {$Date: 2007/11/13 23:48:57 $}]
 
     # Define Tabs
     # Many of these correspond to submodules.
@@ -1403,9 +1403,11 @@ proc DTMRICalculateIJKtoRASRotationMatrix {transform t} {
     # (It also has voxel scaling which we will remove.)
     # -------------------------------------
     #$transform SetMatrix [Tensor($t,node)  GetRasToIjk]
+    $transform Identity
     $transform SetMatrix [Tensor($t,node)  GetWldToIjk]
     # Now it's ijk to ras
     $transform Inverse
+
 
     # Remove the voxel scaling from the matrix.
     # -------------------------------------
