be8cf138f85084cbb1ebfdc4ce2f5cfbae64c56b
pohl <pohl>
2008-02-05 01:30:19 +0000

    just trying

--- Modules/vtkEMLocalSegment/cxx/vtkImageEMLocalSegmenter.h
+++ Modules/vtkEMLocalSegment/cxx/vtkImageEMLocalSegmenter.h
@@ -7,8 +7,8 @@
 
   Program:   3D Slicer
   Module:    $RCSfile: vtkImageEMLocalSegmenter.h,v $
-  Date:      $Date: 2008/02/05 01:29:34 $
-  Version:   $Revision: 1.17 $
+  Date:      $Date: 2008/02/05 01:30:19 $
+  Version:   $Revision: 1.18 $
 
 =========================================================================auto=*/
 // .NAME vtkImageEMLocalSegmenter
@@ -39,11 +39,6 @@ class VTK_EMLOCALSEGMENT_EXPORT vtkImageEMLocalSegmenter : public vtkImageEMGene
   vtkTypeMacro(vtkImageEMLocalSegmenter,vtkObject);
 
   void PrintSelf(ostream& os, vtkIndent indent);
-  dsd
-    dsfs
-    sdd
-    sas
-
   // -----------------------------------------------------
   // Message Protocol
   // -----------------------------------------------------
